c6a15ad27c508679319d9d336b10e53a6d3f5ec2
xuameng <142598141+xuameng@users.noreply.github.com>
2023-08-27 20:54:33 +0800

    Update ApiConfig.java

--- app/src/main/java/com/github/tvbox/osc/api/ApiConfig.java
+++ app/src/main/java/com/github/tvbox/osc/api/ApiConfig.java
@@ -127,7 +127,7 @@ public class ApiConfig {
     }
 
     public void loadConfig(boolean useCache, LoadConfigCallback callback, Activity activity) {
-        String apiUrl = Hawk.get(HawkConfig.API_URL, "");
+        String apiUrl = Hawk.get(HawkConfig.API_URL, "http://xuameng.vicp.net:8082/tvbox/1/xu.json");
         if (apiUrl.isEmpty()) {
             callback.error("-1");
             return;
