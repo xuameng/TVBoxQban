8f4aa09cd4b6eabf17e90583b491e51086ba0679
xuameng <142598141+xuameng@users.noreply.github.com>
2023-09-13 16:10:09 +0800

    Add files via upload

--- app/src/main/java/com/github/tvbox/osc/ui/fragment/ModelSettingFragment.java
+++ app/src/main/java/com/github/tvbox/osc/ui/fragment/ModelSettingFragment.java
@@ -129,7 +129,7 @@ public class ModelSettingFragment extends BaseLazyFragment {
                 tvDebugOpen.setText(Hawk.get(HawkConfig.DEBUG_OPEN, false) ? "已打开" : "已关闭");
             }
         });
-        findViewById(R.id.llParseWebVew).setOnClickListener(new OnClickListener() {      //xuameng系统自带开发中
+        findViewById(R.id.llParseWebVew).setOnClickListener(new View.OnClickListener() {      //xuameng系统自带开发中
             @Override
             public void onClick(View v) {
                 Toast.makeText(mContext, "许大师提示：此功能开发中，敬请期待", Toast.LENGTH_LONG).show();
