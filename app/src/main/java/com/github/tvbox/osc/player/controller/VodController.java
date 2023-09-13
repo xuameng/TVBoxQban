e817ba812835e7210d5280f96061c999bc09f824
xuameng <142598141+xuameng@users.noreply.github.com>
2023-09-13 14:15:16 +0800

    Add files via upload

--- app/src/main/java/com/github/tvbox/osc/player/controller/VodController.java
+++ app/src/main/java/com/github/tvbox/osc/player/controller/VodController.java
@@ -64,8 +64,8 @@ public class VodController extends BaseController {
                     }
                     case 1002: { // 显示底部菜单
                         mBottomRoot.setVisibility(VISIBLE);
-						ObjectAnimator animator = ObjectAnimator.ofFloat(mBottomRoot, "translationY", 600,0);				//xuameng动画菜单
-                        animator.setDuration(2500);				//xuameng动画菜单
+						ObjectAnimator animator = ObjectAnimator.ofFloat(mBottomRoot, "translationY", 400,0);				//xuameng动画菜单
+                        animator.setDuration(1500);				//xuameng动画菜单
 						animator.addListener(new AnimatorListenerAdapter() {
                         @Override
 			            public void onAnimationStart(Animator animation) {
@@ -79,12 +79,12 @@ public class VodController extends BaseController {
                         });
                         animator.start();						//xuameng动画菜单
                         mTopRoot1.setVisibility(VISIBLE);
-						ObjectAnimator animator1 = ObjectAnimator.ofFloat(mTopRoot1, "translationY", -600,0);				//xuameng动画菜单
-                        animator1.setDuration(2500);			//xuameng动画菜单
+						ObjectAnimator animator1 = ObjectAnimator.ofFloat(mTopRoot1, "translationY", -400,0);				//xuameng动画菜单
+                        animator1.setDuration(1500);			//xuameng动画菜单
                         animator1.start();						//xuameng动画菜单
                         mTopRoot2.setVisibility(VISIBLE);
-						ObjectAnimator animator2 = ObjectAnimator.ofFloat(mTopRoot2, "translationY", -600,0);				//xuameng动画菜单
-                        animator2.setDuration(2500);			//xuameng动画菜单
+						ObjectAnimator animator2 = ObjectAnimator.ofFloat(mTopRoot2, "translationY", -400,0);				//xuameng动画菜单
+                        animator2.setDuration(1500);			//xuameng动画菜单
                         animator2.start();						//xuameng动画菜单
                         mxuPlay.requestFocus();				    //底部菜单默认焦点为播放
                         backBtn.setVisibility(ScreenUtils.isTv(context) ? INVISIBLE : VISIBLE);
@@ -94,8 +94,8 @@ public class VodController extends BaseController {
                         break;
                     }
                     case 1003: { // 隐藏底部菜单
-		                ObjectAnimator animator3 = ObjectAnimator.ofFloat(mBottomRoot, "translationY", -0,600);				//xuameng向下划出屏外
-                        animator3.setDuration(2500);				   //xuameng动画菜单        
+		                ObjectAnimator animator3 = ObjectAnimator.ofFloat(mBottomRoot, "translationY", -0,400);				//xuameng向下划出屏外
+                        animator3.setDuration(1500);				   //xuameng动画菜单        
                         animator3.addListener(new AnimatorListenerAdapter() {
                         @Override
 						public void onAnimationStart(Animator animation) {
@@ -111,20 +111,20 @@ public class VodController extends BaseController {
                         }
                         });
                         animator3.start();                          //XUAMENG隐藏底部菜单结束                        
-				        ObjectAnimator animator4 = ObjectAnimator.ofFloat(mTopRoot1, "translationY", 0,-600);				//xuameng向上划出屏外
-                        animator4.setDuration(2500);				//xuameng动画菜单				
+				        ObjectAnimator animator4 = ObjectAnimator.ofFloat(mTopRoot1, "translationY", 0,-400);				//xuameng向上划出屏外
+                        animator4.setDuration(1500);				//xuameng动画菜单				
 		                animator4.start();                          //XUAMENG隐藏上面菜单1结束
-						ObjectAnimator animator5 = ObjectAnimator.ofFloat(mTopRoot2, "translationY", 0,-600);				//xuameng向上划出屏外
-                        animator5.setDuration(2500);			
+						ObjectAnimator animator5 = ObjectAnimator.ofFloat(mTopRoot2, "translationY", 0,-400);				//xuameng向上划出屏外
+                        animator5.setDuration(1500);			
 		                animator5.start();                          //XUAMENG隐藏上面菜单2结束
                         backBtn.setVisibility(INVISIBLE);           //返回键隐藏菜单
 						mPlayTitle.setVisibility(VISIBLE);          //xuameng显示上面节目名称
-				        ObjectAnimator animator6 = ObjectAnimator.ofFloat(mPlayTitle, "translationY", -600,0);				//xuameng动画菜单
-                        animator6.setDuration(2500);				//xuameng动画菜单
+				        ObjectAnimator animator6 = ObjectAnimator.ofFloat(mPlayTitle, "translationY", -400,0);				//xuameng动画菜单
+                        animator6.setDuration(1500);				//xuameng动画菜单
 						animator6.start();						    //XUAMENG显示上面菜单结束
 						mPlayPauseTimexu.setVisibility(VISIBLE);          //xuameng显示上面时间
-				        ObjectAnimator animator7 = ObjectAnimator.ofFloat(mPlayPauseTimexu, "translationY", -600,0);				//xuameng动画菜单
-                        animator7.setDuration(2500);				//xuameng动画菜单
+				        ObjectAnimator animator7 = ObjectAnimator.ofFloat(mPlayPauseTimexu, "translationY", -400,0);				//xuameng动画菜单
+                        animator7.setDuration(1500);				//xuameng动画菜单
 						animator7.start();						    //XUAMENG显示上面菜单的时间结束
                         break;
                     }
@@ -276,7 +276,7 @@ public class VodController extends BaseController {
                 if (getContext() instanceof Activity) {
                     isClickBackBtn = true;
                     ((Activity) getContext()).onBackPressed();
-					FastClickCheckUtil.check(view);
+					FastClickCheckUtilxu.check(view);                //xuameng  防返回键打断动画
                 }
             }
         });
@@ -408,7 +408,7 @@ public class VodController extends BaseController {
             @Override												//xuameng 低菜单播放监听
             public void onClick(View view) {						//xuameng 低菜单播放监听
                 togglePlay();										//xuameng 低菜单播放监听
-				FastClickCheckUtil.check(view);                   //xuameng 时间间隔0.5秒防触碰
+				FastClickCheckUtilxu.check(view);                   //xuameng 防返播放打断动画
             }
         });
         mPreBtn.setOnClickListener(new OnClickListener() {
