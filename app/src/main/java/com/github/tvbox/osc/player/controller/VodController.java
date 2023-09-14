c68c9836a5f673ab7ad96db1d714d4f85d9608f2
xuameng <142598141+xuameng@users.noreply.github.com>
2023-09-14 16:01:41 +0800

    Add files via upload

--- app/src/main/java/com/github/tvbox/osc/player/controller/VodController.java
+++ app/src/main/java/com/github/tvbox/osc/player/controller/VodController.java
@@ -1093,25 +1093,25 @@ public class VodController extends BaseController {
     
     @Override
     public boolean onBackPressed() {
-        if (isClickBackBtn) {
-            isClickBackBtn = false;
-			long secondTime = System.currentTimeMillis();	   //xuameng返回键防连续点击
-			if (secondTime - firstTime < 2000) {
-				firstTime = secondTime;
-            return true;
-            } 
-            if (isBottomVisible()) {                           //xuameng按返回键退出
-                hideBottom();
-            }
-            return false;
+//        if (isClickBackBtn) {
+//            isClickBackBtn = false;
+//            if (isBottomVisible()) {                           //xuameng按返回键退出
+//                hideBottom();
+//            }
+//            return false;
         }
+		long secondTime = System.currentTimeMillis();	   //xuameng返回键防连续点击
+	    if (secondTime - firstTime < 2000) {
+			firstTime = secondTime;
+			return false;
+			}
         if (super.onBackPressed()) {
             return true;
         }
-//        if (isBottomVisible()) {			                   //xuameng按返回键退出
-//            hideBottom();
-//            return true;
-//        }
+        if (isBottomVisible()) {			                   //xuameng按返回键退出
+            hideBottom();
+            return true;
+        }
         return false;
     }
 
