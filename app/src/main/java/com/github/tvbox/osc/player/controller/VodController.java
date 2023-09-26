2d58fecd7340853cb6c6e2fc8ba46fc98976b3f6
xuameng <142598141+xuameng@users.noreply.github.com>
2023-09-26 22:37:32 +0800

    Add files via upload

--- app/src/main/java/com/github/tvbox/osc/player/controller/VodController.java
+++ app/src/main/java/com/github/tvbox/osc/player/controller/VodController.java
@@ -395,20 +395,18 @@ public class VodController extends BaseController {
                     if(keyCode == KeyEvent.KEYCODE_DPAD_RIGHT || keyCode == KeyEvent.KEYCODE_DPAD_LEFT){
 		              if (isInPlayback) {
                       tvSlideStart(keyCode == KeyEvent.KEYCODE_DPAD_RIGHT ? 1 : -1);
-			 	      }
                 return false;
                     }
-				}else if (event.getAction()==KeyEvent.ACTION_UP) {
-				int keyCode = event.getKeyCode();
-                int action = event.getAction();
-				boolean isInPlayback = isInPlaybackState();
-                if (keyCode == KeyEvent.KEYCODE_DPAD_RIGHT || keyCode == KeyEvent.KEYCODE_DPAD_LEFT) {
-                if (isInPlayback) {
-                    tvSlideStop();
-                return false;                    
-                     }
-				  }
-               }
+                  }
+		    	}
+                if(event.getAction()==KeyEvent.ACTION_UP){
+		            if (keyCode == KeyEvent.KEYCODE_DPAD_RIGHT || keyCode == KeyEvent.KEYCODE_DPAD_LEFT) {
+                       if (isInPlayback) {
+                       tvSlideStop();
+                return false;
+                    }
+                  }	
+                }
 		    }
         });
 
