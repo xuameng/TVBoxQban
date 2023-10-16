ecb680fa2e7429b4e4cade34bf2a8325a7a26b3c
xuameng <142598141+xuameng@users.noreply.github.com>
2023-10-16 15:13:08 +0800

    Add files via upload

--- app/src/main/java/com/github/tvbox/osc/ui/activity/LivePlayActivity.java
+++ app/src/main/java/com/github/tvbox/osc/ui/activity/LivePlayActivity.java
@@ -1331,20 +1331,6 @@ public class LivePlayActivity extends BaseActivity {
             }
 
             @Override
-			public boolean onDoubleTap(MotionEvent e) {        //双击事件
-               if(isLl_epgVisible()){ 
-               ll_epg.setVisibility(View.GONE);              //xuameng返回键隐藏下面EPG菜单隐藏
-	           ll_right_top_loading.setVisibility(View.GONE); //xuameng右上菜单隐藏
-             } else if(isBack){
-            
-             } else if(!isLl_epgVisible()){
-               ll_epg.setVisibility(View.VISIBLE);			 //xuameng返回键隐藏下面EPG菜单显示
-	           ll_right_top_loading.setVisibility(View.VISIBLE); //xuameng右上菜单显示
-             } 
-               return true;
-           }
-
-            @Override
             public void playStateChanged(int playState) {
                 switch (playState) {
                     case VideoView.STATE_IDLE:
