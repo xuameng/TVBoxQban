908fe60a2c37f023e97b22fcb107ecf3d4c86505
xuameng <142598141+xuameng@users.noreply.github.com>
2023-11-20 16:13:46 +0800

    Add files via upload

--- app/src/main/java/com/github/tvbox/osc/ui/adapter/LiveEpgAdapter.java
+++ app/src/main/java/com/github/tvbox/osc/ui/adapter/LiveEpgAdapter.java
@@ -124,10 +124,7 @@ public class LiveEpgAdapter extends BaseQuickAdapter<Epginfo, BaseViewHolder> {
     }
 
     public void setFocusedEpgIndex(int focusedEpgIndex) {
-        int preFocusedEpgIndex = this.focusedEpgIndex;
         this.focusedEpgIndex = focusedEpgIndex;
-        if (preFocusedEpgIndex != -1)
-            notifyItemChanged(preFocusedEpgIndex);
         if (this.focusedEpgIndex != -1)
             notifyItemChanged(this.focusedEpgIndex);
     }
