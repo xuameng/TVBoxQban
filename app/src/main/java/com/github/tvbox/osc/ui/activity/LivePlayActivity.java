61a238af6c20f64ee25dbda88bf2b70442baddbf
xuameng <142598141+xuameng@users.noreply.github.com>
2023-11-24 13:26:20 +0800

    Add files via upload

--- app/src/main/java/com/github/tvbox/osc/ui/activity/LivePlayActivity.java
+++ app/src/main/java/com/github/tvbox/osc/ui/activity/LivePlayActivity.java
@@ -587,12 +587,7 @@ public class LivePlayActivity extends BaseActivity {
     }
 
     public void getEpgXu(Date date) {
-   //     String channelName = channel_Name.getChannelName();
-
-
-        
-String channel_Name = getLiveChannels(currentChannelGroupIndex).get(currentLiveChannelIndex);
-Hawk.put(HawkConfig.LIVE_CHANNEL, currentLiveChannelItem.getChannelName());
+        String channelName = currentLiveChannelItem.getChannelSourceNames()
         SimpleDateFormat timeFormat = new SimpleDateFormat("yyyy-MM-dd");
         timeFormat.setTimeZone(TimeZone.getTimeZone("GMT+8:00"));
         String[] epgInfo = EpgUtil.getEpgInfo(channelName);
