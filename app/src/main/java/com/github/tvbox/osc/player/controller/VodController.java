16aed0f0e73cf06cb544d95edb8385ff6380d137
xuameng <142598141+xuameng@users.noreply.github.com>
2024-03-28 20:06:13 +0800

    Update VodController.java

--- app/src/main/java/com/github/tvbox/osc/player/controller/VodController.java
+++ app/src/main/java/com/github/tvbox/osc/player/controller/VodController.java
@@ -998,7 +998,7 @@ public class VodController extends BaseController {
         if (position < 0) position = 0;
         updateSeekUI(currentPosition, position, duration);
 
-		mSeekBar.setProgress(simSeekPosition /1000/1000);
+		mSeekBar.setProgress(simSeekPosition /10000.0f);
 		mCurrentTime.setText(durationToString(simSeekPosition));
 
         simSeekPosition = position;
