2592cb027283450439a7ad4fe8217da652fc5d59
xuameng <142598141+xuameng@users.noreply.github.com>
2024-03-29 10:47:02 +0800

    Update LivePlayActivity.java

--- app/src/main/java/com/github/tvbox/osc/ui/activity/LivePlayActivity.java
+++ app/src/main/java/com/github/tvbox/osc/ui/activity/LivePlayActivity.java
@@ -1368,7 +1368,7 @@ public class LivePlayActivity extends BaseActivity {
 		Mtv_left_top_xu.setVisibility(View.GONE);         //xuameng直播时隐藏回看的菜单
         iv_Play_Xu.setVisibility(View.GONE);       //回看暂停图标
         mVideoView.setUrl(currentLiveChannelItem.getUrl());
-		simSeekPosition = null;      //XUAMENG重要,换视频时重新记录进度
+		simSeekPosition = 0;      //XUAMENG重要,换视频时重新记录进度
         simSlideOffset = 0;       //XUAMENG重要,换视频时重新记录进度
 
        // showChannelInfo();
@@ -2970,7 +2970,7 @@ public class LivePlayActivity extends BaseActivity {
         if (!mVideoView.isPlaying())
         //xuameng快进暂停就暂停测试    mVideoView.start();    //测试成功，如果想暂停时快进自动播放取消注销
         simSlideStart = false;
-        simSeekPosition = null;    //XUAMENG重要
+//        simSeekPosition = 0;    //XUAMENG重要
         simSlideOffset = 0;
     }
 
