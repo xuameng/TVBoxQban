7f9f0e5afc0b8a2a177ed378bb78a139df591044
xuameng <142598141+xuameng@users.noreply.github.com>
2024-04-03 11:33:18 +0800

    Update LivePlayActivity.java

--- app/src/main/java/com/github/tvbox/osc/ui/activity/LivePlayActivity.java
+++ app/src/main/java/com/github/tvbox/osc/ui/activity/LivePlayActivity.java
@@ -2955,12 +2955,14 @@ public class LivePlayActivity extends BaseActivity {
        countDownTimer3.start();
 */
     }
-
+    private boolean simSlideStart = false;
     private int simSeekPosition = 0;
     private long simSlideOffset = 0;
 
     public void tvSlideStop() {
 		isKUAIJIN = false;
+		if (!simSlideStart)
+            return;
         if(simSeekPosition < 10){
 	       mVideoView.release();
            mVideoView.setUrl(currentLiveChannelItem.getUrl());
@@ -2973,6 +2975,7 @@ public class LivePlayActivity extends BaseActivity {
 		}
         if (!mVideoView.isPlaying())
         //xuameng快进暂停就暂停测试    mVideoView.start();    //测试成功，如果想暂停时快进自动播放取消注销
+		simSlideStart = false;
 //        simSeekPosition = 0;    //XUAMENG重要
         simSlideOffset = 0;
     }
@@ -2984,7 +2987,8 @@ public class LivePlayActivity extends BaseActivity {
         int duration = (int) mVideoView.getDuration();
         if (duration <= 0)
             return;
-
+        if (!simSlideStart) {
+            simSlideStart = true;
         // 每次10秒
         simSlideOffset += (10000.0f * dir);
         int currentPosition = (int) mVideoView.getCurrentPosition();
