36241bb1e9d600c2c8c43d272c3516a3ccfa81d4
xuameng <142598141+xuameng@users.noreply.github.com>
2024-04-22 21:34:36 +0800

    Update LivePlayActivity.java

--- app/src/main/java/com/github/tvbox/osc/ui/activity/LivePlayActivity.java
+++ app/src/main/java/com/github/tvbox/osc/ui/activity/LivePlayActivity.java
@@ -1798,8 +1798,8 @@ public class LivePlayActivity extends BaseActivity {
             public void onItemClick(TvRecyclerView parent, View itemView, int position) {
                 mHideChannelListRunXu();  //xuameng隐藏频道菜单
                 liveEpgDateAdapter.setSelectedIndex(position);
-				channel_NameXu = currentLiveChannelItem; 
-                getEpgxu(liveEpgDateAdapter.getData().get(position).getDateParamVal());    //XUAMENG 7天EPG
+				channel_Name = currentLiveChannelItem; 
+                getEpg(liveEpgDateAdapter.getData().get(position).getDateParamVal());    //XUAMENG 7天EPG
             }
         });
 
@@ -1807,27 +1807,15 @@ public class LivePlayActivity extends BaseActivity {
         liveEpgDateAdapter.setOnItemClickListener(new BaseQuickAdapter.OnItemClickListener() {
             @Override
             public void onItemClick(BaseQuickAdapter adapter, View view, int position) {
-			Calendar calendar = Calendar.getInstance();
-            calendar.setTime(new Date());
-            SimpleDateFormat datePresentFormat = new SimpleDateFormat("MM月dd日");          //xuameng加中文
-            calendar.add(Calendar.DAY_OF_MONTH, 1);
-        for (int i = 0; i < 9; i++) {              //XUAMENG8天回看
-            Date dateIns = calendar.getTime();
-            LiveEpgDate epgDate = new LiveEpgDate();
-            epgDate.setIndex(i);
-            epgDate.setDatePresented(datePresentFormat.format(dateIns));
-            epgDate.setDateParamVal(dateIns);
-            liveEpgDateAdapter.addData(epgDate);
-            calendar.add(Calendar.DAY_OF_MONTH, -1);
-        }
+
                 FastClickCheckUtil.check(view);
                 mHideChannelListRunXu();   //xuameng隐藏频道菜单
                 liveEpgDateAdapter.setSelectedIndex(position);
                 currentChannelGroupIndex = liveChannelGroupAdapter.getSelectedGroupIndex();    //XUAMENG 7天EPG
                 currentLiveChannelIndex = liveChannelItemAdapter.getSelectedChannelIndex();
 				currentLiveChannelItem = getLiveChannels(currentChannelGroupIndex).get(currentLiveChannelIndex);
-				channel_NameXu = currentLiveChannelItem; 
-                getEpgxu(liveEpgDateAdapter.getData().get(position).getDateParamVal());    //XUAMENG 7天EPG
+				channel_Name = currentLiveChannelItem; 
+                getEpg(liveEpgDateAdapter.getData().get(position).getDateParamVal());    //XUAMENG 7天EPG
 
             }
         });
