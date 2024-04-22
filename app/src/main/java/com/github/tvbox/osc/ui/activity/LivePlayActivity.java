340d5491960c93a57bce96417cf59b761c70f0ba
xuameng <142598141+xuameng@users.noreply.github.com>
2024-04-22 21:42:28 +0800

    Add files via upload

--- app/src/main/java/com/github/tvbox/osc/ui/activity/LivePlayActivity.java
+++ app/src/main/java/com/github/tvbox/osc/ui/activity/LivePlayActivity.java
@@ -1247,6 +1247,19 @@ public class LivePlayActivity extends BaseActivity {
 		if(isVOD){
 			Mtv_left_top_xu.setVisibility(View.GONE);
 		}
+			Calendar calendar = Calendar.getInstance();
+            calendar.setTime(new Date());
+            SimpleDateFormat datePresentFormat = new SimpleDateFormat("MM月dd日");          //xuameng加中文
+            calendar.add(Calendar.DAY_OF_MONTH, 1);
+        for (int i = 0; i < 9; i++) {              //XUAMENG8天回看
+            Date dateIns = calendar.getTime();
+            LiveEpgDate epgDate = new LiveEpgDate();
+            epgDate.setIndex(i);
+            epgDate.setDatePresented(datePresentFormat.format(dateIns));
+            epgDate.setDateParamVal(dateIns);
+            liveEpgDateAdapter.addData(epgDate);
+            calendar.add(Calendar.DAY_OF_MONTH, -1);
+        }
         if (tvLeftChannelListLayout.getVisibility() == View.INVISIBLE) {
             //重新载入上一次状态
             liveChannelItemAdapter.setNewData(getLiveChannels(currentChannelGroupIndex));
@@ -1798,8 +1811,11 @@ public class LivePlayActivity extends BaseActivity {
             public void onItemClick(TvRecyclerView parent, View itemView, int position) {
                 mHideChannelListRunXu();  //xuameng隐藏频道菜单
                 liveEpgDateAdapter.setSelectedIndex(position);
-				channel_Name = currentLiveChannelItem; 
-                getEpg(liveEpgDateAdapter.getData().get(position).getDateParamVal());    //XUAMENG 7天EPG
+				currentChannelGroupIndexXu = liveChannelGroupAdapter.getSelectedGroupIndex();    //XUAMENG 7天EPG
+                currentLiveChannelIndexXu = liveChannelItemAdapter.getSelectedChannelIndex();
+				currentLiveChannelItemXu = getLiveChannels(currentChannelGroupIndexXu).get(currentLiveChannelIndexXu);
+				channel_NameXu = currentLiveChannelItemXu; 
+                getEpgxu(liveEpgDateAdapter.getData().get(position).getDateParamVal());    //XUAMENG 7天EPG
             }
         });
 
@@ -1811,11 +1827,11 @@ public class LivePlayActivity extends BaseActivity {
                 FastClickCheckUtil.check(view);
                 mHideChannelListRunXu();   //xuameng隐藏频道菜单
                 liveEpgDateAdapter.setSelectedIndex(position);
-                currentChannelGroupIndex = liveChannelGroupAdapter.getSelectedGroupIndex();    //XUAMENG 7天EPG
-                currentLiveChannelIndex = liveChannelItemAdapter.getSelectedChannelIndex();
-				currentLiveChannelItem = getLiveChannels(currentChannelGroupIndex).get(currentLiveChannelIndex);
-				channel_Name = currentLiveChannelItem; 
-                getEpg(liveEpgDateAdapter.getData().get(position).getDateParamVal());    //XUAMENG 7天EPG
+                currentChannelGroupIndexXu = liveChannelGroupAdapter.getSelectedGroupIndex();    //XUAMENG 7天EPG
+                currentLiveChannelIndexXu = liveChannelItemAdapter.getSelectedChannelIndex();
+				currentLiveChannelItemXu = getLiveChannels(currentChannelGroupIndexXu).get(currentLiveChannelIndexXu);
+				channel_NameXu = currentLiveChannelItemXu; 
+                getEpgxu(liveEpgDateAdapter.getData().get(position).getDateParamVal());    //XUAMENG 7天EPG
 
             }
         });
