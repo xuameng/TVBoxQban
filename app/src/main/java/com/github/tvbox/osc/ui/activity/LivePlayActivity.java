6ab3c7927460c6a08f97e03f4b574067b331fc6c
xuameng <142598141+xuameng@users.noreply.github.com>
2024-04-22 17:31:19 +0800

    Update LivePlayActivity.java

--- app/src/main/java/com/github/tvbox/osc/ui/activity/LivePlayActivity.java
+++ app/src/main/java/com/github/tvbox/osc/ui/activity/LivePlayActivity.java
@@ -1807,6 +1807,35 @@ public class LivePlayActivity extends BaseActivity {
             }
         });
 
+        //手机/模拟器
+        liveEpgDateAdapter.setOnItemClickListener(new BaseQuickAdapter.OnItemClickListener() {
+
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(new Date());
+        SimpleDateFormat datePresentFormat = new SimpleDateFormat("MM月dd日");          //xuameng加中文
+        calendar.add(Calendar.DAY_OF_MONTH, 1);
+        for (int i = 0; i < 9; i++) {              //XUAMENG8天回看
+            Date dateIns = calendar.getTime();
+            LiveEpgDate epgDate = new LiveEpgDate();
+            epgDate.setIndex(i);
+            epgDate.setDatePresented(datePresentFormat.format(dateIns));
+            epgDate.setDateParamVal(dateIns);
+            liveEpgDateAdapter.addData(epgDate);
+            calendar.add(Calendar.DAY_OF_MONTH, -1);
+        }
+
+            @Override
+            public void onItemClick(BaseQuickAdapter adapter, View view, int position) {
+                FastClickCheckUtil.check(view);
+                mHideChannelListRunXu();   //xuameng隐藏频道菜单
+                liveEpgDateAdapter.setSelectedIndex(position);
+                currentChannelGroupIndex = liveChannelGroupAdapter.getSelectedGroupIndex();    //XUAMENG 7天EPG
+                currentLiveChannelIndex = liveChannelItemAdapter.getSelectedChannelIndex();
+				currentLiveChannelItem = getLiveChannels(currentChannelGroupIndex).get(currentLiveChannelIndex);
+				channel_NameXu = currentLiveChannelItem; 
+                getEpgxu(liveEpgDateAdapter.getData().get(position).getDateParamVal());    //XUAMENG 7天EPG
+            }
+        });
         liveEpgDateAdapter.setSelectedIndex(1); //xuameng频道EPG日期自动选今天
     }
 
