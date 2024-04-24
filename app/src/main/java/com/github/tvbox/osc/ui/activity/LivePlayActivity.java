db56b125d82add9e644a6234c557ddbea82d5421
xuameng <142598141+xuameng@users.noreply.github.com>
2024-04-24 14:34:15 +0800

    Add files via upload

--- app/src/main/java/com/github/tvbox/osc/ui/activity/LivePlayActivity.java
+++ app/src/main/java/com/github/tvbox/osc/ui/activity/LivePlayActivity.java
@@ -2672,7 +2672,7 @@ public class LivePlayActivity extends BaseActivity {
                   if (percent >= 95) {
                   sBar.setSecondaryProgress(duration2);
                   } else {
-                  sBar.setSecondaryProgress(position + percent * 20000);   //xuameng缓冲进度
+                  sBar.setSecondaryProgress(percent * 10000);   //xuameng缓冲进度
                  }
 			  }
 		    }
