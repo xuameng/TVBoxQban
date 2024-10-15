0b4616b89d1be6dcc88e0c877964282c4204cb32
xuameng <142598141+xuameng@users.noreply.github.com>
2024-10-15 13:08:09 +0800

    Update ExoMediaPlayerFactory.java

--- player/src/main/java/xyz/doikki/videoplayer/exo/ExoMediaPlayerFactory.java
+++ player/src/main/java/xyz/doikki/videoplayer/exo/ExoMediaPlayerFactory.java
@@ -4,25 +4,6 @@ import android.content.Context;
 
 import xyz.doikki.videoplayer.player.PlayerFactory;
 
-import com.google.android.exoplayer2.DefaultLoadControl;
-import com.google.android.exoplayer2.DefaultRenderersFactory;
-import com.google.android.exoplayer2.ExoPlaybackException;
-import com.google.android.exoplayer2.LoadControl;
-import com.google.android.exoplayer2.PlaybackParameters;
-import com.google.android.exoplayer2.Player;
-import com.google.android.exoplayer2.RenderersFactory;
-import com.google.android.exoplayer2.SimpleExoPlayer;
-import com.google.android.exoplayer2.analytics.AnalyticsCollector;
-import com.google.android.exoplayer2.source.DefaultMediaSourceFactory;
-import com.google.android.exoplayer2.source.MediaSource;
-import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
-import com.google.android.exoplayer2.trackselection.MappingTrackSelector;
-import com.google.android.exoplayer2.trackselection.TrackSelector;
-import com.google.android.exoplayer2.upstream.DefaultBandwidthMeter;
-import com.google.android.exoplayer2.util.Clock;
-import com.google.android.exoplayer2.util.EventLogger;
-import com.google.android.exoplayer2.video.VideoSize;
-
 public class ExoMediaPlayerFactory extends PlayerFactory<ExoMediaPlayer> {
 
     public static ExoMediaPlayerFactory create() {
@@ -33,93 +14,4 @@ public class ExoMediaPlayerFactory extends PlayerFactory<ExoMediaPlayer> {
     public ExoMediaPlayer createPlayer(Context context) {
         return new ExoMediaPlayer(context);
     }
-
-    @SuppressLint("UnsafeOptInUsageError")
-    public TrackInfo getTrackInfo() {
-        TrackInfo data = new TrackInfo();
-        MappingTrackSelector.MappedTrackInfo trackInfo = getTrackSelector().getCurrentMappedTrackInfo();
-        if (trackInfo != null) {
-            getExoSelectedTrack();
-            for (int groupArrayIndex = 0; groupArrayIndex < trackInfo.getRendererCount(); groupArrayIndex++) {
-                TrackGroupArray groupArray = trackInfo.getTrackGroups(groupArrayIndex);
-                for (int groupIndex = 0; groupIndex < groupArray.length; groupIndex++) {
-                    TrackGroup group = groupArray.get(groupIndex);
-                    for (int formatIndex = 0; formatIndex < group.length; formatIndex++) {
-                        Format format = group.getFormat(formatIndex);
-                        if (MimeTypes.isAudio(format.sampleMimeType)) {
-                            String trackName = (data.getAudio().size() + 1) + "：" + trackNameProvider.getTrackName(format) + "[" + format.codecs + "]";
-                            TrackInfoBean t = new TrackInfoBean();
-                            t.name = trackName;
-                            t.language = "";
-                            t.trackId = formatIndex;
-                            t.selected = !StringUtils.isEmpty(audioId) && audioId.equals(format.id);
-                            t.trackGroupId = groupIndex;
-                            t.renderId = groupArrayIndex;
-                            data.addAudio(t);
-                        } else if (MimeTypes.isText(format.sampleMimeType)) {
-                            String trackName = (data.getSubtitle().size() + 1) + "：" + trackNameProvider.getTrackName(format);
-                            TrackInfoBean t = new TrackInfoBean();
-                            t.name = trackName;
-                            t.language = "";
-                            t.trackId = formatIndex;
-                            t.selected = !StringUtils.isEmpty(subtitleId) && subtitleId.equals(format.id);
-                            t.trackGroupId = groupIndex;
-                            t.renderId = groupArrayIndex;
-                            data.addSubtitle(t);
-                        }
-                    }
-                }
-            }
-        }
-        return data;
-    }
-
-    @SuppressLint("UnsafeOptInUsageError")
-    private void getExoSelectedTrack() {
-        audioId = "";
-        subtitleId = "";        
-        for (Tracks.Group group : mMediaPlayer.getCurrentTracks().getGroups()) {
-            if (!group.isSelected()) continue;
-            for (int trackIndex = 0; trackIndex < group.length; trackIndex++) {
-                if (!group.isTrackSelected(trackIndex)) continue;
-                Format format = group.getTrackFormat(trackIndex);
-                if (MimeTypes.isAudio(format.sampleMimeType)) {
-                    audioId = format.id;
-                }
-                if (MimeTypes.isText(format.sampleMimeType)) {
-                    subtitleId = format.id;
-                }                
-            }
-        }
-    }
-
-    @SuppressLint("UnsafeOptInUsageError")
-    public void selectExoTrack(@Nullable TrackInfoBean videoTrackBean) {
-        MappingTrackSelector.MappedTrackInfo trackInfo = getTrackSelector().getCurrentMappedTrackInfo();
-        if (trackInfo != null) {
-            if (videoTrackBean == null) {
-                for (int renderIndex = 0; renderIndex < trackInfo.getRendererCount(); renderIndex++) {
-                    if (trackInfo.getRendererType(renderIndex) == C.TRACK_TYPE_TEXT) {
-                        DefaultTrackSelector.Parameters.Builder parametersBuilder = getTrackSelector().getParameters().buildUpon();
-                        parametersBuilder.setRendererDisabled(renderIndex, true);
-                        getTrackSelector().setParameters(parametersBuilder);
-                        break;
-                    }
-                }
-            } else {
-                TrackGroupArray trackGroupArray = trackInfo.getTrackGroups(videoTrackBean.renderId);
-                @SuppressLint("UnsafeOptInUsageError") DefaultTrackSelector.SelectionOverride override = new DefaultTrackSelector.SelectionOverride(videoTrackBean.trackGroupId, videoTrackBean.trackId);
-                DefaultTrackSelector.Parameters.Builder parametersBuilder = getTrackSelector().buildUponParameters();
-                parametersBuilder.setRendererDisabled(videoTrackBean.renderId, false);
-                parametersBuilder.setSelectionOverride(videoTrackBean.renderId, trackGroupArray, override);
-                getTrackSelector().setParameters(parametersBuilder);
-            }
-        }
-
-    }
-
-    public void setOnTimedTextListener(Player.Listener listener) {
-        mMediaPlayer.addListener(listener);
-    }    
 }
-
