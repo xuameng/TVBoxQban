4d63cec80dc1b30448358316c86bbd9c841a12bc
xuameng <142598141+xuameng@users.noreply.github.com>
2024-10-16 18:03:39 +0800

    Update PlayFragment.java

--- app/src/main/java/com/github/tvbox/osc/ui/fragment/PlayFragment.java
+++ app/src/main/java/com/github/tvbox/osc/ui/fragment/PlayFragment.java
@@ -310,6 +310,9 @@ public class PlayFragment extends BaseLazyFragment {
                             @Override
                             public void run() {
                                 String zimuUrl = subtitle.getUrl();
+				    if (zimuUrl = null){    //xuameng 在线搜索字幕空指针
+				    return;
+				    }
                                 LOG.i("Remote Subtitle Url: " + zimuUrl);
                                 setSubtitle(zimuUrl);//设置字幕
                                 searchSubtitleDialog.dismiss();
