0428daefcf2c62030fba3e988464d4c789ac4bc3
xuameng <142598141+xuameng@users.noreply.github.com>
2024-10-18 20:57:50 +0800

    Update ExoMediaSourceHelper.java

--- player/src/main/java/xyz/doikki/videoplayer/exo/ExoMediaSourceHelper.java
+++ player/src/main/java/xyz/doikki/videoplayer/exo/ExoMediaSourceHelper.java
@@ -20,6 +20,7 @@ import com.google.android.exoplayer2.upstream.cache.CacheDataSource;
 import com.google.android.exoplayer2.upstream.cache.LeastRecentlyUsedCacheEvictor;
 import com.google.android.exoplayer2.upstream.cache.SimpleCache;
 import com.google.android.exoplayer2.util.Util;
+import com.google.android.exoplayer2.ext.okhttp.OkHttpDataSource;
 
 import java.io.File;
 import java.lang.reflect.Field;
