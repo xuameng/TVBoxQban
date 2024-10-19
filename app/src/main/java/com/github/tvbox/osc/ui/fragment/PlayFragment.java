e5cf2676ce30619867c93118ff9745e9a038d0a6
xuameng <142598141+xuameng@users.noreply.github.com>
2024-10-19 18:16:50 +0800

    Update PlayFragment.java

--- app/src/main/java/com/github/tvbox/osc/ui/fragment/PlayFragment.java
+++ app/src/main/java/com/github/tvbox/osc/ui/fragment/PlayFragment.java
@@ -603,6 +603,7 @@ public class PlayFragment extends BaseLazyFragment {
     }
 
     private void initSubtitleView() {
+		AbstractPlayer mediaPlayer = mVideoView.getMediaPlayer();
         TrackInfo trackInfo = null;
         if (mVideoView.getMediaPlayer() instanceof IjkMediaPlayer) {
             trackInfo = ((IjkMediaPlayer)(mVideoView.getMediaPlayer())).getTrackInfo();
