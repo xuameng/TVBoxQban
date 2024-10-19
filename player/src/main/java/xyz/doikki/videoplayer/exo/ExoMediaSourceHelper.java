865aeedfc23cfc0b0578e5d2bef5d1e4f637def3
xuameng <142598141+xuameng@users.noreply.github.com>
2024-10-19 20:26:00 +0800

    Update ExoMediaSourceHelper.java

--- player/src/main/java/xyz/doikki/videoplayer/exo/ExoMediaSourceHelper.java
+++ player/src/main/java/xyz/doikki/videoplayer/exo/ExoMediaSourceHelper.java
@@ -127,7 +127,7 @@ public final class ExoMediaSourceHelper {
 
     private Cache newCache() {
         return new SimpleCache(
-                new File(mAppContext.getExternalCachePath(), "exo-video-cache"),//缓存目录
+                new File(mAppContext.getExternalCacheDir(), "exo-video-cache"),//缓存目录
                 new LeastRecentlyUsedCacheEvictor(512 * 1024 * 1024),//缓存大小，默认512M，使用LRU算法实现
                 new StandaloneDatabaseProvider(mAppContext));
     }
