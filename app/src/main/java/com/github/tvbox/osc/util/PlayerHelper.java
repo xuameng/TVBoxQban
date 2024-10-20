6a1feca6e970abfdec77a851a06d2a64b17af74a
xuameng <142598141+xuameng@users.noreply.github.com>
2024-10-20 12:25:10 +0800

    Update PlayerHelper.java

--- app/src/main/java/com/github/tvbox/osc/util/PlayerHelper.java
+++ app/src/main/java/com/github/tvbox/osc/util/PlayerHelper.java
@@ -23,7 +23,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 import tv.danmaku.ijk.media.player.IjkLibLoader;
-import com.github.tvbox.osc.player.exo.ExoMediaPlayerFactory;
+import xyz.doikki.videoplayer.exo.ExoMediaPlayerFactory;
 import xyz.doikki.videoplayer.player.AndroidMediaPlayerFactory;
 import xyz.doikki.videoplayer.player.PlayerFactory;
 import xyz.doikki.videoplayer.player.VideoView;
