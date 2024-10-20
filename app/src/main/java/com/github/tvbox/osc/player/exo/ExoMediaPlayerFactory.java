8db8c9b7e5c320240c3245e0711983fb9d154c85
xuameng <142598141+xuameng@users.noreply.github.com>
2024-10-20 11:13:27 +0800

    Add files via upload

---
+++ app/src/main/java/com/github/tvbox/osc/player/exo/ExoMediaPlayerFactory.java
@@ -0,0 +1,17 @@
+package com.github.tvbox.osc.player.exo;
+
+import android.content.Context;
+
+import xyz.doikki.videoplayer.player.PlayerFactory;
+
+public class ExoMediaPlayerFactory extends PlayerFactory<ExoMediaPlayer> {
+
+    public static ExoMediaPlayerFactory create() {
+        return new ExoMediaPlayerFactory();
+    }
+
+    @Override
+    public ExoMediaPlayer createPlayer(Context context) {
+        return new ExoMediaPlayer(context);
+    }
+}
