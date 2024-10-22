1876a009533b7f40d720e3bfe75e621d9702b979
xuameng <142598141+xuameng@users.noreply.github.com>
2024-10-22 11:36:43 +0800

    Update PlayFragment.java

--- app/src/main/java/com/github/tvbox/osc/ui/fragment/PlayFragment.java
+++ app/src/main/java/com/github/tvbox/osc/ui/fragment/PlayFragment.java
@@ -675,13 +675,19 @@ public class PlayFragment extends BaseLazyFragment {
                                     if (selectedIndex != subtitleTrackInfoBean.trackId) {
                                         ((IjkMediaPlayer)(mVideoView.getMediaPlayer()))
                                             .setTrack(subtitleTrackInfoBean.trackId);
+										else if (mVideoView.getMediaPlayer() instanceof EXOmPlayer){
+                                        ((EXOmPlayer)(mVideoView.getMediaPlayer())).selectExoTrack(subtitleTrackInfoBean);
+                                    }
                                         break;
                                     }
                                 }
                             }
-                            if (!hasCh)((IjkMediaPlayer)(mVideoView.getMediaPlayer()))
-                                .setTrack(subtitleTrackList.get(0)
-                                .trackId);
+						  if(!hasCh){
+                            if (mVideoView.getMediaPlayer() instanceof IjkMediaPlayer){
+                                ((IjkMediaPlayer)(mVideoView.getMediaPlayer())).setTrack(subtitleTrackList.get(0).trackId);
+                            }else if (mVideoView.getMediaPlayer() instanceof EXOmPlayer){
+                                ((EXOmPlayer)(mVideoView.getMediaPlayer())).selectExoTrack(subtitleTrackList.get(0));
+                            }
                         }
                     }
                 }
