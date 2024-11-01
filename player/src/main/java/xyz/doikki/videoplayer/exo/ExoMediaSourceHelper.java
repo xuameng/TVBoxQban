d498ba2e86f8700eee9f80e24018761bef5238e9
xuameng <142598141+xuameng@users.noreply.github.com>
2024-11-01 15:09:46 +0800

    Update ExoMediaSourceHelper.java

--- player/src/main/java/xyz/doikki/videoplayer/exo/ExoMediaSourceHelper.java
+++ player/src/main/java/xyz/doikki/videoplayer/exo/ExoMediaSourceHelper.java
@@ -6,42 +6,32 @@ import android.text.TextUtils;
 
 import com.google.android.exoplayer2.C;
 import com.google.android.exoplayer2.MediaItem;
-//import com.google.android.exoplayer2.PlaybackException;
-import com.google.android.exoplayer2.ExoPlaybackException;
 import com.google.android.exoplayer2.database.ExoDatabaseProvider;
 import com.google.android.exoplayer2.ext.okhttp.OkHttpDataSource;
-//import com.google.android.exoplayer2.database.StandaloneDatabaseProvider;
-import com.google.android.exoplayer2.ext.rtmp.RtmpDataSource;
 import com.google.android.exoplayer2.ext.rtmp.RtmpDataSourceFactory;
-import com.google.android.exoplayer2.extractor.DefaultExtractorsFactory;
-import com.google.android.exoplayer2.extractor.ExtractorsFactory;
-import com.google.android.exoplayer2.extractor.ts.DefaultTsPayloadReaderFactory;
-import com.google.android.exoplayer2.extractor.ts.TsExtractor;
-import com.google.android.exoplayer2.source.DefaultMediaSourceFactory;
 import com.google.android.exoplayer2.source.MediaSource;
 import com.google.android.exoplayer2.source.ProgressiveMediaSource;
 import com.google.android.exoplayer2.source.dash.DashMediaSource;
 import com.google.android.exoplayer2.source.hls.HlsMediaSource;
 import com.google.android.exoplayer2.source.rtsp.RtspMediaSource;
 import com.google.android.exoplayer2.upstream.DataSource;
-import com.google.android.exoplayer2.upstream.DefaultDataSource;
 import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
 import com.google.android.exoplayer2.upstream.cache.Cache;
 import com.google.android.exoplayer2.upstream.cache.CacheDataSource;
 import com.google.android.exoplayer2.upstream.cache.LeastRecentlyUsedCacheEvictor;
 import com.google.android.exoplayer2.upstream.cache.SimpleCache;
-import com.google.android.exoplayer2.util.MimeTypes;
 import com.google.android.exoplayer2.util.Util;
 
 import java.io.File;
 import java.lang.reflect.Field;
+import java.util.Iterator;
 import java.util.Map;
 
 import okhttp3.OkHttpClient;
 
 public final class ExoMediaSourceHelper {
 
-    private static volatile ExoMediaSourceHelper sInstance;
+    private static ExoMediaSourceHelper sInstance;
 
     private final String mUserAgent;
     private final Context mAppContext;
@@ -82,10 +72,6 @@ public final class ExoMediaSourceHelper {
     }
 
     public MediaSource getMediaSource(String uri, Map<String, String> headers, boolean isCache) {
-        return getMediaSource(uri, headers, isCache, -1);
-    }
-
-    public MediaSource getMediaSource(String uri, Map<String, String> headers, boolean isCache, int errorCode) {
         Uri contentUri = Uri.parse(uri);
         if ("rtmp".equals(contentUri.getScheme())) {
             return new ProgressiveMediaSource.Factory(new RtmpDataSourceFactory(null))
@@ -103,7 +89,6 @@ public final class ExoMediaSourceHelper {
         if (mHttpDataSourceFactory != null) {
             setHeaders(headers);
         }
-
         switch (contentType) {
             case C.TYPE_DASH:
                 return new DashMediaSource.Factory(factory).createMediaSource(MediaItem.fromUri(contentUri));
@@ -115,17 +100,11 @@ public final class ExoMediaSourceHelper {
         }
     }
 
-
-    private static synchronized ExtractorsFactory getExtractorsFactory() {
-        return new DefaultExtractorsFactory().setTsExtractorFlags(DefaultTsPayloadReaderFactory.FLAG_ENABLE_HDMV_DTS_AUDIO_STREAMS).setTsExtractorTimestampSearchBytes(TsExtractor.DEFAULT_TIMESTAMP_SEARCH_BYTES * 3);
-
-    }
-
     private int inferContentType(String fileName) {
         fileName = fileName.toLowerCase();
         if (fileName.contains(".mpd") || fileName.contains("type=mpd")) {
             return C.TYPE_DASH;
-        } else if (fileName.contains(".m3u8") || fileName.contains("m3u8")) {
+        } else if (fileName.contains("m3u8")) {
             return C.TYPE_HLS;
         } else {
             return C.TYPE_OTHER;
@@ -187,7 +166,9 @@ public final class ExoMediaSourceHelper {
                     }
                 }
             }
-            for (String k : headers.keySet()) {
+            Iterator<String> iter = headers.keySet().iterator();
+            while (iter.hasNext()) {
+                String k = iter.next();
                 String v = headers.get(k);
                 if (v != null)
                     headers.put(k, v.trim());
@@ -199,5 +180,4 @@ public final class ExoMediaSourceHelper {
     public void setCache(Cache cache) {
         this.mCache = cache;
     }
-
 }
