9adbefb247f5b0dd25e6394c0aacb036d320b598
xuameng <142598141+xuameng@users.noreply.github.com>
2024-11-05 21:03:14 +0800

    Update DescDialog.java

--- app/src/main/java/com/github/tvbox/osc/ui/dialog/DescDialog.java
+++ app/src/main/java/com/github/tvbox/osc/ui/dialog/DescDialog.java
@@ -7,6 +7,7 @@ import androidx.annotation.NonNull;
 import com.github.tvbox.osc.R;
 import org.greenrobot.eventbus.EventBus;
 import org.jetbrains.annotations.NotNull;
+import android.text.method.ScrollingMovementMethod;   //XUAMENG滚动条
 
 public class DescDialog extends BaseDialog {    
 
