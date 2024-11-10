7d9e167d94dc3f8fbaae6aafb39f357010aa4fbd
xuameng <142598141+xuameng@users.noreply.github.com>
2024-11-10 18:20:25 +0800

    Add files via upload

---
+++ app/src/main/java/xyz/doikki/videoplayer/exo/ExoMediaPlayer.java
@@ -0,0 +1,304 @@
+package xyz.doikki.videoplayer.exo;
+
+import android.content.Context;
+import android.content.res.AssetFileDescriptor;
+import android.view.Surface;
+import android.view.SurfaceHolder;
+import com.google.android.exoplayer2.ExoPlaybackException;
+import com.google.android.exoplayer2.DefaultLoadControl;
+import com.google.android.exoplayer2.DefaultRenderersFactory;
+import com.google.android.exoplayer2.LoadControl;
+import com.google.android.exoplayer2.PlaybackParameters;
+import com.google.android.exoplayer2.Player;
+import com.google.android.exoplayer2.RenderersFactory;
+import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.analytics.AnalyticsCollector;
+import com.google.android.exoplayer2.source.DefaultMediaSourceFactory;
+import com.google.android.exoplayer2.source.MediaSource;
+import com.google.android.exoplayer2.source.TrackGroupArray;
+import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
+import com.google.android.exoplayer2.trackselection.MappingTrackSelector;
+import com.google.android.exoplayer2.trackselection.TrackSelectionArray;
+import com.google.android.exoplayer2.trackselection.TrackSelector;
+import com.google.android.exoplayer2.upstream.DefaultBandwidthMeter;
+import com.google.android.exoplayer2.util.NonNullApi;
+import com.google.android.exoplayer2.util.Clock;
+import com.google.android.exoplayer2.util.EventLogger;
+import com.google.android.exoplayer2.video.VideoSize;
+
+import java.util.Map;
+
+import xyz.doikki.videoplayer.player.AbstractPlayer;
+import xyz.doikki.videoplayer.player.VideoViewManager;
+import xyz.doikki.videoplayer.util.PlayerUtils;
+
+public class ExoMediaPlayer extends AbstractPlayer implements Player.Listener {
+
+    protected Context mAppContext;
+    protected SimpleExoPlayer mMediaPlayer;
+    protected MediaSource mMediaSource;
+    protected ExoMediaSourceHelper mMediaSourceHelper;
+
+    private PlaybackParameters mSpeedPlaybackParameters;
+
+    private boolean mIsPreparing;
+
+    private LoadControl mLoadControl;
+    private RenderersFactory mRenderersFactory;
+    private TrackSelector mTrackSelector;
+	protected ExoTrackNameProvider trackNameProvider;
+    protected TrackSelectionArray mTrackSelections;
+
+    public ExoMediaPlayer(Context context) {
+        mAppContext = context.getApplicationContext();
+        mMediaSourceHelper = ExoMediaSourceHelper.getInstance(context);
+    }
+
+    @Override
+    public void initPlayer() {
+        mMediaPlayer = new SimpleExoPlayer.Builder(
+                mAppContext,
+                mRenderersFactory == null ? mRenderersFactory = new DefaultRenderersFactory(mAppContext) : mRenderersFactory,
+                mTrackSelector == null ? mTrackSelector = new DefaultTrackSelector(mAppContext) : mTrackSelector,
+                new DefaultMediaSourceFactory(mAppContext),
+                mLoadControl == null ? mLoadControl = new DefaultLoadControl() : mLoadControl,
+                DefaultBandwidthMeter.getSingletonInstance(mAppContext),
+                new AnalyticsCollector(Clock.DEFAULT))
+                .build();
+        setOptions();
+
+        //播放器日志
+        if (VideoViewManager.getConfig().mIsEnableLog && mTrackSelector instanceof MappingTrackSelector) {
+            mMediaPlayer.addAnalyticsListener(new EventLogger((MappingTrackSelector) mTrackSelector, "ExoPlayer"));
+        }
+
+        mMediaPlayer.addListener(this);
+    }
+    public DefaultTrackSelector getTrackSelector() {
+        return (DefaultTrackSelector) mTrackSelector;
+    }
+    @Override
+    public void onTracksChanged(TrackGroupArray trackGroups, TrackSelectionArray trackSelections) {
+        Player.Listener.super.onTracksChanged(trackGroups, trackSelections);
+        trackNameProvider = new ExoTrackNameProvider(mAppContext.getResources());
+        mTrackSelections = trackSelections;
+    }
+
+    public void setTrackSelector(TrackSelector trackSelector) {
+        mTrackSelector = trackSelector;
+    }
+
+    public void setRenderersFactory(RenderersFactory renderersFactory) {
+        mRenderersFactory = renderersFactory;
+    }
+
+    public void setLoadControl(LoadControl loadControl) {
+        mLoadControl = loadControl;
+    }
+
+    @Override
+    public void setDataSource(String path, Map<String, String> headers) {
+        mMediaSource = mMediaSourceHelper.getMediaSource(path, headers);
+    }
+
+    @Override
+    public void setDataSource(AssetFileDescriptor fd) {
+        //no support
+    }
+
+    @Override
+    public void start() {
+        if (mMediaPlayer == null)
+            return;
+        mMediaPlayer.setPlayWhenReady(true);
+    }
+
+    @Override
+    public void pause() {
+        if (mMediaPlayer == null)
+            return;
+        mMediaPlayer.setPlayWhenReady(false);
+    }
+
+    @Override
+    public void stop() {
+        if (mMediaPlayer == null)
+            return;
+        mMediaPlayer.stop();
+    }
+
+    @Override
+    public void prepareAsync() {
+        if (mMediaPlayer == null)
+            return;
+        if (mMediaSource == null) return;
+        if (mSpeedPlaybackParameters != null) {
+			mMediaPlayer.setPlaybackParameters(mSpeedPlaybackParameters);
+        }
+        mIsPreparing = true;
+        mMediaPlayer.setMediaSource(mMediaSource);
+        mMediaPlayer.prepare();
+    }
+
+    @Override
+    public void reset() {
+        if (mMediaPlayer != null) {
+            mMediaPlayer.stop();
+            mMediaPlayer.clearMediaItems();
+            mMediaPlayer.setVideoSurface(null);
+            mIsPreparing = false;
+        }
+    }
+
+    @Override
+    public boolean isPlaying() {
+        if (mMediaPlayer == null)
+            return false;
+        int state = mMediaPlayer.getPlaybackState();
+        switch (state) {
+            case Player.STATE_BUFFERING:
+            case Player.STATE_READY:
+                return mMediaPlayer.getPlayWhenReady();
+            case Player.STATE_IDLE:
+            case Player.STATE_ENDED:
+            default:
+                return false;
+        }
+    }
+
+    @Override
+    public void seekTo(long time) {
+        if (mMediaPlayer == null)
+            return;
+        mMediaPlayer.seekTo(time);
+    }
+
+    @Override
+    public void release() {
+        if (mMediaPlayer != null) {
+            mMediaPlayer.removeListener(this);
+            mMediaPlayer.release();
+            mMediaPlayer = null;
+        }
+
+        mIsPreparing = false;
+        mSpeedPlaybackParameters = null;
+    }
+
+    @Override
+    public long getCurrentPosition() {
+        if (mMediaPlayer == null)
+            return 0;
+        return mMediaPlayer.getCurrentPosition();
+    }
+
+    @Override
+    public long getDuration() {
+        if (mMediaPlayer == null)
+            return 0;
+        return mMediaPlayer.getDuration();
+    }
+
+    @Override
+    public int getBufferedPercentage() {
+        return mMediaPlayer == null ? 0 : mMediaPlayer.getBufferedPercentage();
+    }
+
+    @Override
+    public void setSurface(Surface surface) {
+        if (mMediaPlayer != null) {
+            mMediaPlayer.setVideoSurface(surface);
+        }
+    }
+
+    @Override
+    public void setDisplay(SurfaceHolder holder) {
+        if (holder == null)
+            setSurface(null);
+        else
+            setSurface(holder.getSurface());
+    }
+
+    @Override
+    public void setVolume(float leftVolume, float rightVolume) {
+        if (mMediaPlayer != null)
+            mMediaPlayer.setVolume((leftVolume + rightVolume) / 2);
+    }
+
+    @Override
+    public void setLooping(boolean isLooping) {
+       if (mMediaPlayer != null)
+            mMediaPlayer.setRepeatMode(isLooping ? Player.REPEAT_MODE_ALL : Player.REPEAT_MODE_OFF);
+    }
+
+    @Override
+    public void setOptions() {
+        //准备好就开始播放
+        mMediaPlayer.setPlayWhenReady(true);
+    }
+
+    @Override
+    public void setSpeed(float speed) {
+        PlaybackParameters playbackParameters = new PlaybackParameters(speed);
+        mSpeedPlaybackParameters = playbackParameters;
+        if (mMediaPlayer != null) {
+            mMediaPlayer.setPlaybackParameters(playbackParameters);
+        }
+    }
+
+    @Override
+    public float getSpeed() {
+        if (mSpeedPlaybackParameters != null) {
+            return mSpeedPlaybackParameters.speed;
+        }
+        return 1f;
+    }
+
+    @Override
+    public long getTcpSpeed() {
+        return PlayerUtils.getNetSpeed(mAppContext);
+    }
+
+    @Override
+    public void onPlaybackStateChanged(int playbackState) {
+        if (mPlayerEventListener == null) return;
+        if (mIsPreparing) {
+            if (playbackState == Player.STATE_READY) {
+                mPlayerEventListener.onPrepared();
+                mPlayerEventListener.onInfo(MEDIA_INFO_RENDERING_START, 0);
+                mIsPreparing = false;
+            }
+            return;
+        }
+        switch (playbackState) {
+            case Player.STATE_BUFFERING:
+                mPlayerEventListener.onInfo(MEDIA_INFO_BUFFERING_START, getBufferedPercentage());
+                break;
+            case Player.STATE_READY:
+                mPlayerEventListener.onInfo(MEDIA_INFO_BUFFERING_END, getBufferedPercentage());
+                break;
+            case Player.STATE_ENDED:
+                mPlayerEventListener.onCompletion();
+                break;
+		    case Player.STATE_IDLE:
+                break;
+        }
+    }
+
+    @Override
+    public void onPlayerError(ExoPlaybackException error) {
+        if (mPlayerEventListener != null) {
+            mPlayerEventListener.onError();
+        }
+    }
+
+    @Override
+    public void onVideoSizeChanged(VideoSize videoSize) {
+        if (mPlayerEventListener != null) {
+            mPlayerEventListener.onVideoSizeChanged(videoSize.width, videoSize.height);
+            if (videoSize.unappliedRotationDegrees > 0) {
+                mPlayerEventListener.onInfo(MEDIA_INFO_VIDEO_ROTATION_CHANGED, videoSize.unappliedRotationDegrees);
+            }
+        }
+    }
+}
