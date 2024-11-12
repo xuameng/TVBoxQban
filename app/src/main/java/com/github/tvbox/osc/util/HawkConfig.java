c7b773c44eb98c464f4b70a787d620adfd6c5588
xuameng <142598141+xuameng@users.noreply.github.com>
2024-11-12 14:21:18 +0800

    Update HawkConfig.java

--- app/src/main/java/com/github/tvbox/osc/util/HawkConfig.java
+++ app/src/main/java/com/github/tvbox/osc/util/HawkConfig.java
@@ -10,14 +10,14 @@ public class HawkConfig {
     public static final String PUSH_TO_PORT = "push_to_port"; // xuameng推送到地址的端口
     public static final String API_URL = "api_url";
     public static final String EPG_URL = "epg_url";
-    public static final String SHOW_PREVIEW = "show_preview";
+    public static final String SHOW_PREVIEW = "show_preview";   //xuameng窗口播放
     public static final String API_HISTORY = "api_history";
     public static final String EPG_HISTORY = "epg_history";
     public static final String HOME_API = "home_api";
     public static final String DEFAULT_PARSE = "parse_default";
     public static final String DEBUG_OPEN = "debug_open";
     public static final String PARSE_WEBVIEW = "parse_webview"; // true 系统 false xwalk
-    public static final String IJK_CODEC = "ijk_codec";
+    public static final String IJK_CODEC = "ijk_codec";   //硬解软解
     public static final String PLAY_TYPE = "play_type";//0 系统 1 ijk 2 exo 10 MXPlayer
     public static final String PLAY_RENDER = "play_render"; //0 texture 2
     public static final String PLAY_SCALE = "play_scale"; //0 texture 2
@@ -32,14 +32,14 @@ public class HawkConfig {
     public static final String LIVE_CONNECT_TIMEOUT = "live_connect_timeout";
     public static final String LIVE_SHOW_NET_SPEED = "live_show_net_speed";
     public static final String LIVE_SHOW_TIME = "live_show_time";
-    public static final String FAST_SEARCH_MODE = "fast_search_mode";
+    public static final String FAST_SEARCH_MODE = "fast_search_mode";   //聚合模式
     public static final String SUBTITLE_TEXT_SIZE = "subtitle_text_size";
     public static final String SUBTITLE_TIME_DELAY = "subtitle_time_delay";
     public static final String SOURCES_FOR_SEARCH = "checked_sources_for_search";
     public static final String HOME_REC_STYLE = "home_rec_style";
     public static final String NOW_DATE = "now_date"; //当前日期
-    public static final String REMOTE_TVBOX = "remote_tvbox_host";
-    public static final String IJK_CACHE_PLAY = "ijk_cache_play";
+    public static final String REMOTE_TVBOX = "remote_tvbox_host";   
+    public static final String IJK_CACHE_PLAY = "ijk_cache_play";       //IJK缓存
 	public static final String HOME_DEFAULT_SHOW = "home_default_show";  //xuameng 启动时直接进直播的开关
     public static boolean hotVodDelete;
 }
