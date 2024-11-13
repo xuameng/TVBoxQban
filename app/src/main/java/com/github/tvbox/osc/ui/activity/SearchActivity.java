b817cd332fb595d1b8ab15362c345ee6e2bafd58
xuameng <142598141+xuameng@users.noreply.github.com>
2024-11-13 20:11:13 +0800

    Update SearchActivity.java

--- app/src/main/java/com/github/tvbox/osc/ui/activity/SearchActivity.java
+++ app/src/main/java/com/github/tvbox/osc/ui/activity/SearchActivity.java
@@ -255,14 +255,15 @@ public class SearchActivity extends BaseActivity {
                 }
             }
         });
-        tvClear.setOnClickListener(new View.OnClickListener() {
+        tvClear.setOnClickListener(new View.OnClickListener() {     
             @Override
             public void onClick(View v) {
                 FastClickCheckUtil.check(v);
                 etSearch.setText("");
 				wordAdapter.setNewData(hots);
                 mGridViewWord.smoothScrollToPosition(0);
-                tHotSearchText.setText("热门搜索");          //xuameng修复热门搜索为空
+                tHotSearchText.setText("热门搜索");          //xuameng修复删除内容后，热门搜索为空
+				cancel();
             }
         });
 
