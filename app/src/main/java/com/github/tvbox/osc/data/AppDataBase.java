f99de9c57f53742ec7faaf97b69dfb7bea6168e1
xuameng <142598141+xuameng@users.noreply.github.com>
2024-11-13 12:41:33 +0800

    Update AppDataBase.java

--- app/src/main/java/com/github/tvbox/osc/data/AppDataBase.java
+++ app/src/main/java/com/github/tvbox/osc/data/AppDataBase.java
@@ -9,6 +9,8 @@ import com.github.tvbox.osc.cache.VodCollect;
 import com.github.tvbox.osc.cache.VodCollectDao;
 import com.github.tvbox.osc.cache.VodRecord;
 import com.github.tvbox.osc.cache.VodRecordDao;
+import com.github.tvbox.osc.cache.SearchDao;   //xuameng搜索历史
+import com.github.tvbox.osc.cache.SearchHistory; //xuameng搜索历史
 
 
 /**
@@ -17,11 +19,13 @@ import com.github.tvbox.osc.cache.VodRecordDao;
  * @author pj567
  * @since 2020/5/15
  */
-@Database(entities = {Cache.class, VodRecord.class, VodCollect.class}, version = 1)
+@Database(entities = {Cache.class, VodRecord.class, VodCollect.class, SearchHistory.class}, version = 3)  //xuameng搜索历史
 public abstract class AppDataBase extends RoomDatabase {
     public abstract CacheDao getCacheDao();
 
     public abstract VodRecordDao getVodRecordDao();
 
     public abstract VodCollectDao getVodCollectDao();
+
+	public abstract SearchDao getSearchDao();  //xuameng搜索历史
 }
