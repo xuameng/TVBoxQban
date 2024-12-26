03aaffd388a42eef1da2225309cdf8e6d2111d07
xuameng <142598141+xuameng@users.noreply.github.com>
2024-12-26 13:29:27 +0800

    Update VideoView.java

--- player/src/main/java/xyz/doikki/videoplayer/player/VideoView.java
+++ player/src/main/java/xyz/doikki/videoplayer/player/VideoView.java
@@ -885,16 +885,12 @@ public class VideoView<P extends AbstractPlayer> extends FrameLayout
 
     @Override
     public void onVideoSizeChanged(int videoWidth, int videoHeight) {
+        mVideoSize[0] = videoWidth;
+        mVideoSize[1] = videoHeight;
+
         if (mRenderView != null) {
             mRenderView.setScaleType(mCurrentScreenScaleType);
             mRenderView.setVideoSize(videoWidth, videoHeight);
-			if (videoWidth.length() > 1 && videoHeight.length() > 1){
-			mVideoSize[0] = videoWidth;
-			mVideoSize[1] = videoHeight;
-			}else{
-			mVideoSize[0] = 0;
-			mVideoSize[1] = 0;
-			}
         }
     }
 
