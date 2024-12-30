f58903396451c9650af1118d2b6a66e576b68068
xuameng <142598141+xuameng@users.noreply.github.com>
2024-12-30 16:32:18 +0800

    Update VodController.java

--- app/src/main/java/com/github/tvbox/osc/player/controller/VodController.java
+++ app/src/main/java/com/github/tvbox/osc/player/controller/VodController.java
@@ -576,9 +576,6 @@ public class VodController extends BaseController {
 				DOUBLE_CLICK_TIME_2 = System.currentTimeMillis();
 				if (!isDisplay || !isAnimation){
 					togglePlay();
-				if(mTvPausexu.getVisibility() == View.VISIBLE ||isPlaying){
-				hideBottom();
-				}
 				}													//xuameng 低菜单播放监听
             }
         });
