53e7aebac83a35ff98904cdf470b3cb65a1c54b7
xuameng <142598141+xuameng@users.noreply.github.com>
2024-12-31 17:49:52 +0800

    Update PlayFragment.java

--- app/src/main/java/com/github/tvbox/osc/ui/fragment/PlayFragment.java
+++ app/src/main/java/com/github/tvbox/osc/ui/fragment/PlayFragment.java
@@ -866,13 +866,6 @@ public class PlayFragment extends BaseLazyFragment {
     }
 
     @Override
-    public static void onRelease() {
-        if (mVideoView != null) {
-            mVideoView.release();
-        }
-    }
-
-    @Override
     public void onHiddenChanged(boolean hidden) {
         if (hidden) {
             if (mVideoView != null) {
