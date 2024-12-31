8e89e7373431a1767ae287e151503d9768965088
xuameng <142598141+xuameng@users.noreply.github.com>
2024-12-31 21:49:19 +0800

    Update PlayFragment.java

--- app/src/main/java/com/github/tvbox/osc/ui/fragment/PlayFragment.java
+++ app/src/main/java/com/github/tvbox/osc/ui/fragment/PlayFragment.java
@@ -353,6 +353,16 @@ public class PlayFragment extends BaseLazyFragment {
         subtitleDialog.show();
     }
 
+	
+
+subtitleDialog.setOnDismissListener(new SubtitleDialog.OnDismissListener() {
+    @Override
+    public void onDismiss(DialogInterface subtitleDialog) {
+        // Dialog关闭时的操作
+        Log.d("Dialog", "Dialog已关闭");
+    }
+});
+
     @SuppressLint("UseCompatLoadingForColorStateLists")
     void setSubtitleViewTextStyle(int style) {
         if (style == 0) {
