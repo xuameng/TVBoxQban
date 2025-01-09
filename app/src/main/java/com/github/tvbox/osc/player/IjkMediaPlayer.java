1704cd9bc7e48d1c56904fe94dc60e3064df1388
xuameng <142598141+xuameng@users.noreply.github.com>
2025-01-09 15:58:32 +0800

    Update IjkMediaPlayer.java

--- app/src/main/java/com/github/tvbox/osc/player/IjkMediaPlayer.java
+++ app/src/main/java/com/github/tvbox/osc/player/IjkMediaPlayer.java
@@ -130,7 +130,7 @@ public class IjkMediaPlayer extends IjkPlayer {
         int subtitleSelected = mMediaPlayer.getSelectedTrack(ITrackInfo.MEDIA_TRACK_TYPE_TIMEDTEXT);
         int audioSelected = mMediaPlayer.getSelectedTrack(ITrackInfo.MEDIA_TRACK_TYPE_AUDIO);
         int index = 0;
-
+        for (IjkTrackInfo info : trackInfo) {
 						String originalString = info.getLanguage();   //xuameng显示字幕类型
 						String ch = "ch";  //xuameng过滤字幕类型里application/字符串
 						String change = "中文";
@@ -138,12 +138,11 @@ public class IjkMediaPlayer extends IjkPlayer {
 						originalString = originalString.replace(ch, change);  //xuameng过滤字幕类型里application/字符串
 						}
 
-        for (IjkTrackInfo info : trackInfo) {
             if (info.getTrackType() == ITrackInfo.MEDIA_TRACK_TYPE_AUDIO) {//音轨信息
 				String trackName = (data.getAudio().size() + 1) + "：" + info.getInfoInline();
                 TrackInfoBean a = new TrackInfoBean();
                 a.name = trackName;
-                a.language = originalString;
+                a.language = info.getLanguage();
                 a.trackId = index;
                 a.selected = index == audioSelected;
                 // 如果需要，还可以检查轨道的描述或标题以获取更多信息
@@ -153,7 +152,7 @@ public class IjkMediaPlayer extends IjkPlayer {
 				String trackName = (data.getSubtitle().size() + 1) + "：" + info.getInfoInline();
                 TrackInfoBean t = new TrackInfoBean();
                 t.name = trackName;
-                t.language = originalString;
+                t.language = info.getLanguage();
                 t.trackId = index;
                 t.selected = index == subtitleSelected;
                 data.addSubtitle(t);
