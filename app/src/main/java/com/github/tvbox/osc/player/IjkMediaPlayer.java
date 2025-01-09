c0ac90dbf6f37ea4d0c68526dd00d5bfe61470e3
xuameng <142598141+xuameng@users.noreply.github.com>
2025-01-09 15:03:21 +0800

    Update IjkMediaPlayer.java

--- app/src/main/java/com/github/tvbox/osc/player/IjkMediaPlayer.java
+++ app/src/main/java/com/github/tvbox/osc/player/IjkMediaPlayer.java
@@ -132,10 +132,10 @@ public class IjkMediaPlayer extends IjkPlayer {
         int index = 0;
         for (IjkTrackInfo info : trackInfo) {
             if (info.getTrackType() == ITrackInfo.MEDIA_TRACK_TYPE_AUDIO) {//音轨信息
-				String trackName = (data.getAudio().size() + 1) + "：" + info.getLanguage() + ", " + info.getInfoInline();
+				String trackName = (data.getAudio().size() + 1) + "：" + info.getInfoInline();
                 TrackInfoBean a = new TrackInfoBean();
                 a.name = trackName;
-    //            a.language = info.getLanguage();
+                a.language = info.getLanguage();
                 a.trackId = index;
                 a.selected = index == audioSelected;
                 // 如果需要，还可以检查轨道的描述或标题以获取更多信息
