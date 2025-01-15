6885d578cbf47420c48b8f41b1e01b2d31983c0e
xuameng <142598141+xuameng@users.noreply.github.com>
2025-01-16 00:36:48 +0800

    Update SurfaceRenderView.java

--- app/src/main/java/com/github/tvbox/osc/player/render/SurfaceRenderView.java
+++ app/src/main/java/com/github/tvbox/osc/player/render/SurfaceRenderView.java
@@ -34,8 +34,8 @@ public class SurfaceRenderView extends SurfaceView implements IRenderView, Surfa
     {
         mMeasureHelper = new MeasureHelper();
         SurfaceHolder surfaceHolder = getHolder();
-        
-        
+        setZOrderOnTop(true);
+        setZOrderMediaOverlay(true);
         surfaceHolder.setFormat(PixelFormat.TRANSPARENT);
             surfaceHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
   
