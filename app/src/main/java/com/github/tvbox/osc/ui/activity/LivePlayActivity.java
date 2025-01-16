db98343f5c1952fff28f6de2a7c1b3a047a28f78
xuameng <142598141+xuameng@users.noreply.github.com>
2025-01-16 11:00:20 +0800

    Update LivePlayActivity.java

--- app/src/main/java/com/github/tvbox/osc/ui/activity/LivePlayActivity.java
+++ app/src/main/java/com/github/tvbox/osc/ui/activity/LivePlayActivity.java
@@ -114,7 +114,7 @@ public class LivePlayActivity extends BaseActivity {
     private TvRecyclerView mLiveChannelView;
     private LiveChannelGroupAdapter liveChannelGroupAdapter;
     private LiveChannelItemAdapter liveChannelItemAdapter;
-	private LiveController controllerxu;
+//	private LiveController controllerxu;
     private long mExitTime = 0; //xuameng返回键退出时间
     private long mExitTimeUp = 0; //xuameng上键间隔时间
     private long mExitTimeDown = 0; //xuameng下键间隔时间
@@ -1960,6 +1960,24 @@ public class LivePlayActivity extends BaseActivity {
 						mLoading.setVisibility(View.GONE); //xuameng 播放音频时的缓冲动画
 					    String width = Integer.toString(mVideoView.getVideoSize()[0]);
 						String height = Integer.toString(mVideoView.getVideoSize()[1]);
+						if (width.length() > 1 && height.length() > 1){
+							LiveController controller = new LiveController(this);
+							controller.setCanChangePosition(false);
+							controller.setEnableInNormal(true);
+							controller.setGestureEnabled(true);
+							controller.setDoubleTapTogglePlayEnabled(false);
+							mVideoView.setVideoController(controller);
+							setVideoControllerXu(null);
+						}else{
+							LiveController controller = new LiveController(this);
+							mVideoView.setVideoController(null);
+							controller.setCanChangePosition(false);
+							controller.setEnableInNormal(true);
+							controller.setGestureEnabled(true);
+							controller.setDoubleTapTogglePlayEnabled(false);
+							setVideoControllerXu(controller);
+						}
+
 						tv_size.setText("[" + width + " X " + height +"]");
 						if (width.length() > 1 && height.length() > 1){
 							listenBuffer = false;  //xuameng音频缓冲
@@ -2131,6 +2149,10 @@ public class LivePlayActivity extends BaseActivity {
     }
 
     public void setVideoControllerXu(@Nullable BaseVideoController mediaController) {
+		mVideoController = mediaController;
+        if (mediaController != null) {
+            mediaController.setMediaPlayer(this);
+        }
 
     }
 
@@ -2654,9 +2676,6 @@ public class LivePlayActivity extends BaseActivity {
 					}else{
 						if (MxuamengMusic.getVisibility() == View.GONE){  //xuameng播放音乐背景
 						MxuamengMusic.setVisibility(View.VISIBLE);
-
-mVideoView.setVideoController(null);
-setVideoControllerXu(controllerxu);
 						}
 						if (isBuffer || isShowlist){
 							if (iv_circle_bg_xu.getVisibility() == View.VISIBLE){  //xuameng音乐播放时图标
