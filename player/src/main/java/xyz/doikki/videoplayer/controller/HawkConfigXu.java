934734176336ac6bd820fcf95696f5d299afc1ab
xuameng <142598141+xuameng@users.noreply.github.com>
2025-01-21 21:08:30 +0800

    Update HawkConfigXu.java

--- player/src/main/java/xyz/doikki/videoplayer/controller/HawkConfigXu.java
+++ player/src/main/java/xyz/doikki/videoplayer/controller/HawkConfigXu.java
@@ -7,4 +7,5 @@ package xyz.doikki.videoplayer.controller;
  */
 public class HawkConfigXu {
     public static final String PLAY_RENDER_XU = "play_render_xu"; //0 texture 2
+	public static boolean intVod = false;  //调节亮度声音
 }
