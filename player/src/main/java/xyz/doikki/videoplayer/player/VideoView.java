5d9bb139700f7de9e6c2cbe63f6b43375f199d31
xuameng <142598141+xuameng@users.noreply.github.com>
2025-01-21 19:20:09 +0800

    Update VideoView.java

--- player/src/main/java/xyz/doikki/videoplayer/player/VideoView.java
+++ player/src/main/java/xyz/doikki/videoplayer/player/VideoView.java
@@ -354,20 +354,20 @@ public class VideoView<P extends AbstractPlayer> extends FrameLayout
      * 继续播放
      */
     public void resume() {
-		int renderType = Hawk.get(HawkConfigXu.PLAY_RENDER, 0);  //xuameng surfaceview判断用
-		int duration = (int) getDuration();
+		int renderType = Hawk.get(HawkConfigXu.PLAY_RENDER_XU, 0);  //xuameng surfaceview判断用
 		String width = Integer.toString(getVideoSize()[0]);
 		String height = Integer.toString(getVideoSize()[1]);
-		if(duration > 130000) {
-			Progress = (int) getCurrentPosition();
-		}
 
 		if (renderType == 1 && width.length() > 1 && height.length() > 1) {
+			int duration = (int) getDuration();
+			if(duration > 130000) {
+			Progress = (int) getCurrentPosition();
+			}
 		initPlayer();
         addDisplay();
 		startPrepare(false);
 		isSurface = true;
-		}
+		}       //xuameng surfaceview判断完
         if (isInPlaybackState()
                 && !mMediaPlayer.isPlaying()) {
             mMediaPlayer.start();
@@ -574,7 +574,7 @@ public class VideoView<P extends AbstractPlayer> extends FrameLayout
             case AbstractPlayer.MEDIA_INFO_RENDERING_START: // 视频/音频开始渲染
                 setPlayState(STATE_PLAYING);
                 mPlayerContainer.setKeepScreenOn(true);
-				if (Progress > 0 && isSurface){
+				if (Progress > 0 && isSurface){   //xuameng surface读取播放进度
 					seekTo(Progress);
 					Progress = 0;
 					isSurface = false;
