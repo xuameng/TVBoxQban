76d57f0055299b2687d26a496a2d484f3ba11f41
xuameng <142598141+xuameng@users.noreply.github.com>
2025-01-22 12:11:41 +0800

    Update DetailActivity.java

--- app/src/main/java/com/github/tvbox/osc/ui/activity/DetailActivity.java
+++ app/src/main/java/com/github/tvbox/osc/ui/activity/DetailActivity.java
@@ -21,7 +21,6 @@ import android.content.ClipboardManager;
 import android.content.ClipData;
 
 import android.view.animation.BounceInterpolator;   //xuameng动画
-import xyz.doikki.videoplayer.controller.HawkConfigXu;  //xuameng surfaceview判断用
 
 import androidx.fragment.app.FragmentContainerView;
 import androidx.lifecycle.Observer;
@@ -156,7 +155,7 @@ public class DetailActivity extends BaseActivity {
         initView();
         initViewModel();
         initData();
-		HawkConfigXu.intVod = true;  //xuameng判断进入播放
+		HawkConfig.intVod = true;  //xuameng判断进入播放
     }
 
     private void initView() {
@@ -1155,7 +1154,7 @@ public class DetailActivity extends BaseActivity {
 		else if (showPreview) {    //xuameng如果显示小窗口播放就释放视频，修复退出还显示暂停图标等图标的BUG
             playFragment.mVideoView.release();
         }
-		HawkConfigXu.intVod = false;  //xuameng判断进入播放
+		HawkConfig.intVod = false;  //xuameng判断进入播放
         super.onBackPressed();
     }
 
