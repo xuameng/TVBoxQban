774a2ee98e6aac1d8e9686164052992b9ffb88b0
xuameng <142598141+xuameng@users.noreply.github.com>
2025-01-22 11:48:37 +0800

    Add files via upload

---
+++ app/src/main/java/xyz/doikki/videoplayer/controller/HawkConfigXu.java
@@ -0,0 +1,11 @@
+package xyz.doikki.videoplayer.controller;
+
+/**
+ * @author pj567
+ * @date :2020/12/23
+ * @description:
+ */
+public class HawkConfigXu {
+    public static final String PLAY_RENDER_XU = "play_render_xu"; //0 texture 2
+	public static boolean intVod = false;  //调节亮度声音
+}
