e56e8c37d191a3fc6816dd7b72560beda9c82c12
xuameng <142598141+xuameng@users.noreply.github.com>
2025-02-14 12:40:21 +0800

    Update VodController.java

--- app/src/main/java/com/github/tvbox/osc/player/controller/VodController.java
+++ app/src/main/java/com/github/tvbox/osc/player/controller/VodController.java
@@ -351,6 +351,7 @@ public class VodController extends BaseController {
 				.memoryPolicy(MemoryPolicy.NO_CACHE, MemoryPolicy.NO_STORE)
 				.networkPolicy(NetworkPolicy.NO_CACHE)
 				.into(MxuamengMusic); // xuameng内容空显示banner
+				mHandler.postDelayed(this, 15000);
 				return;
 				}
 			if (!ApiConfig.get().wallpaper.isEmpty()){
@@ -366,8 +367,21 @@ public class VodController extends BaseController {
 				.memoryPolicy(MemoryPolicy.NO_CACHE, MemoryPolicy.NO_STORE)
 				.networkPolicy(NetworkPolicy.NO_CACHE)
 				.into(MxuamengMusic); // xuameng内容空显示banner
+				mHandler.postDelayed(this, 15000);
 				return;
 				}
+				String Url = "https://api.dzzui.com/api/cosplay";
+				Picasso.get()
+				.load(Url)
+//				.placeholder(R.drawable.xumusic)   //xuameng默认的站位图
+				.noPlaceholder()   //不使用站位图，效果不好
+				.resize(1920,1080)
+				.centerCrop()
+//				.error(R.drawable.xumusic)
+				.config(Bitmap.Config.RGB_565)
+				.memoryPolicy(MemoryPolicy.NO_CACHE, MemoryPolicy.NO_STORE)
+				.networkPolicy(NetworkPolicy.NO_CACHE)
+				.into(MxuamengMusic); // xuameng内容空显示banner
 			}
         mHandler.postDelayed(this, 15000);
        }
