8e885e98c063993b258fbf00b28cb28284d56e9b
xuameng <142598141+xuameng@users.noreply.github.com>
2025-02-15 18:31:32 +0800

    Update PlayFragment.java

--- app/src/main/java/com/github/tvbox/osc/ui/fragment/PlayFragment.java
+++ app/src/main/java/com/github/tvbox/osc/ui/fragment/PlayFragment.java
@@ -337,12 +337,6 @@ public class PlayFragment extends BaseLazyFragment {
             @Override
             public void openLocalFileChooserDialog() {
 				HawkConfig.intSubtitle = true;  //xuameng判断进入本地字幕
-				new Handler().postDelayed(new Runnable() {
-					@Override
-					public void run() {
-					HawkConfig.intSubtitle = false;  //xuameng判断进入本地字幕
-					}
-				}, 800);
                 new ChooserDialog(getActivity())
                         .withFilter(false, false, "srt", "ass", "scc", "stl", "ttml")
                         .withStartFile("/storage/emulated/0/Download")
@@ -358,6 +352,12 @@ public class PlayFragment extends BaseLazyFragment {
             }
         });
         subtitleDialog.show();
+		new Handler().postDelayed(new Runnable() {
+			@Override
+			public void run() {
+			HawkConfig.intSubtitle = false;  //xuameng判断进入本地字幕
+			}
+		}, 800);
     }
 
     @SuppressLint("UseCompatLoadingForColorStateLists")
