5cd63a695904e98d0ef4df762227ea334b5c0396
xuameng <142598141+xuameng@users.noreply.github.com>
2025-02-15 15:13:33 +0800

    Update PlayFragment.java

--- app/src/main/java/com/github/tvbox/osc/ui/fragment/PlayFragment.java
+++ app/src/main/java/com/github/tvbox/osc/ui/fragment/PlayFragment.java
@@ -336,6 +336,13 @@ public class PlayFragment extends BaseLazyFragment {
         subtitleDialog.setLocalFileChooserListener(new SubtitleDialog.LocalFileChooserListener() {
             @Override
             public void openLocalFileChooserDialog() {
+				HawkConfig.intSubtitle = true;  //xuameng判断进入本地字幕
+				new Handler().postDelayed(new Runnable() {
+					@Override
+					public void run() {
+					HawkConfig.intSubtitle = false;  //xuameng判断进入本地字幕
+					}
+				}, 800);
                 new ChooserDialog(getActivity())
                         .withFilter(false, false, "srt", "ass", "scc", "stl", "ttml")
                         .withStartFile("/storage/emulated/0/Download")
