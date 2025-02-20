ff61c80627db342ed6c31b3c222f720a5adc5df9
xuameng <142598141+xuameng@users.noreply.github.com>
2025-02-20 21:10:14 +0800

    Update AndroidMediaPlayer.java

--- app/src/main/java/xyz/doikki/videoplayer/player/AndroidMediaPlayer.java
+++ app/src/main/java/xyz/doikki/videoplayer/player/AndroidMediaPlayer.java
@@ -131,12 +131,15 @@ public class AndroidMediaPlayer extends AbstractPlayer implements MediaPlayer.On
         mMediaPlayer.setOnVideoSizeChangedListener(null);
         stop();
         final MediaPlayer mediaPlayer = mMediaPlayer;
-        mMediaPlayer = null;
+    //xuameng修复空指针    mMediaPlayer = null;
         new Thread() {
             @Override
             public void run() {
                 try {
-                    mediaPlayer.release();
+					if(mediaPlayer != null) {  //xuameng修复空指针 
+                    mediaPlayer.release();  
+					mMediaPlayer = null;  //xuameng修复空指针 
+					}
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
