f5589cd57279eb4c415ae73921f7792979c9f071
xuameng <142598141+xuameng@users.noreply.github.com>
2025-02-22 10:42:51 +0800

    Update VideoView.java

--- app/src/main/java/xyz/doikki/videoplayer/player/VideoView.java
+++ app/src/main/java/xyz/doikki/videoplayer/player/VideoView.java
@@ -351,6 +351,7 @@ public class VideoView<P extends AbstractPlayer> extends FrameLayout
      * 继续播放
      */
     public void resume() {
+		addDisplay();
         if (isInPlaybackState()
                 && !mMediaPlayer.isPlaying()) {
             mMediaPlayer.start();
