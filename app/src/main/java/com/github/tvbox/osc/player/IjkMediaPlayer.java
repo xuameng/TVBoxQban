10d017f325c45d65bdcaaaa458d2181c3c0c5d73
xuameng <142598141+xuameng@users.noreply.github.com>
2025-02-27 18:23:36 +0800

    Update IjkMediaPlayer.java

--- app/src/main/java/com/github/tvbox/osc/player/IjkMediaPlayer.java
+++ app/src/main/java/com/github/tvbox/osc/player/IjkMediaPlayer.java
@@ -5,12 +5,16 @@ import android.text.TextUtils;
 
 import com.github.tvbox.osc.api.ApiConfig;
 import com.github.tvbox.osc.bean.IJKCode;
+import com.github.tvbox.osc.server.ControlManager;
 import com.github.tvbox.osc.util.FileUtils;
 import com.github.tvbox.osc.util.HawkConfig;
+import com.github.tvbox.osc.util.LOG;
 import com.github.tvbox.osc.util.MD5;
 import com.orhanobut.hawk.Hawk;
 
 import java.io.File;
+import java.net.URI;
+import java.net.URLEncoder;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
@@ -48,11 +52,8 @@ public class IjkMediaPlayer extends IjkPlayer {
                 }
             }
         }
+        mMediaPlayer.setOption(tv.danmaku.ijk.media.player.IjkMediaPlayer.OPT_CATEGORY_PLAYER, "max-fps", 60);
 
-        // 在每个数据包之后启用 I/O 上下文的刷新
- //       mMediaPlayer.setOption(tv.danmaku.ijk.media.player.IjkMediaPlayer.OPT_CATEGORY_FORMAT, "flush_packets", 1);        xuameng不上下文的刷新
-        // 当 CPU 处理不过来的时候的丢帧帧数，默认为 0，参数范围是 [-1, 120]
-//        mMediaPlayer.setOption(tv.danmaku.ijk.media.player.IjkMediaPlayer.OPT_CATEGORY_PLAYER, "framedrop", 5);          xuameng不丢帧
         // 设置视频流格式
         mMediaPlayer.setOption(tv.danmaku.ijk.media.player.IjkMediaPlayer.OPT_CATEGORY_PLAYER, "overlay-format", tv.danmaku.ijk.media.player.IjkMediaPlayer.SDL_FCC_RV32);
 
@@ -61,35 +62,63 @@ public class IjkMediaPlayer extends IjkPlayer {
 
         mMediaPlayer.setOption(tv.danmaku.ijk.media.player.IjkMediaPlayer.OPT_CATEGORY_FORMAT, "dns_cache_clear", 1);
         mMediaPlayer.setOption(tv.danmaku.ijk.media.player.IjkMediaPlayer.OPT_CATEGORY_FORMAT, "dns_cache_timeout", -1);
+
+        mMediaPlayer.setOption(tv.danmaku.ijk.media.player.IjkMediaPlayer.OPT_CATEGORY_FORMAT,"safe",0);
+
+        if(Hawk.get(HawkConfig.PLAYER_IS_LIVE)){
+            LOG.i("type-直播");
+            mMediaPlayer.setOption(tv.danmaku.ijk.media.player.IjkMediaPlayer.OPT_CATEGORY_PLAYER, "packet-buffering", 0);
+//            mMediaPlayer.setOption(tv.danmaku.ijk.media.player.IjkMediaPlayer.OPT_CATEGORY_FORMAT, "fflags", "nobuffer"); // 减少协议层缓冲
+        }else{
+            LOG.i("type-点播");
+//            mMediaPlayer.setOption(tv.danmaku.ijk.media.player.IjkMediaPlayer.OPT_CATEGORY_PLAYER, "max_cached_duration", 100);
+            mMediaPlayer.setOption(tv.danmaku.ijk.media.player.IjkMediaPlayer.OPT_CATEGORY_FORMAT, "infbuf", 0);
+            mMediaPlayer.setOption(tv.danmaku.ijk.media.player.IjkMediaPlayer.OPT_CATEGORY_PLAYER, "min-frames", 5);
+        }
     }
 
+    private static final String ITV_TARGET_DOMAIN = "gslbserv.itv.cmvideo.cn";
     @Override
     public void setDataSource(String path, Map<String, String> headers) {
         try {
-            if (path.contains("rtsp") || path.contains("udp") || path.contains("rtp")) {
-                mMediaPlayer.setOption(tv.danmaku.ijk.media.player.IjkMediaPlayer.OPT_CATEGORY_FORMAT, "infbuf", 1);
-                mMediaPlayer.setOption(tv.danmaku.ijk.media.player.IjkMediaPlayer.OPT_CATEGORY_FORMAT, "rtsp_transport", "tcp");
-                mMediaPlayer.setOption(tv.danmaku.ijk.media.player.IjkMediaPlayer.OPT_CATEGORY_FORMAT, "rtsp_flags", "prefer_tcp");
-                mMediaPlayer.setOption(tv.danmaku.ijk.media.player.IjkMediaPlayer.OPT_CATEGORY_FORMAT, "probesize", 512 * 1000);
-                mMediaPlayer.setOption(tv.danmaku.ijk.media.player.IjkMediaPlayer.OPT_CATEGORY_FORMAT, "analyzeduration", 2 * 1000 * 1000);
-            } else if (!TextUtils.isEmpty(path)
-                    && !path.contains(".m3u8")
-                    && (path.contains(".mp4") || path.contains(".mkv") || path.contains(".avi"))) {
-                if (Hawk.get(HawkConfig.IJK_CACHE_PLAY, false)) {
-                    String cachePath = FileUtils.getCachePath() + "/ijkcaches/";
-                    String cacheMapPath = cachePath;
-                    File cacheFile = new File(cachePath);
-                    if (!cacheFile.exists()) cacheFile.mkdirs();
-                    String tmpMd5 = MD5.string2MD5(path);
-                    cachePath += tmpMd5 + ".file";
-                    cacheMapPath += tmpMd5 + ".map";
-                    mMediaPlayer.setOption(tv.danmaku.ijk.media.player.IjkMediaPlayer.OPT_CATEGORY_FORMAT, "cache_file_path", cachePath);
-                    mMediaPlayer.setOption(tv.danmaku.ijk.media.player.IjkMediaPlayer.OPT_CATEGORY_FORMAT, "cache_map_path", cacheMapPath);
-                    mMediaPlayer.setOption(tv.danmaku.ijk.media.player.IjkMediaPlayer.OPT_CATEGORY_FORMAT, "parse_cache_map", 1);
-                    mMediaPlayer.setOption(tv.danmaku.ijk.media.player.IjkMediaPlayer.OPT_CATEGORY_FORMAT, "auto_save_map", 1);
-                    mMediaPlayer.setOption(tv.danmaku.ijk.media.player.IjkMediaPlayer.OPT_CATEGORY_FORMAT, "cache_max_capacity", 60 * 1024 * 1024);
-                    path = "ijkio:cache:ffio:" + path;
-                }
+            switch (getStreamType(path)) {
+                case RTSP_UDP_RTP:
+                    mMediaPlayer.setOption(tv.danmaku.ijk.media.player.IjkMediaPlayer.OPT_CATEGORY_FORMAT, "infbuf", 1);
+                    mMediaPlayer.setOption(tv.danmaku.ijk.media.player.IjkMediaPlayer.OPT_CATEGORY_FORMAT, "rtsp_transport", "tcp");
+                    mMediaPlayer.setOption(tv.danmaku.ijk.media.player.IjkMediaPlayer.OPT_CATEGORY_FORMAT, "rtsp_flags", "prefer_tcp");
+                    mMediaPlayer.setOption(tv.danmaku.ijk.media.player.IjkMediaPlayer.OPT_CATEGORY_FORMAT, "probesize", 512 * 1000);
+                    mMediaPlayer.setOption(tv.danmaku.ijk.media.player.IjkMediaPlayer.OPT_CATEGORY_FORMAT, "analyzeduration", 2 * 1000 * 1000);
+                    break;
+
+                case CACHE_VIDEO:
+                    if (Hawk.get(HawkConfig.IJK_CACHE_PLAY, false)) {
+                        String cachePath = FileUtils.getCachePath() + "/ijkcaches/";
+                        File cacheFile = new File(cachePath);
+                        if (!cacheFile.exists()) cacheFile.mkdirs();
+                        String tmpMd5 = MD5.string2MD5(path);
+                        String cacheFilePath = cachePath + tmpMd5 + ".file";
+                        String cacheMapPath = cachePath + tmpMd5 + ".map";
+
+                        mMediaPlayer.setOption(tv.danmaku.ijk.media.player.IjkMediaPlayer.OPT_CATEGORY_FORMAT, "cache_file_path", cacheFilePath);
+                        mMediaPlayer.setOption(tv.danmaku.ijk.media.player.IjkMediaPlayer.OPT_CATEGORY_FORMAT, "cache_map_path", cacheMapPath);
+                        mMediaPlayer.setOption(tv.danmaku.ijk.media.player.IjkMediaPlayer.OPT_CATEGORY_FORMAT, "parse_cache_map", 1);
+                        mMediaPlayer.setOption(tv.danmaku.ijk.media.player.IjkMediaPlayer.OPT_CATEGORY_FORMAT, "auto_save_map", 1);
+                        mMediaPlayer.setOption(tv.danmaku.ijk.media.player.IjkMediaPlayer.OPT_CATEGORY_FORMAT, "cache_max_capacity", 60 * 1024 * 1024);
+                        path = "ijkio:cache:ffio:" + path;
+                    }
+                    break;
+
+                case M3U8:
+                    // 直播且是ijk的时候自动自动走代理解决DNS
+                    if (Hawk.get(HawkConfig.PLAYER_IS_LIVE, false) ) {
+                        URI uri = new URI(path);
+                        String host = uri.getHost();
+                        if(ITV_TARGET_DOMAIN.equalsIgnoreCase(host))path = ControlManager.get().getAddress(true) + "proxy?go=live&type=m3u8&url="+ URLEncoder.encode(path,"UTF-8");
+                    }
+                    break;
+
+                default:
+                    break;
             }
         } catch (Exception e) {
             e.printStackTrace();
@@ -97,6 +126,33 @@ public class IjkMediaPlayer extends IjkPlayer {
         setDataSourceHeader(headers);
         mMediaPlayer.setOption(tv.danmaku.ijk.media.player.IjkMediaPlayer.OPT_CATEGORY_FORMAT, "protocol_whitelist", "ijkio,ffio,async,cache,crypto,file,dash,http,https,ijkhttphook,ijkinject,ijklivehook,ijklongurl,ijksegment,ijktcphook,pipe,rtp,tcp,tls,udp,ijkurlhook,data");
         super.setDataSource(path, null);
+    }
+
+    /**
+     * 解析 URL
+     */
+    private static final int RTSP_UDP_RTP = 1;
+    private static final int CACHE_VIDEO = 2;
+    private static final int M3U8 = 3;
+    private static final int OTHER = 0;
+
+    private int getStreamType(String path) {
+        if (TextUtils.isEmpty(path)) {
+            return OTHER;
+        }
+        // 低成本检查 RTSP/UDP/RTP 类型
+        String lowerPath = path.toLowerCase();
+        if (lowerPath.startsWith("rtsp://") || lowerPath.startsWith("udp://") || lowerPath.startsWith("rtp://")) {
+            return RTSP_UDP_RTP;
+        }
+        String cleanUrl = path.split("\\?")[0];
+        if (cleanUrl.endsWith(".m3u8")) {
+            return M3U8;
+        }
+        if (cleanUrl.endsWith(".mp4") || cleanUrl.endsWith(".mkv") || cleanUrl.endsWith(".avi")) {
+            return CACHE_VIDEO;
+        }
+        return OTHER;
     }
 
     private void setDataSourceHeader(Map<String, String> headers) {
@@ -132,21 +188,19 @@ public class IjkMediaPlayer extends IjkPlayer {
         int index = 0;
         for (IjkTrackInfo info : trackInfo) {
             if (info.getTrackType() == ITrackInfo.MEDIA_TRACK_TYPE_AUDIO) {//音轨信息
-				String trackName = (data.getAudio().size() + 1) + "：" + info.getInfoInline();
                 TrackInfoBean a = new TrackInfoBean();
-                a.name = trackName;
-                a.language = "";
-                a.trackId = index;
+                a.name = info.getInfoInline();
+                a.language = info.getLanguage();
+                a.index = index;
                 a.selected = index == audioSelected;
                 // 如果需要，还可以检查轨道的描述或标题以获取更多信息
                 data.addAudio(a);
             }
             if (info.getTrackType() == ITrackInfo.MEDIA_TRACK_TYPE_TIMEDTEXT) {//内置字幕
-				String trackName = "";
                 TrackInfoBean t = new TrackInfoBean();
-                t.name = trackName;
-                t.language = (data.getSubtitle().size() + 1) + "：" + info.getInfoInline();
-                t.trackId = index;
+                t.name = info.getInfoInline();
+                t.language = info.getLanguage();
+                t.index = index;
                 t.selected = index == subtitleSelected;
                 data.addSubtitle(t);
             }
