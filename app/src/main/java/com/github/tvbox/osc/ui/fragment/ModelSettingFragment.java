a188e1a7f1110e5f839ff7c38fc27cf28ebb08b9
xuameng <142598141+xuameng@users.noreply.github.com>
2025-03-06 20:57:40 +0800

    Update ModelSettingFragment.java

--- app/src/main/java/com/github/tvbox/osc/ui/fragment/ModelSettingFragment.java
+++ app/src/main/java/com/github/tvbox/osc/ui/fragment/ModelSettingFragment.java
@@ -752,8 +752,12 @@ public class ModelSettingFragment extends BaseLazyFragment {
 		boolean rec_style1=Hawk.get(HawkConfig.HOME_REC_STYLE, false);
 		boolean rec_style2 = rec_style == rec_style1;
 		if (!rec_style2){   //xuameng首页单行判断
-			DefaultConfig.restartApp();
-			return;
+           Intent intent =new Intent(mContext, HomeActivity.class);
+           intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+           Bundle bundle = new Bundle();
+           bundle.putBoolean("useCache", true);
+           intent.putExtras(bundle);
+           startActivity(intent);
 		}
         super.onDestroyView();
         SettingActivity.callback = null;		
