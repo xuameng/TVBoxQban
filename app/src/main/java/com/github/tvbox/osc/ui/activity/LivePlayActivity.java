d0543cf9ac24a7bf7d7ee994483d3faf7c3bfda6
xuameng <142598141+xuameng@users.noreply.github.com>
2025-03-12 21:01:51 +0800

    Update LivePlayActivity.java

--- app/src/main/java/com/github/tvbox/osc/ui/activity/LivePlayActivity.java
+++ app/src/main/java/com/github/tvbox/osc/ui/activity/LivePlayActivity.java
@@ -2483,9 +2483,15 @@ public class LivePlayActivity extends BaseActivity {
         if (list.isEmpty()) {
             if(Hawk.get(HawkConfig.LIVE_GROUP_INDEX, 0)!=0){
                 Hawk.put(HawkConfig.LIVE_GROUP_INDEX, 0);
+                Toast.makeText(App.getInstance(), "聚汇影视提示您：频道列表为空！", Toast.LENGTH_SHORT).show();
+                finish();
+				return;
+			}else{
+                Toast.makeText(App.getInstance(), "聚汇影视提示您：频道列表为空！", Toast.LENGTH_SHORT).show();
+                finish();
 				return;
 			}
-            }
+        }
         
 
         if (list.size() == 1 && list.get(0).getGroupName().startsWith("http://127.0.0.1")) {
