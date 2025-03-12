3999d11110b61059aef5b67ccbc287fc9ea3867f
xuameng <142598141+xuameng@users.noreply.github.com>
2025-03-12 13:16:04 +0800

    Update GridAdapter.java

--- app/src/main/java/com/github/tvbox/osc/ui/adapter/GridAdapter.java
+++ app/src/main/java/com/github/tvbox/osc/ui/adapter/GridAdapter.java
@@ -12,9 +12,8 @@ import com.github.tvbox.osc.bean.Movie;
 import com.github.tvbox.osc.picasso.RoundTransformation;
 import com.github.tvbox.osc.util.DefaultConfig;
 import com.github.tvbox.osc.util.MD5;
-import com.squareup.picasso.MemoryPolicy;
 import com.squareup.picasso.Picasso;
-import com.github.tvbox.osc.util.Base64Img;   //xuamengBASE64图片
+import com.github.tvbox.osc.util.ImgUtil;   //xuamengBASE64图片
 
 import java.util.ArrayList;
 
@@ -42,9 +41,9 @@ public class GridAdapter extends BaseQuickAdapter<Movie.Video, BaseViewHolder> {
             //由于部分电视机使用glide报错
             if (!TextUtils.isEmpty(item.pic)) {
                 item.pic=item.pic.trim();
-                if(Base64Img.isBase64Image(item.pic)){
+                if(ImgUtil.isBase64Image(item.pic)){
                     // 如果是 Base64 图片，解码并设置
-                    ivThumb.setImageBitmap(Base64Img.decodeBase64ToBitmap(item.pic));
+                    ivThumb.setImageBitmap(ImgUtil.decodeBase64ToBitmap(item.pic));
                 }else {
                     Picasso.get()
                             .load(DefaultConfig.checkReplaceProxy(item.pic))
@@ -54,11 +53,12 @@ public class GridAdapter extends BaseQuickAdapter<Movie.Video, BaseViewHolder> {
                                     .roundRadius(AutoSizeUtils.mm2px(mContext, 10), RoundTransformation.RoundType.ALL))
                             .placeholder(R.drawable.img_loading_placeholder)
                             .noFade()
-                            .error(R.drawable.img_loading_placeholder)
+                            .error(ImgUtil.createTextDrawable(item.name))
                             .into(ivThumb);
                 }
             } else {
-                ivThumb.setImageResource(R.drawable.img_loading_placeholder);
+                //ivThumb.setImageResource(R.drawable.img_loading_placeholder);
+				ivThumb.setImageDrawable(ImgUtil.createTextDrawable(item.name));
             }
             return;
         }
@@ -98,9 +98,9 @@ public class GridAdapter extends BaseQuickAdapter<Movie.Video, BaseViewHolder> {
         //由于部分电视机使用glide报错
         if (!TextUtils.isEmpty(item.pic)) {
             item.pic=item.pic.trim();
-            if(Base64Img.isBase64Image(item.pic)){
+            if(ImgUtil.isBase64Image(item.pic)){
                 // 如果是 Base64 图片，解码并设置
-                ivThumb.setImageBitmap(Base64Img.decodeBase64ToBitmap(item.pic));
+                ivThumb.setImageBitmap(ImgUtil.decodeBase64ToBitmap(item.pic));
             }else {
                 Picasso.get()
                         .load(DefaultConfig.checkReplaceProxy(item.pic))
@@ -110,11 +110,12 @@ public class GridAdapter extends BaseQuickAdapter<Movie.Video, BaseViewHolder> {
                                 .roundRadius(AutoSizeUtils.mm2px(mContext, 10), RoundTransformation.RoundType.ALL))
                         .placeholder(R.drawable.img_loading_placeholder)
                         .noFade()
-                        .error(R.drawable.img_loading_placeholder)
+                        .error(ImgUtil.createTextDrawable(item.name))
                         .into(ivThumb);
             }
         } else {
-            ivThumb.setImageResource(R.drawable.img_loading_placeholder);
+            //ivThumb.setImageResource(R.drawable.img_loading_placeholder);
+			ivThumb.setImageDrawable(ImgUtil.createTextDrawable(item.name));
         }
     }
 }
