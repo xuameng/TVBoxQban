573e2d094cec745851b478d6fcefce488ec129b0
xuameng <142598141+xuameng@users.noreply.github.com>
2025-03-14 12:51:40 +0800

    Update FastSearchActivity.java

--- app/src/main/java/com/github/tvbox/osc/ui/activity/FastSearchActivity.java
+++ app/src/main/java/com/github/tvbox/osc/ui/activity/FastSearchActivity.java
@@ -353,6 +353,26 @@ public class FastSearchActivity extends BaseActivity {
     }
 
     private void search(String title) {
+				List<SourceBean> searchRequestList = new ArrayList<>();  //xuameng修复不选择搜索源还进行搜索，还显示搜索动画
+				searchRequestList.addAll(ApiConfig.get().getSourceBeanList());
+				SourceBean home = ApiConfig.get().getHomeSourceBean();
+				searchRequestList.remove(home);
+				searchRequestList.add(0, home);
+				ArrayList<String> siteKey = new ArrayList<>();
+				for (SourceBean bean : searchRequestList) {
+					if (!bean.isSearchable()) {
+						continue;
+					}
+					if (mCheckSources != null && !mCheckSources.containsKey(bean.getKey())) {
+						continue;
+					}
+					siteKey.add(bean.getKey());
+					allRunCount.incrementAndGet();
+				}
+				if (siteKey.size() <= 0) {
+					Toast.makeText(FastSearchActivity.this, "没有指定搜索源", Toast.LENGTH_SHORT).show();
+					return;
+				}    //xuameng修复不选择搜索源还进行搜索，还显示搜索动画完 
         cancel();
         showLoading();
         this.searchTitle = title;
@@ -411,10 +431,6 @@ public class FastSearchActivity extends BaseActivity {
             siteKey.add(bean.getKey());
             this.spNames.put(bean.getName(), bean.getKey());
             allRunCount.incrementAndGet();
-			if (siteKey.size() <= 0) {
-				Toast.makeText(mContext, "没有指定搜索源", Toast.LENGTH_SHORT).show();
-				return;
-			}    //xuameng修复不选择搜索源还进行搜索，还显示搜索动画完 
         }
 
         for (String key : siteKey) {
