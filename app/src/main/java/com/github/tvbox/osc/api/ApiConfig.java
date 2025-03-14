87224dbc6a6e60adf409575aff68af0236a45dee
xuameng <142598141+xuameng@users.noreply.github.com>
2025-03-14 11:19:36 +0800

    Update ApiConfig.java

--- app/src/main/java/com/github/tvbox/osc/api/ApiConfig.java
+++ app/src/main/java/com/github/tvbox/osc/api/ApiConfig.java
@@ -185,11 +185,8 @@ public class ApiConfig {
                     }
                 }else {
                     OkGo.<String>get(liveApiConfigUrl)
-												    .setConnectTimeout(5000) // 超时设置
                             .headers("User-Agent", userAgent)
                             .headers("Accept", requestAccept)
-
-							//.setRetryCount(2); // 重试次数
                             .execute(new AbsCallback<String>() {
                                 @Override
                                 public void onSuccess(Response<String> response) {
