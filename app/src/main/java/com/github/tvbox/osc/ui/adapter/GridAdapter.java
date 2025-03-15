d13777744e6ee5a572c81f160253e6dc04a9861a
xuameng <142598141+xuameng@users.noreply.github.com>
2025-03-15 11:37:53 +0800

    Update GridAdapter.java

--- app/src/main/java/com/github/tvbox/osc/ui/adapter/GridAdapter.java
+++ app/src/main/java/com/github/tvbox/osc/ui/adapter/GridAdapter.java
@@ -115,8 +115,8 @@ public class GridAdapter extends BaseQuickAdapter<Movie.Video, BaseViewHolder> {
                         .into(ivThumb);
             }
         } else {
-            ivThumb.setImageResource(R.drawable.img_loading_placeholder);
-			//ivThumb.setImageDrawable(ImgUtil.createTextDrawable(item.name));
+          //  ivThumb.setImageResource(R.drawable.img_loading_placeholder);
+			ivThumb.setImageDrawable(ImgUtil.createTextDrawable(item.name));
         }
     }
 }
