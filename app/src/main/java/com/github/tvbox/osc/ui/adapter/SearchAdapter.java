52d63f644aacd9b2adc07fd2f7472698b24b565f
xuameng <142598141+xuameng@users.noreply.github.com>
2025-03-19 22:01:44 +0800

    Update SearchAdapter.java

--- app/src/main/java/com/github/tvbox/osc/ui/adapter/SearchAdapter.java
+++ app/src/main/java/com/github/tvbox/osc/ui/adapter/SearchAdapter.java
@@ -13,7 +13,7 @@ import com.github.tvbox.osc.util.HawkConfig;
 import com.github.tvbox.osc.util.MD5;
 import com.orhanobut.hawk.Hawk;
 import com.squareup.picasso.Picasso;
-import com.github.tvbox.osc.util.ImgUtil;   //xuamengBASE64图片
+import com.github.tvbox.osc.util.ImgUtilXu;   //xuamengBASE64图片
 
 import java.util.ArrayList;
 
@@ -36,7 +36,7 @@ public class SearchAdapter extends BaseQuickAdapter<Movie.Video, BaseViewHolder>
             if (item.note != null && !item.note.isEmpty()) {
                 helper.setText(R.id.tvNote, item.note);
             }
-            ImageView ivThumbXu = helper.getView(R.id.ivThumbXu);
+            ImageView ivThumb = helper.getView(R.id.ivThumb);
             if (!TextUtils.isEmpty(item.pic)) {
                 Picasso.get()
                         .load(item.pic)
@@ -47,11 +47,11 @@ public class SearchAdapter extends BaseQuickAdapter<Movie.Video, BaseViewHolder>
                         .placeholder(R.drawable.img_loading_placeholder)
                         .noFade()
                     //    .error(R.drawable.img_loading_placeholder)
-						.error(ImgUtil.createTextDrawable(item.name))
-                        .into(ivThumbXu);
+						.error(ImgUtilXu.createTextDrawable(item.name))
+                        .into(ivThumb);
             } else {
-             //   ivThumbXu.setImageResource(R.drawable.img_loading_placeholder);
-				ivThumbXu.setImageDrawable(ImgUtil.createTextDrawable(item.name));
+             //   ivThumb.setImageResource(R.drawable.img_loading_placeholder);
+				ivThumb.setImageDrawable(ImgUtilXu.createTextDrawable(item.name));
             }
         }
     }
