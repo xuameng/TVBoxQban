68a3ddf9034b9a6314fa8c90f6718c76fffc7b32
xuameng <142598141+xuameng@users.noreply.github.com>
2025-03-19 13:44:38 +0800

    Update CollectAdapter.java

--- app/src/main/java/com/github/tvbox/osc/ui/adapter/CollectAdapter.java
+++ app/src/main/java/com/github/tvbox/osc/ui/adapter/CollectAdapter.java
@@ -52,7 +52,7 @@ public class CollectAdapter extends BaseQuickAdapter<VodCollect, BaseViewHolder>
             Picasso.get()
                     .load(DefaultConfig.checkReplaceProxy(item.pic))
                     .transform(new RoundTransformation(MD5.string2MD5(item.pic))
-                  //          .centerCorp(true)
+                            .centerCorp(true)
                             .override(AutoSizeUtils.mm2px(mContext, 300), AutoSizeUtils.mm2px(mContext, 400))
                             .roundRadius(AutoSizeUtils.mm2px(mContext, 10), RoundTransformation.RoundType.ALL))
                     .placeholder(R.drawable.img_loading_placeholder)
