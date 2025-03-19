5ba0607148a41e54e370ed61607462ca02e8bc16
xuameng <142598141+xuameng@users.noreply.github.com>
2025-03-19 12:25:19 +0800

    Update CollectActivity.java

--- app/src/main/java/com/github/tvbox/osc/ui/activity/CollectActivity.java
+++ app/src/main/java/com/github/tvbox/osc/ui/activity/CollectActivity.java
@@ -94,14 +94,12 @@ public class CollectActivity extends BaseActivity {
         mGridView.setOnItemListener(new TvRecyclerView.OnItemListener() {
             @Override
             public void onItemPreSelected(TvRecyclerView parent, View itemView, int position) {
-                itemView.animate().scaleX(1.0f).scaleY(1.0f).setDuration(300).setInterpolator(new BounceInterpolator()).start();
-				itemView.notifyDataSetChanged();
+               
             }
 
             @Override
             public void onItemSelected(TvRecyclerView parent, View itemView, int position) {
-                itemView.animate().scaleX(1.05f).scaleY(1.05f).setDuration(300).setInterpolator(new BounceInterpolator()).start();
-				itemView.notifyDataSetChanged();
+               
             }
 
             @Override
