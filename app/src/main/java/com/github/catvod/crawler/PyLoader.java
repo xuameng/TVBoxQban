a4dc97897ad9ab9fe6be748579015fa7b04be799
xuameng <142598141+xuameng@users.noreply.github.com>
2025-03-25 15:06:23 +0800

    Update PyLoader.java

--- app/src/main/java/com/github/catvod/crawler/PyLoader.java
+++ app/src/main/java/com/github/catvod/crawler/PyLoader.java
@@ -1,35 +1,29 @@
 package com.github.catvod.crawler;
 
-import android.util.Log;
-
 import com.github.tvbox.osc.base.App;
 import com.undcover.freedom.pyramid.PythonLoader;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 public class PyLoader {
     private final PythonLoader pythonLoader = PythonLoader.getInstance().setApplication(App.getInstance());
-    private static final ConcurrentHashMap<String, Spider> spiders = new ConcurrentHashMap<>();
+    private static ConcurrentHashMap<String, Spider> spiders = new ConcurrentHashMap<>();
 
     private String lastConfig = null; // 记录上次的配置
-    public void clear() {
-        spiders.clear();
-    }
+
     public void setConfig(String jsonStr) {
         if (jsonStr != null && !jsonStr.equals(lastConfig)) {
-            Log.i("PyLoader","echo-setConfig 初始化json ");
             pythonLoader.setConfig(jsonStr);
             lastConfig = jsonStr;
         }
     }
     public Spider getSpider(String key, String cls, String ext) {
-        if (spiders.containsKey(key)){
-            Log.i("PyLoader","echo-getSpider spider缓存: " + key);
+        if (spiders.containsKey(key))
+//            Log.i("PyLoader", "echo-getSpider spider缓存: " + key);
+//            LOG.i("");
             return spiders.get(key);
-        }
         try {
             Spider sp = pythonLoader.getSpider(key, getPyUrl(cls,ext));
             spiders.put(key, sp);
-            Log.i("PyLoader","echo-getSpider 加载spider: " + key);
             return sp;
         } catch (Throwable th) {
             th.printStackTrace();
@@ -37,12 +31,12 @@ public class PyLoader {
         return new SpiderNull();
     }
 
-    public Object[] proxyInvoke(Map<String,String> params,String key,String api,String ext) {
+    public Object[] proxyInvoke(String key,String url,Map<String,String> params) {
         try {
             String doStr = params.get("do");
             assert doStr != null;
             if (doStr.equals("ck") || doStr.equals("live"))return pythonLoader.proxyLocal("", "", params);
-            return (Object[]) pythonLoader.proxyLocal(key, getPyUrl(api,ext), params);
+            return (Object[]) pythonLoader.proxyLocal(key, url, params);
         } catch (Throwable th) {
             th.printStackTrace();
         }
