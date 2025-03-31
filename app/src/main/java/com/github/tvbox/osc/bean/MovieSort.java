9f011433bf9fb25e6920f83b98af80dc0093e331
xuameng <142598141+xuameng@users.noreply.github.com>
2025-03-31 18:34:32 +0800

    Update MovieSort.java

--- app/src/main/java/com/github/tvbox/osc/bean/MovieSort.java
+++ app/src/main/java/com/github/tvbox/osc/bean/MovieSort.java
@@ -43,6 +43,9 @@ public class MovieSort implements Serializable {
         }
 
         public int filterSelectCount() {
+            if (filterSelect == null) {
+                return 0;
+            }
             int count = 0;
             for (String filter : filterSelect.values()) {
                 if (filter != null && !filter.isEmpty()) {
