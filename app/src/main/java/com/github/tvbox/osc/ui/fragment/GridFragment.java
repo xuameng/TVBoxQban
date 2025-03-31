dfe0903f468ddc1de0b2cca7e81db20331106a73
xuameng <142598141+xuameng@users.noreply.github.com>
2025-03-31 21:32:02 +0800

    Update GridFragment.java

--- app/src/main/java/com/github/tvbox/osc/ui/fragment/GridFragment.java
+++ app/src/main/java/com/github/tvbox/osc/ui/fragment/GridFragment.java
@@ -325,15 +325,15 @@ public class GridFragment extends BaseLazyFragment {
     public void showFilter() {
         if (!sortData.filters.isEmpty() && gridFilterDialog == null) {
             gridFilterDialog = new GridFilterDialog(mContext);
-            gridFilterDialog.setData(sortData);
-            gridFilterDialog.setOnDismiss(new GridFilterDialog.Callback() {
-                @Override
-                public void change() {
-                    page = 1;
-                    initData();
-                }
-            });
-//            setFilterDialogData();
+//            gridFilterDialog.setData(sortData);
+//            gridFilterDialog.setOnDismiss(new GridFilterDialog.Callback() {
+//                @Override
+//                public void change() {
+//                    page = 1;
+//                    initData();
+//                }
+//            });
+            setFilterDialogData();
         }
         if (gridFilterDialog != null)
             gridFilterDialog.show();
