853a282ee6d7f82cd84f17031c0eb5d5de5e84cf
xuameng <142598141+xuameng@users.noreply.github.com>
2025-04-07 21:21:48 +0800

    Update DetailActivity.java

--- app/src/main/java/com/github/tvbox/osc/ui/activity/DetailActivity.java
+++ app/src/main/java/com/github/tvbox/osc/ui/activity/DetailActivity.java
@@ -510,7 +510,7 @@ public class DetailActivity extends BaseActivity {
                     VodInfo.VodSeriesFlag flag = vodInfo.seriesFlags.get(position);
                     flag.selected = true;
                     // clean pre flag select status
-                    if (vodInfo.seriesMap.get(vodInfo.playFlag).size() > playFlagXu) {
+                    if (vodInfo.seriesMap.get(vodInfo.playFlag).size() > playFlagXu.size()) {
                         vodInfo.seriesMap.get(vodInfo.playFlag).get(vodInfo.playIndex).selected = false;
                     }
 					playFlagXu = vodInfo.playFlag;
@@ -675,11 +675,11 @@ public class DetailActivity extends BaseActivity {
 
     @SuppressLint("NotifyDataSetChanged")
     void refreshList() {
-        if (vodInfo.seriesMap.get(vodInfo.playFlag).size() < playFlagXu) {
+        if (vodInfo.seriesMap.get(vodInfo.playFlag).size() < playFlagXu.size()) {
   //          vodInfo.playIndex = 0;
         }
 
-        if (vodInfo.seriesMap.get(vodInfo.playFlag) != null && vodInfo.seriesMap.get(vodInfo.playFlag).size() >= playFlagXu) {
+        if (vodInfo.seriesMap.get(vodInfo.playFlag) != null && vodInfo.seriesMap.get(vodInfo.playFlag).size() >= playFlagXu.size()) {
             boolean canSelect = true;
             for (int j = 0; j < vodInfo.seriesMap.get(vodInfo.playFlag).size(); j++) {
                 if(vodInfo.seriesMap.get(vodInfo.playFlag).get(j).selected){
@@ -713,7 +713,7 @@ public class DetailActivity extends BaseActivity {
         seriesAdapter.setNewData(vodInfo.seriesMap.get(vodInfo.playFlag));
 
         setSeriesGroupOptions();
-        if (vodInfo.seriesMap.get(vodInfo.playFlag).size() < playFlagXu) {
+        if (vodInfo.seriesMap.get(vodInfo.playFlag).size() < playFlagXu.size()) {
  return;
         }
         mGridView.postDelayed(new Runnable() {
