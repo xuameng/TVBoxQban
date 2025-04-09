83996a55f90cab631274a3e5814ede93b5e6f843
xuameng <142598141+xuameng@users.noreply.github.com>
2025-04-09 15:53:25 +0800

    Update HistoryAdapter.java

--- app/src/main/java/com/github/tvbox/osc/ui/adapter/HistoryAdapter.java
+++ app/src/main/java/com/github/tvbox/osc/ui/adapter/HistoryAdapter.java
@@ -88,7 +88,7 @@ public class HistoryAdapter extends BaseQuickAdapter<VodInfo, BaseViewHolder> {
                     .load(DefaultConfig.checkReplaceProxy(item.pic))
                     .transform(new RoundTransformation(MD5.string2MD5(item.pic))
                             .centerCorp(true)
-                            .override(AutoSizeUtils.mm2px(mContext, 300), AutoSizeUtils.mm2px(mContext, 400))
+                            .override(AutoSizeUtils.mm2px(mContext, ImgUtil.defaultWidth), AutoSizeUtils.mm2px(mContext, ImgUtil.defaultHeight))
                             .roundRadius(AutoSizeUtils.mm2px(mContext, 10), RoundTransformation.RoundType.ALL))
                     .placeholder(R.drawable.img_loading_placeholder)
                     .noFade()
