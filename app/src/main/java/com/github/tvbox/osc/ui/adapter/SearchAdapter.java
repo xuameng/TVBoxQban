33861574c46476bf91cf715035fe43a4cbb50bff
xuameng <142598141+xuameng@users.noreply.github.com>
2025-04-09 16:09:38 +0800

    Update SearchAdapter.java

--- app/src/main/java/com/github/tvbox/osc/ui/adapter/SearchAdapter.java
+++ app/src/main/java/com/github/tvbox/osc/ui/adapter/SearchAdapter.java
@@ -14,6 +14,7 @@ import com.github.tvbox.osc.util.MD5;
 import com.orhanobut.hawk.Hawk;
 import com.squareup.picasso.Picasso;
 import com.github.tvbox.osc.util.ImgUtilXu;   //xuamengBASE64图片
+import com.github.tvbox.osc.util.ImgUtil;   //xuamengBASE64图片
 
 import java.util.ArrayList;
 
