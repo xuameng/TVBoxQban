a35544b72346eb058c3eac03e8d7fb65958b3580
xuameng <142598141+xuameng@users.noreply.github.com>
2025-04-10 14:07:48 +0800

    Update HawkConfig.java

--- app/src/main/java/com/github/tvbox/osc/util/HawkConfig.java
+++ app/src/main/java/com/github/tvbox/osc/util/HawkConfig.java
@@ -57,7 +57,6 @@ public class HawkConfig {
 	public static boolean intSYSplayer = false;  //xuameng是否进入系统播放器
 	public static boolean intVod = false;  //xuameng判断是否进入VOD界面
 	public static boolean ISrestore = false;  //xuameng判断是否进行恢复操作
-	public static boolean LIVEerror = false;  //xuameng判断直播文件读取成功
 
     public static boolean hotVodDelete;
 }
