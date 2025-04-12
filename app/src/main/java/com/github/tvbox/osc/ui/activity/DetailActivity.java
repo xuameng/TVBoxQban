84bfeecb5373e38ff2552ed889a865a9b0d86d39
xuameng <142598141+xuameng@users.noreply.github.com>
2025-04-12 15:00:50 +0800

    Update DetailActivity.java

--- app/src/main/java/com/github/tvbox/osc/ui/activity/DetailActivity.java
+++ app/src/main/java/com/github/tvbox/osc/ui/activity/DetailActivity.java
@@ -93,6 +93,7 @@ import android.text.TextPaint;
 import androidx.annotation.NonNull;
 import android.graphics.Typeface;
 import androidx.recyclerview.widget.RecyclerView;
+import com.github.tvbox.osc.util.ImgUtil;   //xuameng base64图片
 
 /**
  * @author pj567
@@ -838,14 +839,16 @@ public class DetailActivity extends BaseActivity {
                                 .load(DefaultConfig.checkReplaceProxy(mVideo.pic))
                                 .transform(new RoundTransformation(MD5.string2MD5(mVideo.pic))
                                         .centerCorp(true)
-                                        .override(AutoSizeUtils.mm2px(mContext, 300), AutoSizeUtils.mm2px(mContext, 400))
+                                        .override(AutoSizeUtils.mm2px(mContext, ImgUtil.defaultWidth), AutoSizeUtils.mm2px(mContext, ImgUtil.defaultHeight))
                                         .roundRadius(AutoSizeUtils.mm2px(mContext, 10), RoundTransformation.RoundType.ALL))
                                 .placeholder(R.drawable.img_loading_placeholder)
                                 .noFade()
-                                .error(R.drawable.img_loading_placeholder)
+                             //   .error(R.drawable.img_loading_placeholder)
+						        .error(ImgUtil.createTextDrawable(mVideo.pic))
                                 .into(ivThumb);
                     } else {
-                        ivThumb.setImageResource(R.drawable.img_loading_placeholder);
+                     //   ivThumb.setImageResource(R.drawable.img_loading_placeholder);
+						ivThumb.setImageDrawable(ImgUtil.createTextDrawable(mVideo.pic));
                     }
 
                     if (vodInfo.seriesMap != null && vodInfo.seriesMap.size() > 0) {
