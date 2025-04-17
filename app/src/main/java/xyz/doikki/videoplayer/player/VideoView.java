f16f740f092ebe9f150e334e0fef5fd83e7f7b39
xuameng <142598141+xuameng@users.noreply.github.com>
2025-04-17 15:35:38 +0800

    Update VideoView.java

--- app/src/main/java/xyz/doikki/videoplayer/player/VideoView.java
+++ app/src/main/java/xyz/doikki/videoplayer/player/VideoView.java
@@ -951,7 +951,6 @@ public class VideoView<P extends AbstractPlayer> extends FrameLayout
             LayoutParams params = new LayoutParams(
                     ViewGroup.LayoutParams.MATCH_PARENT,
                     ViewGroup.LayoutParams.MATCH_PARENT);
-ViewGroup.setBackgroundResource(R.drawable.xubackground);
             mPlayerContainer.addView(mVideoController, params);
         }
     }
