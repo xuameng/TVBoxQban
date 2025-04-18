f25c09e4ff4980d41cf08f4cfd5397afceb4ac43
xuameng <142598141+xuameng@users.noreply.github.com>
2025-04-18 13:57:32 +0800

    Update GridAdapter.java

--- app/src/main/java/com/github/tvbox/osc/ui/adapter/GridAdapter.java
+++ app/src/main/java/com/github/tvbox/osc/ui/adapter/GridAdapter.java
@@ -29,32 +29,18 @@ import me.jessyan.autosize.utils.AutoSizeUtils;
  */
 public class GridAdapter extends BaseQuickAdapter<Movie.Video, BaseViewHolder> {
     private boolean mShowList ;
-     private int defaultWidth = 350;
-     private final Style style; // 动态风格，传入时调整图片宽高比
-
-    /**
-     * style 数据结构：ratio 指定宽高比（宽 / 高），type 表示风格（例如 rect、list）
-     */
-    public static class Style {
-        public float ratio;
-        public String type;
-
-        public Style(float ratio, String type) {
-            this.ratio = ratio;
-            this.type = type;
-        }
-    }
+	private int defaultWidth;
+    public ImgUtil.Style style; // 动态风格，传入时调整图片宽高比
 
     /**
      * 如果 style 传 null，则采用 item_grid.xml 中的默认尺寸
      */
-    public GridAdapter(boolean showList, Style style) {
+    public GridAdapter(boolean showList, ImgUtil.Style style) {
         super( showList ? R.layout.item_list:R.layout.item_grid, new ArrayList<>());
         this.mShowList = showList;
         if(style!=null ){
             if(style.type.equals("list"))this.mShowList=true;
-            if(style.ratio<1)this.defaultWidth=300;
-            if(style.ratio>1.7)this.defaultWidth=400;
+			this.defaultWidth=ImgUtil.getStyleDefaultWidth(style);
         }
         this.style = style;
     }
@@ -76,22 +62,10 @@ public class GridAdapter extends BaseQuickAdapter<Movie.Video, BaseViewHolder> {
         } else {
             helper.setText(R.id.tvName, item.name);
         }
-        TextView tvYear = helper.getView(R.id.tvYear);
-        if (item.year <= 0) {
-            tvYear.setVisibility(View.GONE);
-        } else {
-            tvYear.setText(String.valueOf(item.year));
-            tvYear.setVisibility(View.VISIBLE);
-        }
-        TextView tvLang = helper.getView(R.id.tvLang);
-        tvLang.setVisibility(View.GONE);
-        TextView tvArea = helper.getView(R.id.tvArea);
-        tvArea.setVisibility(View.GONE);
- //           helper.setText(R.id.tvName, item.name);
             ImageView ivThumb = helper.getView(R.id.ivThumb);
 
-        int newWidth = 300;
-        int newHeight = 400;
+        int newWidth = ImgUtil.defaultWidth;
+        int newHeight = ImgUtil.defaultHeight;
         if(style!=null){
              newWidth = defaultWidth;
              newHeight = (int)(newWidth / style.ratio);
@@ -120,6 +94,7 @@ public class GridAdapter extends BaseQuickAdapter<Movie.Video, BaseViewHolder> {
               //  ivThumb.setImageResource(R.drawable.img_loading_placeholder);
 				ivThumb.setImageDrawable(ImgUtil.createTextDrawable(item.name));
             }
+			applyStyleToImage(ivThumb);//动态设置宽高
             return;
         }
 
@@ -160,6 +135,12 @@ public class GridAdapter extends BaseQuickAdapter<Movie.Video, BaseViewHolder> {
         }
  //       helper.setText(R.id.tvName, item.name);
         helper.setText(R.id.tvActor, item.actor);
+        int newWidth = ImgUtil.defaultWidth;
+        int newHeight = ImgUtil.defaultHeight;
+        if(style!=null){
+             newWidth = defaultWidth;
+             newHeight = (int)(newWidth / style.ratio);
+         }
         ImageView ivThumb = helper.getView(R.id.ivThumb);
         //由于部分电视机使用glide报错
         if (!TextUtils.isEmpty(item.pic)) {
@@ -172,7 +153,7 @@ public class GridAdapter extends BaseQuickAdapter<Movie.Video, BaseViewHolder> {
                         .load(DefaultConfig.checkReplaceProxy(item.pic))
                         .transform(new RoundTransformation(MD5.string2MD5(item.pic))
                                 .centerCorp(true)
-                                .override(AutoSizeUtils.mm2px(mContext, 240), AutoSizeUtils.mm2px(mContext, 320))
+                                .override(AutoSizeUtils.mm2px(mContext,newWidth), AutoSizeUtils.mm2px(mContext,newHeight))
                                 .roundRadius(AutoSizeUtils.mm2px(mContext, 10), RoundTransformation.RoundType.ALL))
                         .placeholder(R.drawable.img_loading_placeholder)
                         .noFade()
@@ -190,9 +171,9 @@ public class GridAdapter extends BaseQuickAdapter<Movie.Video, BaseViewHolder> {
      * 根据传入的 style 动态设置 ImageView 的高度：高度 = 宽度 / ratio
      */
     private void applyStyleToImage(final ImageView ivThumb) {
-        ViewGroup container = (ViewGroup) ivThumb.getParent();
-        int width = defaultWidth;
         if(style!=null){
+            ViewGroup container = (ViewGroup) ivThumb.getParent();
+            int width = defaultWidth;
             int height = (int) (width / style.ratio);
             ViewGroup.LayoutParams containerParams = container.getLayoutParams();
             containerParams.height = AutoSizeUtils.mm2px(mContext, height); // 高度
