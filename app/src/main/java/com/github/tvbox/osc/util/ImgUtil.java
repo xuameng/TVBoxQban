8a8c96a1172a8232ac4be0ed71f348df5c4c86f2
xuameng <142598141+xuameng@users.noreply.github.com>
2025-04-18 18:44:49 +0800

    Update ImgUtil.java

--- app/src/main/java/com/github/tvbox/osc/util/ImgUtil.java
+++ app/src/main/java/com/github/tvbox/osc/util/ImgUtil.java
@@ -99,7 +99,7 @@ public class ImgUtil {
         }
         int width = 240, height = 320; // 设定图片大小
         int randomColor = getRandomColor();
-        float cornerRadius = AutoSizeUtils.mm2px(App.getInstance(), 10); // 圆角半径
+        float cornerRadius = AutoSizeUtils.mm2px(App.getInstance(), 8); // 圆角半径
 
         Bitmap bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
         Canvas canvas = new Canvas(bitmap);
