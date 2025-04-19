c8ac70c31a9e4fd8c37c174d87cefe6a1a179634
xuameng <142598141+xuameng@users.noreply.github.com>
2025-04-19 15:30:47 +0800

    Update VideoView.java

--- app/src/main/java/xyz/doikki/videoplayer/player/VideoView.java
+++ app/src/main/java/xyz/doikki/videoplayer/player/VideoView.java
@@ -302,8 +302,8 @@ public class VideoView<P extends AbstractPlayer> extends FrameLayout
             setOptions();
         }
         if (prepareDataSource()) {
-		//	mVideoSize[0] = 0;   //xuameng重要修复获取视频尺寸不刷新
-	//		mVideoSize[1] = 0;
+			mVideoSize[0] = 0;   //xuameng重要修复获取视频尺寸不刷新
+			mVideoSize[1] = 0;
             mMediaPlayer.prepareAsync();
             setPlayState(STATE_PREPARING);
             setPlayerState(isFullScreen() ? PLAYER_FULL_SCREEN : isTinyScreen() ? PLAYER_TINY_SCREEN : PLAYER_NORMAL);
@@ -564,13 +564,13 @@ public class VideoView<P extends AbstractPlayer> extends FrameLayout
 		}
 		String width = Integer.toString(getVideoSize()[0]);
 		String height = Integer.toString(getVideoSize()[1]);
-	/*	if (width.length() <= 1 && height.length() <= 1){
+		if (width.length() <= 1 && height.length() <= 1){
 			if (mRenderView != null) {
 				mPlayerContainer.removeView(mRenderView.getView());      //xuameng重要当视频为空时释放当前VIDEO VIEW
                 mRenderView.release();
                 mRenderView = null;
 			}
-		}  */
+		}
     }
 
     /**
