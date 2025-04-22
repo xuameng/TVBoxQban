2acc7e7fbb7d1332a5526571582471e85ee566c6
xuameng <142598141+xuameng@users.noreply.github.com>
2025-04-22 15:52:28 +0800

    Update SearchCheckboxDialog.java

--- app/src/main/java/com/github/tvbox/osc/ui/dialog/SearchCheckboxDialog.java
+++ app/src/main/java/com/github/tvbox/osc/ui/dialog/SearchCheckboxDialog.java
@@ -78,7 +78,7 @@ public class SearchCheckboxDialog extends BaseDialog{
         mGridView.setLayoutManager(new V7GridLayoutManager(getContext(), spanCount));
         View root = findViewById(R.id.root);
         ViewGroup.LayoutParams clp = root.getLayoutParams();
-        clp.width = AutoSizeUtils.mm2px(getContext(), 400 + 260 * (spanCount - 1));
+        clp.width = AutoSizeUtils.mm2px(getContext(), 400 + 200 * (spanCount - 1));
 
         mGridView.setAdapter(checkboxSearchAdapter);
         checkboxSearchAdapter.setData(mSourceList, mCheckSourcees);
@@ -92,13 +92,13 @@ public class SearchCheckboxDialog extends BaseDialog{
                 }
             }
         }
-//        final int scrollPosition = pos;
-//        mGridView.post(new Runnable() {
-//            @Override
-//            public void run() {
-//                mGridView.smoothScrollToPosition(scrollPosition);
-//            }
-//        });
+        final int scrollPosition = pos;
+        mGridView.post(new Runnable() {
+            @Override
+            public void run() {
+                mGridView.smoothScrollToPosition(scrollPosition);
+            }
+        });
         checkAll.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
