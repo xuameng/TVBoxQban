10742e28d7b1c007a541dbc1cd4f7b79831a33bd
xuameng <142598141+xuameng@users.noreply.github.com>
2025-04-24 13:45:05 +0800

    Update LivePlayActivity.java

--- app/src/main/java/com/github/tvbox/osc/ui/activity/LivePlayActivity.java
+++ app/src/main/java/com/github/tvbox/osc/ui/activity/LivePlayActivity.java
@@ -461,13 +461,13 @@ public class LivePlayActivity extends BaseActivity {
                 //xuameng防止跳焦点                 mRightEpgList.setSelection(i);
                 epgListAdapter.setSelectedEpgIndex(i);
                 int finalI = i;
-                mRightEpgList.post(new Runnable() {
+				mRightEpgList.scrollToPositionWithOffset(finalI, 0);
+                mRightEpgList.postDelayed(new Runnable() {
                     @Override
                     public void run() {
-                        mRightEpgList.scrollToPositionWithOffset(finalI, 0);
 						mRightEpgList.smoothScrollToPosition(finalI);
                     }
-                });
+                }, 50);
             }
         } else { //xuameng无EPG时提示信息
             Epginfo epgbcinfo = new Epginfo(date, "聚汇直播提示您：暂无节目信息！", date, "00:00", "01:59", 0);
@@ -516,13 +516,13 @@ public class LivePlayActivity extends BaseActivity {
                 int finalI = i;
                 mRightEpgList.setSelectedPosition(i);
                 epgListAdapter.setSelectedEpgIndex(i);
-                mRightEpgList.post(new Runnable() {
+				mRightEpgList.scrollToPositionWithOffset(finalI, 0);
+                mRightEpgList.postDelayed(new Runnable() {
                     @Override
                     public void run() {
-                        mRightEpgList.scrollToPositionWithOffset(finalI, 0);
 						mRightEpgList.smoothScrollToPosition(finalI);
                     }
-                });
+                }, 50);
             }
         } else { //xuameng无EPG时提示信息
             Epginfo epgbcinfo = new Epginfo(date, "聚汇直播提示您：暂无节目信息！", date, "00:00", "01:59", 0);
