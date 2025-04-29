4a0c32f3d540a63519a18a2225506b2fe637e7d3
xuameng <142598141+xuameng@users.noreply.github.com>
2025-04-29 11:45:55 +0800

    Update PlayActivity.java

--- app/src/main/java/com/github/tvbox/osc/ui/activity/PlayActivity.java
+++ app/src/main/java/com/github/tvbox/osc/ui/activity/PlayActivity.java
@@ -1712,13 +1712,13 @@ public class PlayActivity extends BaseActivity {
                     loadFoundVideoUrlsHeader.put(url, headers);
                     LOG.i("echo-loadFoundVideoUrl:" + url );
                     if (loadFoundCount.incrementAndGet() == 1) {
-                        url = loadFoundVideoUrls.poll();
+                        stopLoadWebView(false);
+						SuperParse.stopJsonJx();
                         mHandler.removeMessages(100);
+                        url = loadFoundVideoUrls.poll();
                         String cookie = CookieManager.getInstance().getCookie(url);
                         if(!TextUtils.isEmpty(cookie))headers.put("Cookie", " " + cookie);//携带cookie
                         playUrl(url, headers);
-                        stopLoadWebView(false);
-						SuperParse.stopJsonJx();
                     }
                 }
             }
@@ -1900,13 +1900,13 @@ public class PlayActivity extends BaseActivity {
                     loadFoundVideoUrlsHeader.put(url, webHeaders);
                     LOG.i("echo-loadFoundVideoUrl:" + url );
                     if (loadFoundCount.incrementAndGet() == 1) {
+                        stopLoadWebView(false);
+                        SuperParse.stopJsonJx();
                         mHandler.removeMessages(100);
                         url = loadFoundVideoUrls.poll();
                         String cookie = CookieManager.getInstance().getCookie(url);
                         if(!TextUtils.isEmpty(cookie))webHeaders.put("Cookie", " " + cookie);//携带cookie
                         playUrl(url, webHeaders);
-                        stopLoadWebView(false);
-						SuperParse.stopJsonJx();
                     }
                 }
             }
