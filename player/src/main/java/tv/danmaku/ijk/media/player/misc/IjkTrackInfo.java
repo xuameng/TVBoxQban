348c3015f172a11bc2c09c1f4c898df788b842b0
xuameng <142598141+xuameng@users.noreply.github.com>
2025-04-29 13:01:49 +0800

    Update IjkTrackInfo.java

--- player/src/main/java/tv/danmaku/ijk/media/player/misc/IjkTrackInfo.java
+++ player/src/main/java/tv/danmaku/ijk/media/player/misc/IjkTrackInfo.java
@@ -274,6 +274,11 @@ public class IjkTrackInfo implements ITrackInfo {
 				if(Language.contains(rum)) {  //xuameng过滤字幕类型里application/字符串
 					Language = Language.replace(rum, changerum);  //xuameng过滤字幕类型里application/字符串
 				}
+				String mul = "mul";  //xuameng过滤字幕类型里application/字符串
+				String changemul = "多语言";
+				if(Language.contains(mul)) {  //xuameng过滤字幕类型里application/字符串
+					Language = Language.replace(mul, changemul);  //xuameng过滤字幕类型里application/字符串
+				}
 				String und = "und";  //xuameng过滤字幕类型里application/字符串
 				String changeund = "未知";
 				if(Language.contains(und)) {  //xuameng过滤字幕类型里application/字符串
