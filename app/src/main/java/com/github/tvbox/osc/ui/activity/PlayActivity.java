17e934d5591e301ecbcd7ed9b6e37f2b136e586e
xuameng <142598141+xuameng@users.noreply.github.com>
2025-07-09 16:02:19 +0800

    Update PlayActivity.java

--- app/src/main/java/com/github/tvbox/osc/ui/activity/PlayActivity.java
+++ app/src/main/java/com/github/tvbox/osc/ui/activity/PlayActivity.java
@@ -907,6 +907,15 @@ public class PlayActivity extends BaseActivity {
             mVideoView.release();
             mVideoView = null;
         }
+        String CachePath = FileUtils.getCachePath();     //xuameng 清空缓存
+        File CachePathDir = new File(CachePath); 
+        new Thread(() -> {
+        try {
+            if(CachePathDir.exists())FileUtils.cleanDirectory(CachePathDir);
+        } catch (Exception e) {
+              e.printStackTrace();
+        }
+        }).start();
         stopLoadWebView(true);
         stopParse();
 		mController.stopOther();
