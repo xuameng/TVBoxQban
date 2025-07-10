b4f9369df84986fa20ecdc247b7740a76eef0f54
xuameng <142598141+xuameng@users.noreply.github.com>
2025-07-10 11:31:46 +0800

    Update Jianpian.java

--- app/src/main/java/com/github/tvbox/osc/util/thunder/Jianpian.java
+++ app/src/main/java/com/github/tvbox/osc/util/thunder/Jianpian.java
@@ -30,9 +30,11 @@ public class Jianpian {
                         .P2Pdoxadd(replace.getBytes("GBK"));
                 } else if (replace.equals(App.burl)) {
                     App.getp2p()
-                        .P2Pdoxpause(App.burl.getBytes("GBK"));
+                        .P2Pdoxdel(App.burl.getBytes("GBK"));
                     App.getp2p()
                         .P2Pdoxstart(replace.getBytes("GBK"));
+                    App.getp2p()
+                        .P2Pdoxadd(replace.getBytes("GBK"));
                 } else {
                     App.getp2p()
                         .P2Pdoxpause(App.burl.getBytes("GBK"));
