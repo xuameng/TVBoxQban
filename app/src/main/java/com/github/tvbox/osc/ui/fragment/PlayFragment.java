d45c73b3785c2a36fca0efd3444b96f62b136ccd
xuameng <142598141+xuameng@users.noreply.github.com>
2025-07-25 21:58:38 +0800

    Update PlayFragment.java

--- app/src/main/java/com/github/tvbox/osc/ui/fragment/PlayFragment.java
+++ app/src/main/java/com/github/tvbox/osc/ui/fragment/PlayFragment.java
@@ -835,10 +835,6 @@ public class PlayFragment extends BaseLazyFragment {
         mVodInfo = App.getInstance().getVodInfo();
         sourceKey = bundle.getString("sourceKey");
         sourceBean = ApiConfig.get().getSource(sourceKey);
-		if (sourceBean == null){
-			Toast.makeText(requireContext(), "没有源！", Toast.LENGTH_SHORT).show();
-			return;
-		}
         initPlayerCfg();
         play(false);
     }
