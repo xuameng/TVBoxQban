d0fa7230ed530dedc0e0fadb5ef6d10bd390943a
xuameng <142598141+xuameng@users.noreply.github.com>
2025-07-28 12:08:14 +0800

    Update LivePlayActivity.java

--- app/src/main/java/com/github/tvbox/osc/ui/activity/LivePlayActivity.java
+++ app/src/main/java/com/github/tvbox/osc/ui/activity/LivePlayActivity.java
@@ -448,7 +448,7 @@ public class LivePlayActivity extends BaseActivity {
                       if(finalI >= 0 && finalI < epgListAdapter.getItemCount()) {
                          mRightEpgList.scrollToPositionWithOffset(finalI, 0);
                       }
-                  }, 100); 
+                  }, 500); 
               }
            } else { //xuameng无EPG时提示信息
                Epginfo epgbcinfo = new Epginfo(date, "聚汇直播提示您：暂无节目信息！", date, "00:00", "01:59", 0);
@@ -502,7 +502,7 @@ public class LivePlayActivity extends BaseActivity {
                       if(finalI >= 0 && finalI < epgListAdapter.getItemCount()) {
                          mRightEpgList.scrollToPositionWithOffset(finalI, 0);
                       }
-                  }, 100); 
+                  }, 500); 
               }
            } else { //xuameng无EPG时提示信息
                Epginfo epgbcinfo = new Epginfo(date, "聚汇直播提示您：暂无节目信息！", date, "00:00", "01:59", 0);
@@ -560,7 +560,7 @@ public class LivePlayActivity extends BaseActivity {
                      if(finalI >= 0 && finalI < epgListAdapter.getItemCount()) {
                         mRightEpgList.scrollToPositionWithOffset(finalI, 0);
                      }
-                 }, 100); 
+                 }, 500); 
               }
            } else { //xuameng无EPG时提示信息
                Epginfo epgbcinfo = new Epginfo(date, "聚汇直播提示您：暂无节目信息！", date, "00:00", "01:59", 0);
@@ -613,7 +613,7 @@ public class LivePlayActivity extends BaseActivity {
                      if(finalI >= 0 && finalI < epgListAdapter.getItemCount()) {
                         mRightEpgList.scrollToPositionWithOffset(finalI, 0);
                      }
-                 }, 100); 
+                 }, 500); 
               }
            } else { //xuameng无EPG时提示信息
                Epginfo epgbcinfo = new Epginfo(date, "聚汇直播提示您：暂无节目信息！", date, "00:00", "01:59", 0);
@@ -888,9 +888,11 @@ public class LivePlayActivity extends BaseActivity {
         divEpg.setVisibility(View.VISIBLE);
         divLoadEpgleft.setVisibility(View.VISIBLE);
         divLoadEpg.setVisibility(View.GONE);
-        if(finalI >= 0 && finalI < epgListAdapter.getItemCount()) {
-           mRightEpgList.scrollToPositionWithOffset(finalI, 0);
-        }
+        mRightEpgList.postDelayed(() -> {
+            if(finalI >= 0 && finalI < epgListAdapter.getItemCount()) {
+               mRightEpgList.scrollToPositionWithOffset(finalI, 0);
+            }
+        }, 500); 
         epgListAdapter.getSelectedIndex(); //xuamengEPG打开菜单自动变颜色
         mHideChannelListRunXu(); //xuameng BUG
     }
