cc647a4249e257a022a5b717b3d2aec3e19f8268
xuameng <142598141+xuameng@users.noreply.github.com>
2025-07-31 09:23:41 +0800

    Update LivePlayActivity.java

--- app/src/main/java/com/github/tvbox/osc/ui/activity/LivePlayActivity.java
+++ app/src/main/java/com/github/tvbox/osc/ui/activity/LivePlayActivity.java
@@ -496,7 +496,7 @@ public class LivePlayActivity extends BaseActivity {
         //epgListAdapter.updateData(date, new ArrayList<>());
 		String savedEpgKey = channelName + "_" + Objects.requireNonNull(liveEpgDateAdapter.getItem(liveEpgDateAdapter.getSelectedIndex())).getDatePresented();
         if(hsEpg.containsKey(savedEpgKey)) {  //xuameng如果有缓存EPG直接显示
-	if(!hsEpg.get(savedEpgKey).containsKey("聚汇影视")) {  	
+	if(!hsEpg.get(savedEpgKey).contains("聚汇影视")) {  	
            showEpg(date, hsEpg.get(savedEpgKey));
            showBottomEpgXU(); //xuameng测试EPG刷新
            return;
@@ -609,7 +609,7 @@ public class LivePlayActivity extends BaseActivity {
         //epgListAdapter.updateData(date, new ArrayList<>());
 		String savedEpgKey = channelName + "_" + Objects.requireNonNull(liveEpgDateAdapter.getItem(liveEpgDateAdapter.getSelectedIndex())).getDatePresented();
         if(hsEpg.containsKey(savedEpgKey)) {   //xuameng如果有缓存EPG直接显示
-		if(!hsEpg.get(savedEpgKey).containsKey("聚汇影视")) {  	
+		if(!hsEpg.get(savedEpgKey).contains("聚汇影视")) {  	
            showEpgxu(date, hsEpg.get(savedEpgKey));        
            return;
 		}	
