7e21ba9bbb3376c0e9160eda691ce1cae46ec797
xuameng <142598141+xuameng@users.noreply.github.com>
2025-08-10 16:49:11 +0800

    Update SearchActivity.java

--- app/src/main/java/com/github/tvbox/osc/ui/activity/SearchActivity.java
+++ app/src/main/java/com/github/tvbox/osc/ui/activity/SearchActivity.java
@@ -518,13 +518,10 @@ public class SearchActivity extends BaseActivity {
 			return;
 		}
         cancel();   
-
-runOnUiThread(() -> {
-    if(remoteDialog != null) {
-        remoteDialog.dismiss();
-        remoteDialog = null;
-    }
-});
+        if (remoteDialog != null) {
+            remoteDialog.dismiss();
+            remoteDialog = null;
+        }
         etSearch.setText(title);
         this.searchTitle = title;
         mGridView.setVisibility(View.GONE); //xuameng 搜索历史
