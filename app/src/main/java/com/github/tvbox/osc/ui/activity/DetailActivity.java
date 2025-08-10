75c573d25b76b7825a29077e578b4c86be09bfff
xuameng <142598141+xuameng@users.noreply.github.com>
2025-08-10 10:27:42 +0800

    Update DetailActivity.java

--- app/src/main/java/com/github/tvbox/osc/ui/activity/DetailActivity.java
+++ app/src/main/java/com/github/tvbox/osc/ui/activity/DetailActivity.java
@@ -16,7 +16,6 @@ import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
-import android.widget.Toast;
 import android.content.ClipboardManager;
 import android.content.ClipData;
 
