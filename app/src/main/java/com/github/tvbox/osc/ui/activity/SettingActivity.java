79bc72602d74ebdbb7d8a35400282a391a701f91
xuameng <142598141+xuameng@users.noreply.github.com>
2025-08-10 10:31:26 +0800

    Update SettingActivity.java

--- app/src/main/java/com/github/tvbox/osc/ui/activity/SettingActivity.java
+++ app/src/main/java/com/github/tvbox/osc/ui/activity/SettingActivity.java
@@ -7,7 +7,6 @@ import android.view.KeyEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.viewpager.widget.ViewPager;
 
