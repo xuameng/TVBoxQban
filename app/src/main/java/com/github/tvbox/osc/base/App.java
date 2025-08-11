5dc9f523e35d5aaac4bc39bd6af6e911e3a6bbda
xuameng <142598141+xuameng@users.noreply.github.com>
2025-08-11 10:32:10 +0800

    Update App.java

--- app/src/main/java/com/github/tvbox/osc/base/App.java
+++ app/src/main/java/com/github/tvbox/osc/base/App.java
@@ -156,7 +156,7 @@ public class App extends MultiDexApplication {
 
     public static void showToastLong(Context context, String msg) {  //xuameeng showtoast
         if (mToast != null) {
-			mToast.cancel();
+            mToast.cancel();
             mToast.setText(msg);
         } else {
             mToast = Toast.makeText(context, msg, Toast.LENGTH_LONG);
