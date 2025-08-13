5901adeb0b74d18c0e0c2cfb25d621ce75bc604f
xuameng <142598141+xuameng@users.noreply.github.com>
2025-08-13 21:48:19 +0800

    Update CollectActivity.java

--- app/src/main/java/com/github/tvbox/osc/ui/activity/CollectActivity.java
+++ app/src/main/java/com/github/tvbox/osc/ui/activity/CollectActivity.java
@@ -107,17 +107,6 @@ public class CollectActivity extends BaseActivity {
 
             }
         });
-
-        private View.OnFocusChangeListener focusChangeListener = new View.OnFocusChangeListener() {     //xuameng 触碰变大
-            @Override
-            public void onFocusChange(View v, boolean hasFocus) {
-                if (hasFocus)
-                    v.animate().scaleX(1.05f).scaleY(1.05f).setDuration(300).setInterpolator(new BounceInterpolator()).start();
-                else
-                    v.animate().scaleX(1.0f).scaleY(1.0f).setDuration(300).setInterpolator(new BounceInterpolator()).start();
-            }
-        };
-
         collectAdapter.setOnItemClickListener(new BaseQuickAdapter.OnItemClickListener() {
             @Override
             public void onItemClick(BaseQuickAdapter adapter, View view, int position) {
@@ -160,6 +149,16 @@ public class CollectActivity extends BaseActivity {
             }
         });
     }
+
+    private View.OnFocusChangeListener focusChangeListener = new View.OnFocusChangeListener() {     //xuameng 触碰变大
+        @Override
+        public void onFocusChange(View v, boolean hasFocus) {
+            if (hasFocus)
+                v.animate().scaleX(1.05f).scaleY(1.05f).setDuration(300).setInterpolator(new BounceInterpolator()).start();
+            else
+                v.animate().scaleX(1.0f).scaleY(1.0f).setDuration(300).setInterpolator(new BounceInterpolator()).start();
+        }
+    };
 
     private void initData() {
         List<VodCollect> allVodRecord = RoomDataManger.getAllVodCollect();
