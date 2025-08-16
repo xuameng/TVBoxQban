64a58024c1baeeb3e8250b6814ff802207466838
xuameng <142598141+xuameng@users.noreply.github.com>
2025-08-16 13:50:55 +0800

    Update LivePlayActivity.java

--- app/src/main/java/com/github/tvbox/osc/ui/activity/LivePlayActivity.java
+++ app/src/main/java/com/github/tvbox/osc/ui/activity/LivePlayActivity.java
@@ -2206,8 +2206,9 @@ public class LivePlayActivity extends BaseActivity {
             @Override
             public void onItemSelected(TvRecyclerView parent, View itemView, int position) {
                 isTouch = false;
+int channelGroupIndexXu = liveChannelGroupAdapter.getSelectedGroupIndex(); //xuameng当前选定的频道组
                 if(position < 0 || position > getLiveChannels(channelGroupIndexXu).size() - 1) return;
-                int channelGroupIndexXu = liveChannelGroupAdapter.getSelectedGroupIndex(); //xuameng当前选定的频道组
+                
                 if(position == getLiveChannels(channelGroupIndexXu).size() - 1) { //xuameng判断是否是最后一个item
                     itemView.setId(View.generateViewId());
                     itemView.setNextFocusDownId(itemView.getId()); //xuameng不超出item
