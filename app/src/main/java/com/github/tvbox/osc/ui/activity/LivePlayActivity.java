2f59d34fbeb2ca1fca5cf8c9a7b7e0cad323a929
xuameng <142598141+xuameng@users.noreply.github.com>
2025-08-16 10:44:46 +0800

    Update LivePlayActivity.java

--- app/src/main/java/com/github/tvbox/osc/ui/activity/LivePlayActivity.java
+++ app/src/main/java/com/github/tvbox/osc/ui/activity/LivePlayActivity.java
@@ -3246,4 +3246,13 @@ public class LivePlayActivity extends BaseActivity {
             xToast = null;
         }
     }
+    private View.OnFocusChangeListener focusChangeListener = new View.OnFocusChangeListener() {     //xuameng 触碰变大
+        @Override
+        public void onFocusChange(View v, boolean hasFocus) {
+            if (hasFocus)
+                v.animate().scaleX(1.05f).scaleY(1.05f).setDuration(300).setInterpolator(new BounceInterpolator()).start();
+            else
+                v.animate().scaleX(1.0f).scaleY(1.0f).setDuration(300).setInterpolator(new BounceInterpolator()).start();
+        }
+    };
 }
