257875a9e6267dbf53d05854b54921c16622b899
xuameng <142598141+xuameng@users.noreply.github.com>
2025-08-18 14:01:24 +0800

    Update FastSearchActivity.java

--- app/src/main/java/com/github/tvbox/osc/ui/activity/FastSearchActivity.java
+++ app/src/main/java/com/github/tvbox/osc/ui/activity/FastSearchActivity.java
@@ -41,6 +41,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
+import java.util.concurrent.Executors;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.ArrayBlockingQueue;   //xuameng 线程池
