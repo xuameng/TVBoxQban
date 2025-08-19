04bde4f10db334a7cef64183eba3a34f1fddd39e
xuameng <142598141+xuameng@users.noreply.github.com>
2025-08-19 21:16:06 +0800

    Update VodController.java

--- app/src/main/java/com/github/tvbox/osc/player/controller/VodController.java
+++ app/src/main/java/com/github/tvbox/osc/player/controller/VodController.java
@@ -2183,7 +2183,6 @@ private void initVisualizer() {
 
         // 初始化可视化组件
         Visualizer visualizer = new Visualizer(sessionId);
-		visualizer.setEnabled(false);  // 先禁用
     releaseVisualizer();
         
         // 设置FFT数据回调
@@ -2223,7 +2222,6 @@ visualizer.setDataCaptureListener(
  */
 private void releaseVisualizer() {
     try {
-		Visualizer.setEnabled(false);  // 先禁用
         if (visualizerRef != null) {
             Visualizer v = visualizerRef.get();
             if (v != null) {
