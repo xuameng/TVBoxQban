ef9a11cd55f2fd89de610e6a4e743f65e0f8b710
xuameng <142598141+xuameng@users.noreply.github.com>
2025-08-19 21:54:18 +0800

    Update MusicVisualizerView.java

--- app/src/main/java/com/github/tvbox/osc/ui/tv/widget/MusicVisualizerView.java
+++ app/src/main/java/com/github/tvbox/osc/ui/tv/widget/MusicVisualizerView.java
@@ -19,7 +19,7 @@ import android.animation.ValueAnimator;
  */
 public class MusicVisualizerView extends View {
     // 常量定义
-    private static final int MAX_AMPLITUDE = 382767;   // 最大振幅值（16位有符号整数范围）
+    private static final int MAX_AMPLITUDE = 196602;   // 最大振幅值（16位有符号整数范围）
     private static final int BAR_COUNT = 16;          // 柱状图数量
     private static final int ANIMATION_DURATION = 200;// 动画时长(ms)
 
