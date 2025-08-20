7ae679576eab8f11357668cbccafabf814a319b3
xuameng <142598141+xuameng@users.noreply.github.com>
2025-08-20 11:37:10 +0800

    Update MusicVisualizerView.java

--- app/src/main/java/com/github/tvbox/osc/ui/tv/widget/MusicVisualizerView.java
+++ app/src/main/java/com/github/tvbox/osc/ui/tv/widget/MusicVisualizerView.java
@@ -23,12 +23,11 @@ public class MusicVisualizerView extends View {
     private static final int BAR_COUNT = 20;
     private static final int ANIMATION_DURATION = 200;
     
-    // 颜色渐变参数
     private static final int[] COLOR_SPECTRUM = {
         Color.parseColor("#DBDB70"), // 黄色
         Color.parseColor("#FF9900"), // 橙黄
         Color.parseColor("#FF3300")  // 橙红
-    };‌
+    };
 
     // 绘图工具
     private final Paint mBarPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
