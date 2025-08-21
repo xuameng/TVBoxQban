d09c4af90b60284e83f34616104c5de40e86a741
xuameng <142598141+xuameng@users.noreply.github.com>
2025-08-21 12:30:32 +0800

    Update SourceViewModel.java

--- app/src/main/java/com/github/tvbox/osc/viewmodel/SourceViewModel.java
+++ app/src/main/java/com/github/tvbox/osc/viewmodel/SourceViewModel.java
@@ -538,6 +538,11 @@ public class SourceViewModel extends ViewModel {
         String id = urlid;
     
         SourceBean sourceBean = ApiConfig.get().getSource(sourceKey);
+        if (sourceBean == null) {      //xuameng判断sourceKey为空 远程推送BUG
+            detailResult.postValue(null);
+            Log.e("sourceBean", "get sourceBean got null, this should not be happended, maybe apiconfig get from http failed and use cache, sourceKey is " + sourceKey);
+            return;
+        }
         int type = sourceBean.getType();
         if (type == 3) {
             spThreadPool.execute(new Runnable() {
