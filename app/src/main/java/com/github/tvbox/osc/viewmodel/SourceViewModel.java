cf4b071be90202f8686a910fffc8ac5a00803060
xuameng <142598141+xuameng@users.noreply.github.com>
2025-08-21 12:48:55 +0800

    Update SourceViewModel.java

--- app/src/main/java/com/github/tvbox/osc/viewmodel/SourceViewModel.java
+++ app/src/main/java/com/github/tvbox/osc/viewmodel/SourceViewModel.java
@@ -64,6 +64,7 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
+import android.util.Log;
 
 import okhttp3.Call;
 
