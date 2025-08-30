876f49f7db6d0d3d9c7d37a6c5a20f8d31f0a37d
xuameng <142598141+xuameng@users.noreply.github.com>
2025-08-30 10:03:04 +0800

    Update EXOmPlayer.java

--- app/src/main/java/com/github/tvbox/osc/player/EXOmPlayer.java
+++ app/src/main/java/com/github/tvbox/osc/player/EXOmPlayer.java
@@ -153,7 +153,7 @@ public class EXOmPlayer extends ExoMediaPlayer {
                 for (int renderIndex = 0; renderIndex < trackInfo.getRendererCount(); renderIndex++) {
                     if (trackInfo.getRendererType(renderIndex) == C.TRACK_TYPE_TEXT) {
                         DefaultTrackSelector.ParametersBuilder parametersBuilder = getTrackSelector().getParameters().buildUpon();
-                        parametersBuilder.setRendererDisabled(renderIndex, true);
+                        parametersBuilder.setRendererDisabled(renderIndex, false);
                         getTrackSelector().setParameters(parametersBuilder);
                         break;
                     }
