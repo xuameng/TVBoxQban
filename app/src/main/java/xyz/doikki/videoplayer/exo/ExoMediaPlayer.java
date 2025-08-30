08359c6635c56135f43def4dfd1e87266ed8f0d4
xuameng <142598141+xuameng@users.noreply.github.com>
2025-08-30 14:44:16 +0800

    Update ExoMediaPlayer.java

--- app/src/main/java/xyz/doikki/videoplayer/exo/ExoMediaPlayer.java
+++ app/src/main/java/xyz/doikki/videoplayer/exo/ExoMediaPlayer.java
@@ -126,10 +126,6 @@ public class ExoMediaPlayer extends AbstractPlayer implements Player.Listener {
         mTrackSelections = trackSelections;
     }
 
-    public void setTrackSelector(TrackSelector trackSelector) {
-        mTrackSelector = trackSelector;
-    }
-
     public void setRenderersFactory(RenderersFactory renderersFactory) {
         mRenderersFactory = renderersFactory;
     }
