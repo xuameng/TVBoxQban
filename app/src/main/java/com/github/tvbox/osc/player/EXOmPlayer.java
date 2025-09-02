c64b0f61f4c81cc348cff35ee78c74cf5fc5ce03
xuameng <142598141+xuameng@users.noreply.github.com>
2025-09-02 15:03:34 +0800

    Update EXOmPlayer.java

--- app/src/main/java/com/github/tvbox/osc/player/EXOmPlayer.java
+++ app/src/main/java/com/github/tvbox/osc/player/EXOmPlayer.java
@@ -182,12 +182,10 @@ public class EXOmPlayer extends ExoMediaPlayer {
                     }
                 }
             } else {
-				int audioRendererIndexXu = findAudioRendererIndex(trackInfo);
                 TrackGroupArray trackGroupArray = trackInfo.getTrackGroups(videoTrackBean.renderId);
                 DefaultTrackSelector.SelectionOverride override = new DefaultTrackSelector.SelectionOverride(videoTrackBean.trackGroupId, videoTrackBean.trackId);
                 DefaultTrackSelector.ParametersBuilder parametersBuilder = getTrackSelector().buildUponParameters();
                 parametersBuilder.setRendererDisabled(videoTrackBean.renderId, false);
-				parametersBuilder.clearSelectionOverrides(audioRendererIndexXu);
                 parametersBuilder.setSelectionOverride(videoTrackBean.renderId, trackGroupArray, override);
                 getTrackSelector().setParameters(parametersBuilder);
                 //xuameng记忆选择音轨
