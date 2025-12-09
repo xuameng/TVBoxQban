8a1e2e13f7164fb9ce4785c81c75063487f11acc
xuameng <142598141+xuameng@users.noreply.github.com>
2025-12-09 19:25:56 +0800

    Update GridFilterKVAdapter.java

--- app/src/main/java/com/github/tvbox/osc/ui/adapter/GridFilterKVAdapter.java
+++ app/src/main/java/com/github/tvbox/osc/ui/adapter/GridFilterKVAdapter.java
@@ -8,22 +8,35 @@ import java.util.ArrayList;
 
 public class GridFilterKVAdapter extends BaseQuickAdapter<String, BaseViewHolder> {
     private int selectedPosition = -1;
+    private int defaultColor;
+    private int selectedColor;
     
-    public GridFilterKVAdapter() {
+    public GridFilterKVAdapter(int defaultColor, int selectedColor) {
         super(R.layout.item_grid_filter_value, new ArrayList<>());
+        this.defaultColor = defaultColor;
+        this.selectedColor = selectedColor;
     }
 
     @Override
     protected void convert(BaseViewHolder helper, String item) {
         helper.setText(R.id.filterValue, item);
+        
+        TextView valueTv = helper.getView(R.id.filterValue);
+        if (helper.getAdapterPosition() == selectedPosition) {         //xuameng新增方法修正多个高亮BUG
+            valueTv.getPaint().setFakeBoldText(true);
+            valueTv.setTextColor(selectedColor);
+        } else {
+            valueTv.getPaint().setFakeBoldText(false);           //xuameng新增方法修正多个高亮BUG
+            valueTv.setTextColor(defaultColor);
+        }
     }
     
-    public void setSelectedPosition(int position) {       //xuameng新增方法
+    public void setSelectedPosition(int position) {    //xuameng新增方法修正多个高亮BUG
         this.selectedPosition = position;
         notifyDataSetChanged();
     }
     
-    public int getSelectedPosition() {   //xuameng新增方法
+    public int getSelectedPosition() {    //xuameng新增方法修正多个高亮BUG
         return selectedPosition;
     }
 }
