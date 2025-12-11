9c34ec13eaef7fbd4d8a0e6326470e2a325ca13b
xuameng <142598141+xuameng@users.noreply.github.com>
2025-12-11 08:54:19 +0800

    Update GridFilterKVAdapter.java

--- app/src/main/java/com/github/tvbox/osc/ui/adapter/GridFilterKVAdapter.java
+++ app/src/main/java/com/github/tvbox/osc/ui/adapter/GridFilterKVAdapter.java
@@ -34,7 +34,7 @@ public class GridFilterKVAdapter extends BaseQuickAdapter<String, BaseViewHolder
     
     public void setSelectedPosition(int position) {    //xuameng新增方法修正多个高亮BUG
         this.selectedPosition = position;
-        notifyItemChanged(position)，
+        notifyItemChanged(position);
     }
     
     public int getSelectedPosition() {    //xuameng新增方法修正多个高亮BUG
