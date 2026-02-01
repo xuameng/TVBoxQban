698d7e451d6eaefc9de3e709ef69c5a9bf2c6f0f
xuameng <142598141+xuameng@users.noreply.github.com>
2026-02-01 14:47:16 +0800

    Update SurfaceRenderView.java

--- app/src/main/java/com/github/tvbox/osc/player/render/SurfaceRenderView.java
+++ app/src/main/java/com/github/tvbox/osc/player/render/SurfaceRenderView.java
@@ -76,7 +76,14 @@ public class SurfaceRenderView extends SurfaceView implements IRenderView, Surfa
 
     @Override
     public void release() {
-
+        if (mMediaPlayer != null) {
+            mMediaPlayer.setDisplay(null);
+        }
+        SurfaceHolder surfaceHolder = getHolder();
+        if (surfaceHolder != null) {
+            surfaceHolder.removeCallback(this);
+        }
+        mMediaPlayer = null;
     }
 
     @Override
