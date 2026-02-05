010c7e114389504075e0869a81868759c81e9113
xuameng <142598141+xuameng@users.noreply.github.com>
2026-02-05 21:55:54 +0800

    Update LiveChannelItemAdapter.java

--- app/src/main/java/com/github/tvbox/osc/ui/adapter/LiveChannelItemAdapter.java
+++ app/src/main/java/com/github/tvbox/osc/ui/adapter/LiveChannelItemAdapter.java
@@ -14,6 +14,7 @@ import com.google.gson.JsonObject;
 import com.orhanobut.hawk.Hawk;
 import java.util.ArrayList;
 import com.github.tvbox.osc.R;
+import com.github.tvbox.osc.ui.activity.LivePlayActivity;
 
 import com.github.tvbox.osc.bean.LiveChannelItem;
 import com.orhanobut.hawk.Hawk;
