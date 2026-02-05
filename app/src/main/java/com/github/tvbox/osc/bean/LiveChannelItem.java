c6b4616bb09f5f4f1dda92b87511bcb0c58b3a78
xuameng <142598141+xuameng@users.noreply.github.com>
2026-02-05 20:53:06 +0800

    Update LiveChannelItem.java

--- app/src/main/java/com/github/tvbox/osc/bean/LiveChannelItem.java
+++ app/src/main/java/com/github/tvbox/osc/bean/LiveChannelItem.java
@@ -101,4 +101,109 @@ public class LiveChannelItem {
     public String getSourceName() {
         return channelSourceNames.get(sourceIndex);
     }
+
+    /**
+     * 将 LiveChannelItem 转换为 JsonObject 以便存储到 Hawk
+     */
+    public static JsonObject convertChannelToJson(LiveChannelItem channel) {
+        JsonObject json = new JsonObject();
+        json.addProperty("channelIndex", channel.getChannelIndex());
+        json.addProperty("channelNum", channel.getChannelNum());
+        json.addProperty("channelName", channel.getChannelName());
+        json.addProperty("sourceIndex", channel.getSourceIndex());
+        json.addProperty("sourceNum", channel.getSourceNum());
+        json.addProperty("include_back", channel.getinclude_back());
+
+        JsonArray sourceNameArray = new JsonArray();
+        ArrayList<String> sourceNames = channel.getChannelSourceNames();
+        if (sourceNames != null) {
+            for (String name : sourceNames) {
+                sourceNameArray.add(name);
+            }
+        }
+        json.add("channelSourceNames", sourceNameArray);
+
+        JsonArray urlArray = new JsonArray();
+        ArrayList<String> urls = channel.getChannelUrls();
+        if (urls != null) {
+            for (String url : urls) {
+                urlArray.add(url);
+            }
+        }
+        json.add("channelUrls", urlArray);
+
+        return json;
+    }
+
+    /**
+     * 从 Hawk 中读取收藏的频道，并构建一个 LiveChannelGroup 对象
+     */
+    public static LiveChannelGroup createFavoriteChannelGroup() {
+        LiveChannelGroup group = new LiveChannelGroup();
+        group.setGroupIndex(-1);
+        group.setGroupName("我的收藏");
+        group.setGroupPassword("");
+
+        ArrayList<LiveChannelItem> favoriteChannels = new ArrayList<>();
+        JsonArray favoriteArray = Hawk.get(HawkConfig.LIVE_FAVORITE_CHANNELS, new JsonArray());
+
+        for (int i = 0; i < favoriteArray.size(); i++) {
+            try {
+                JsonObject channelJson = favoriteArray.get(i).getAsJsonObject();
+                LiveChannelItem item = new LiveChannelItem();
+
+                item.setChannelIndex(channelJson.get("channelIndex").getAsInt());
+                item.setChannelNum(channelJson.get("channelNum").getAsInt());
+                item.setChannelName(channelJson.get("channelName").getAsString());
+                item.setSourceIndex(channelJson.get("sourceIndex").getAsInt());
+                item.setSourceNum(channelJson.get("sourceNum").getAsInt());
+                item.setinclude_back(channelJson.get("include_back").getAsBoolean());
+
+                JsonArray sourceNameArray = channelJson.getAsJsonArray("channelSourceNames");
+                ArrayList<String> sourceNames = new ArrayList<>();
+                for (int j = 0; j < sourceNameArray.size(); j++) {
+                    sourceNames.add(sourceNameArray.get(j).getAsString());
+                }
+                item.setChannelSourceNames(sourceNames);
+
+                JsonArray urlArray = channelJson.getAsJsonArray("channelUrls");
+                ArrayList<String> urls = new ArrayList<>();
+                for (int j = 0; j < urlArray.size(); j++) {
+                    urls.add(urlArray.get(j).getAsString());
+                }
+                item.setChannelUrls(urls);
+
+                favoriteChannels.add(item);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        group.setLiveChannels(favoriteChannels);
+        return group;
+    }
+
+    /**
+     * 判断两个 JsonObject 是否代表同一个频道
+     */
+    public static boolean isSameChannel(JsonObject fav1, JsonObject fav2) {
+        if (!fav1.get("channelName").getAsString().equals(fav2.get("channelName").getAsString())) {
+            return false;
+        }
+
+        JsonArray urls1 = fav1.getAsJsonArray("channelUrls");
+        JsonArray urls2 = fav2.getAsJsonArray("channelUrls");
+
+        if (urls1.size() != urls2.size()) {
+            return false;
+        }
+
+        Set<String> set1 = new HashSet<>();
+        Set<String> set2 = new HashSet<>();
+        for (int i = 0; i < urls1.size(); i++) {
+            set1.add(urls1.get(i).getAsString());
+            set2.add(urls2.get(i).getAsString());
+        }
+
+        return set1.equals(set2);
+    }
 }
