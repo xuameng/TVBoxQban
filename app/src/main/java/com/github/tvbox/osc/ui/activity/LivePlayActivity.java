3203cb20809833d80f47a7b380e1e771a0b2fbf1
xuameng <142598141+xuameng@users.noreply.github.com>
2026-02-12 00:32:01 +0800

    Add files via upload

--- app/src/main/java/com/github/tvbox/osc/ui/activity/LivePlayActivity.java
+++ app/src/main/java/com/github/tvbox/osc/ui/activity/LivePlayActivity.java
@@ -2804,16 +2804,13 @@ public class LivePlayActivity extends BaseActivity {
                 App.showToastShort(mContext, "1111111111！");
             } else {
             
-                    mHandler.post(new Runnable() {
-                        @Override
-                        public void run() {
+                 
                            
                                 setDefaultLiveChannelList();
                          
                   showSuccess();
                 App.showToastShort(mContext, "聚汇影22222！");                          
-                        }
-                    });      
+                          
             
              
 
@@ -3696,7 +3693,7 @@ private void initLiveState() {
     
         // 2. xuameng创建默认直播分组（索引从1开始）
         LiveChannelGroup defaultGroup = new LiveChannelGroup();
-        defaultGroup.setGroupIndex(1); // xuameng改为1，避免与收藏组冲突
+        defaultGroup.setGroupIndex(10); // xuameng改为1，避免与收藏组冲突
         defaultGroup.setGroupName("聚汇直播");
         defaultGroup.setGroupPassword("");
     
@@ -3720,7 +3717,7 @@ private void initLiveState() {
         liveChannelGroupList.add(defaultGroup);
     
         showSuccess();
-       // initLiveState();
+        initLiveState();
     }
 
     private void initLiveObj(){
