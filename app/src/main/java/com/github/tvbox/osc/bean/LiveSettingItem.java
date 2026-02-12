925c3366153673557e421e09a7c8f34e6b69a000
xuameng <142598141+xuameng@users.noreply.github.com>
2026-02-12 13:33:50 +0800

    Update LiveSettingItem.java

--- app/src/main/java/com/github/tvbox/osc/bean/LiveSettingItem.java
+++ app/src/main/java/com/github/tvbox/osc/bean/LiveSettingItem.java
@@ -1,14 +1,15 @@
 package com.github.tvbox.osc.bean;
 
 /**
- * @author pj567
- * @date :2021/1/12
+ * @author xuameng
+ * @date :2026/2/12
  * @description:
  */
 public class LiveSettingItem {
     private int itemIndex;
     private String itemName;
     private boolean itemSelected = false;
+    private String itemUrl; // xuaemng新增：存储线路URL
 
     public int getItemIndex() {
         return itemIndex;
@@ -32,5 +33,10 @@ public class LiveSettingItem {
 
     public void setItemSelected(boolean itemSelected) {
         this.itemSelected = itemSelected;
+    }
+
+    // xuameng新增：itemUrl的Getter/Setter
+    public String getItemUrl() {
+        return itemUrl;
     }
 }
