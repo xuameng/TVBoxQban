2578965205a13b279d4597fe8383b1960650afdf
xuameng <142598141+xuameng@users.noreply.github.com>
2026-02-13 14:36:30 +0800

    Update OkGoHelper.java

--- app/src/main/java/com/github/tvbox/osc/util/OkGoHelper.java
+++ app/src/main/java/com/github/tvbox/osc/util/OkGoHelper.java
@@ -31,7 +31,7 @@ import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ExecutorService;
-import java.util.Executors;
+import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
