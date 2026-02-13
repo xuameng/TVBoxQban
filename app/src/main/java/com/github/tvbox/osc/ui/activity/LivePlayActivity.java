b698133a32ffd7b3dff186c19901c7be6f36fdbc
xuameng <142598141+xuameng@users.noreply.github.com>
2026-02-13 19:42:59 +0800

    Update LivePlayActivity.java

--- app/src/main/java/com/github/tvbox/osc/ui/activity/LivePlayActivity.java
+++ app/src/main/java/com/github/tvbox/osc/ui/activity/LivePlayActivity.java
@@ -1554,6 +1554,7 @@ public class LivePlayActivity extends BaseActivity {
             return;
         }
         currentLiveChannelItem.preSource();
+        liveSettingGroupAdapter.setSelectedGroupIndex(-1); //xuameng右菜单BUG修复
         playChannel(currentChannelGroupIndex, currentLiveChannelIndex, true);
     }
     public void playNextSource() {
@@ -1565,6 +1566,7 @@ public class LivePlayActivity extends BaseActivity {
             return;
         }
         currentLiveChannelItem.nextSource();
+        liveSettingGroupAdapter.setSelectedGroupIndex(-1); //xuameng右菜单BUG修复
         playChannel(currentChannelGroupIndex, currentLiveChannelIndex, true);
     }
     public void playXuSource() {
@@ -2226,7 +2228,6 @@ public class LivePlayActivity extends BaseActivity {
                     }
                 } else {
                     playNextSource();
-                    liveSettingGroupAdapter.setSelectedGroupIndex(-1); //xuameng右菜单BUG修复
                 } else if(direction < 0)
                     if(isBack) { //xuameng手机换源和隐藏时移控制栏
                         if(backcontroller.getVisibility() == View.VISIBLE) {
@@ -2242,7 +2243,6 @@ public class LivePlayActivity extends BaseActivity {
                     }
                 } else {
                     playPreSource();
-                    liveSettingGroupAdapter.setSelectedGroupIndex(-1); //xuameng右菜单BUG修复
                 }
             }
         });
