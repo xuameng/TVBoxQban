3fa9b427a4ad0a30f38bafb7ed230858e7039ecf
xuameng <142598141+xuameng@users.noreply.github.com>
2026-04-04 19:09:27 +0800

    Update GridAdapter.java

--- app/src/main/java/com/github/tvbox/osc/ui/adapter/GridAdapter.java
+++ app/src/main/java/com/github/tvbox/osc/ui/adapter/GridAdapter.java
@@ -157,7 +157,6 @@ public class GridAdapter extends BaseQuickAdapter<Movie.Video, BaseViewHolder> {
             }else {
                 Picasso.get()
                         .load(DefaultConfig.checkReplaceProxy(item.pic))
-                        .resize(300,400)
                         .transform(new RoundTransformation(MD5.string2MD5(item.pic))
                                 .centerCorp(true)
                                 .override(AutoSizeUtils.mm2px(mContext,newWidth), AutoSizeUtils.mm2px(mContext,newHeight))
