1a4c5eba4f7ca031a9de969d802e96cc18d40bec
xuameng <142598141+xuameng@users.noreply.github.com>
2026-04-06 11:22:21 +0800

    Update ImgUtil.java

--- app/src/main/java/com/github/tvbox/osc/util/ImgUtil.java
+++ app/src/main/java/com/github/tvbox/osc/util/ImgUtil.java
@@ -86,8 +86,8 @@ public class ImgUtil {
             } else if (style.ratio >= 1.3) {
                 spanCount = 4; // 4:3
             }
-        } else if ("list".equals(style.type)) {   //xuameng list 时 首页推荐 5列要不太难看
-            spanCount = 5;
+        } else if ("list".equals(style.type)) {   //xuameng list 时 首页推荐 1列
+            spanCount = 1;
         }
         return spanCount;
     }
