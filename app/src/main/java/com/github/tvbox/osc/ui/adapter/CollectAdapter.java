75f730dee5dc3a23053a65a56b32a731590089c6
xuameng <142598141+xuameng@users.noreply.github.com>
2026-04-07 18:44:29 +0800

    Update CollectAdapter.java

--- app/src/main/java/com/github/tvbox/osc/ui/adapter/CollectAdapter.java
+++ app/src/main/java/com/github/tvbox/osc/ui/adapter/CollectAdapter.java
@@ -18,6 +18,7 @@ import com.github.tvbox.osc.util.HawkConfig;
 import com.github.tvbox.osc.util.MD5;
 import com.squareup.picasso.Picasso;
 import com.github.tvbox.osc.util.ImgUtil;   //xuamengBASE64图片
+import com.github.tvbox.osc.util.ImgUtilCollect;   //xuamengBASE64图片
 
 import java.util.ArrayList;
 
@@ -68,12 +69,12 @@ public class CollectAdapter extends BaseQuickAdapter<VodCollect, BaseViewHolder>
                         .placeholder(R.drawable.img_loading_placeholder)
                         .noFade()
                        // .error(R.drawable.img_loading_placeholder)
-					    .error(ImgUtil.createTextDrawable(item.name))
+					    .error(ImgUtilCollect.createTextDrawable(item.name))
                         .into(ivThumb);
             }
         } else {
            // ivThumb.setImageResource(R.drawable.img_loading_placeholder);
-			ivThumb.setImageDrawable(ImgUtil.createTextDrawable(item.name));
+			ivThumb.setImageDrawable(ImgUtilCollect.createTextDrawable(item.name));
         }
     }
 }
