8b2dda834f8d86cfe1d33551794c13f0c29f0164
xuameng <142598141+xuameng@users.noreply.github.com>
2026-04-07 12:56:12 +0800

    Update App.java

--- app/src/main/java/com/github/tvbox/osc/base/App.java
+++ app/src/main/java/com/github/tvbox/osc/base/App.java
@@ -6,7 +6,7 @@ import androidx.multidex.MultiDexApplication;
 import com.github.tvbox.osc.bean.VodInfo;
 import com.github.tvbox.osc.callback.EmptyCallback;
 import com.github.tvbox.osc.callback.LoadingCallback;
-import com.github.tvbox.osc.callback.ConfigCallback;  //配置中心
+import com.github.tvbox.osc.callback.ConfigCallback;  //xuameng配置中心
 import com.github.tvbox.osc.data.AppDataManager;
 import com.github.tvbox.osc.server.ControlManager;
 import com.github.tvbox.osc.util.AppManager;
@@ -55,7 +55,7 @@ public class App extends MultiDexApplication {
         LoadSir.beginBuilder()
                 .addCallback(new EmptyCallback())
                 .addCallback(new LoadingCallback())
-                .addCallback(new ConfigCallback())
+                .addCallback(new ConfigCallback())  //xuameng配置中心
                 .commit();
         AutoSizeConfig.getInstance().setCustomFragment(true).getUnitsManager()
                 .setSupportDP(false)
