672f0af6be2128dc9cb16f522956ba535b50d64b
xuameng <142598141+xuameng@users.noreply.github.com>
2026-04-09 12:19:38 +0800

    Update HistoryActivity.java

--- app/src/main/java/com/github/tvbox/osc/ui/activity/HistoryActivity.java
+++ app/src/main/java/com/github/tvbox/osc/ui/activity/HistoryActivity.java
@@ -27,6 +27,8 @@ import org.greenrobot.eventbus.ThreadMode;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.squareup.picasso.Picasso; //xuameng清空Picasso
+
 /**
  * @author pj567
  * @date :2021/1/7
@@ -174,6 +176,7 @@ public class HistoryActivity extends BaseActivity {
     @Override
     protected void onDestroy() {
         super.onDestroy();
+        Picasso.get().cancelTag("image_load"); //xuameng清空Picasso
         EventBus.getDefault().unregister(this);
     }
 
