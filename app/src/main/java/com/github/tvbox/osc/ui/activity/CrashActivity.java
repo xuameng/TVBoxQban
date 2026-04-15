98c819e26658564f0898589b43499924734daf34
xuameng <142598141+xuameng@users.noreply.github.com>
2026-04-15 14:00:35 +0800

    Update CrashActivity.java

--- app/src/main/java/com/github/tvbox/osc/ui/activity/CrashActivity.java
+++ app/src/main/java/com/github/tvbox/osc/ui/activity/CrashActivity.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.view.KeyEvent;
 import android.view.View;
 import android.widget.ProgressBar;
