6f04288b8f9adabf06b66093f47ac8ff23612feb
xuameng <142598141+xuameng@users.noreply.github.com>
2026-04-16 19:38:33 +0800

    Update RefreshEvent.java

--- app/src/main/java/com/github/tvbox/osc/event/RefreshEvent.java
+++ app/src/main/java/com/github/tvbox/osc/event/RefreshEvent.java
@@ -18,7 +18,7 @@ public class RefreshEvent {
     public static final int TYPE_API_URL_CHANGE = 8;
     public static final int TYPE_PUSH_URL = 9;
     public static final int TYPE_EPG_URL_CHANGE = 10;
-    public static final int TYPE_SETTING_SEARCH_TV = 11;
+//没用了留着    public static final int TYPE_SETTING_SEARCH_TV = 11;
     public static final int TYPE_SUBTITLE_SIZE_CHANGE = 12;
     public static final int TYPE_FILTER_CHANGE = 13;
     public static final int TYPE_PAUSE_VOD = 14;   //xuameng 全屏时如果是暂停状态就显示暂停图标
