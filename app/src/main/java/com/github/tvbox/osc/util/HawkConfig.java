0464864da73b7dcbdf2d984cabf67afcc1ee4b67
xuameng <142598141+xuameng@users.noreply.github.com>
2026-04-16 21:58:35 +0800

    Update HawkConfig.java

--- app/src/main/java/com/github/tvbox/osc/util/HawkConfig.java
+++ app/src/main/java/com/github/tvbox/osc/util/HawkConfig.java
@@ -61,8 +61,7 @@ public class HawkConfig {
 	public static final String VOD_SWITCHPLAYER = "vod_switchplayer";  //xuameng播放器切换
     public static final String LIVE_FAVORITE_CHANNELS = "live_favorite_channels"; // xuameng存储收藏频道列表的键
 	public static final String SUBTITLE_TEXT_STYLE = "subtitle_text_style";   //xuameng 存储字幕颜色信息
-
-	public static final String REMOTE_TV_LIST = "remote_tv_list";
+	public static final String REMOTE_TV_LIST = "remote_tv_list";  //xuameng  存储远端聚汇影视列表
 
 	public static boolean intLIVEPLAYTYPE = false;  //xuameng是否有直播默认播放器
 	public static boolean intSYSplayer = false;  //xuameng是否进入系统播放器
