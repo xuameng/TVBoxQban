ec64882fac331ffe4efbe27a509307b4c8536f42
xuameng <142598141+xuameng@users.noreply.github.com>
2026-04-28 11:53:03 +0800

    Update PlayActivity.java

--- app/src/main/java/com/github/tvbox/osc/ui/activity/PlayActivity.java
+++ app/src/main/java/com/github/tvbox/osc/ui/activity/PlayActivity.java
@@ -654,9 +654,6 @@ public class PlayActivity extends BaseActivity {
                     mVideoView.release();
                     if (finalUrl != null) {
                         String url = finalUrl;
-                        if (url.startsWith("push://") && ApiConfig.get().getSource("push_agent") != null) {  //xuameng 如是推送链接直接返回由detailactivity重新解析
-                            return;
-                        }
                         try {
                             int playerType = mVodPlayerCfg.getInt("pl");
                             if (playerType >= 10) {
