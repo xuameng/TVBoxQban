a119467162609835f22f71cde1d5bd9d71388eab
xuameng <142598141+xuameng@users.noreply.github.com>
2026-05-08 13:47:23 +0800

    Update HomeActivity.java

--- app/src/main/java/com/github/tvbox/osc/ui/activity/HomeActivity.java
+++ app/src/main/java/com/github/tvbox/osc/ui/activity/HomeActivity.java
@@ -216,7 +216,7 @@ public class HomeActivity extends BaseActivity {
                     safeUpdateSortAdapterSelection(position, tvRecyclerView);
                     if (!isFinishing() && isGridViewSafe()) {  //xuameng安全检查
                         mHandler.removeCallbacks(mDataRunnable);
-                        mHandler.postDelayed(mDataRunnable, 5000);  //xuameng 延迟到下一个主线程周期执行
+                        mHandler.post(mDataRunnable);  //xuameng 延迟到下一个主线程周期执行
                     }
                 }
             }
