70d08f25d50f48118b380900656673c704fad93d
xuameng <142598141+xuameng@users.noreply.github.com>
2026-05-08 14:58:15 +0800

    Update GridFragment.java

--- app/src/main/java/com/github/tvbox/osc/ui/fragment/GridFragment.java
+++ app/src/main/java/com/github/tvbox/osc/ui/fragment/GridFragment.java
@@ -358,6 +358,11 @@ public class GridFragment extends BaseLazyFragment {
                         gridAdapter.loadMoreComplete();
                         gridAdapter.setEnableLoadMore(true);
                     }
+			new AlertDialog.Builder(getContext())
+        .setTitle("AbsXml 详情")
+        .setMessage(absXmlToText(absXml))
+        .setPositiveButton("关闭", null)
+        .show();
                 } else {
                     if (page == 1) {
                         showEmpty();
@@ -411,17 +416,10 @@ public class GridFragment extends BaseLazyFragment {
     private View.OnFocusChangeListener focusChangeListener = new View.OnFocusChangeListener() {     //xuameng触碰变大
         @Override
         public void onFocusChange(View v, boolean hasFocus) {
-            if (hasFocus){
+            if (hasFocus)
                 v.animate().scaleX(1.05f).scaleY(1.05f).setDuration(300).setInterpolator(new BounceInterpolator()).start();
-			new AlertDialog.Builder(this)
-        .setTitle("AbsXml 详情")
-        .setMessage(absXmlToText(absXml))
-        .setPositiveButton("关闭", null)
-        .show();
-			}
-            else{
+            else
                 v.animate().scaleX(1.0f).scaleY(1.0f).setDuration(300).setInterpolator(new BounceInterpolator()).start();
-			}
         }
     };
 
