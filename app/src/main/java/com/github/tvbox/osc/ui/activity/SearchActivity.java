25b1e7079f249fab1c262b3c60e0daedf72c5c21
xuameng <142598141+xuameng@users.noreply.github.com>
2026-06-06 22:20:01 +0800

    Update SearchActivity.java

--- app/src/main/java/com/github/tvbox/osc/ui/activity/SearchActivity.java
+++ app/src/main/java/com/github/tvbox/osc/ui/activity/SearchActivity.java
@@ -285,6 +285,7 @@ public class SearchActivity extends BaseActivity {
         tvSearch.setOnLongClickListener(new View.OnLongClickListener() {
             @Override
             public boolean onLongClick(View v) {
+                FastClickCheckUtil.check(view);
                 showHotSearchtext();   //xuameng 刷新搜索热词
                 App.showToastShort(SearchActivity.this, "热门搜索已刷新！");
                 return true;
@@ -459,6 +460,7 @@ public class SearchActivity extends BaseActivity {
                     child.setOnLongClickListener(new View.OnLongClickListener() {
                         @Override
                         public boolean onLongClick(View v) {
+                            FastClickCheckUtil.check(view);
                             // 直接执行删除操作，不弹出对话框
                             // 1. 从数据库删除数据
                             boolean success = searchPresenter.deleteKeyWordsFromDb(keywordToDelete);
