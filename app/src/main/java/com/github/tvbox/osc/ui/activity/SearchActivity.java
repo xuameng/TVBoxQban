5f9f386b22c7d1f728d104cc22ad4df7968f26c9
xuameng <142598141+xuameng@users.noreply.github.com>
2026-06-24 12:27:01 +0800

    Update SearchActivity.java

--- app/src/main/java/com/github/tvbox/osc/ui/activity/SearchActivity.java
+++ app/src/main/java/com/github/tvbox/osc/ui/activity/SearchActivity.java
@@ -254,6 +254,7 @@ public class SearchActivity extends BaseActivity {
                     isSearchBack = true;
                     Bundle bundle = new Bundle();
                     bundle.putString("id", video.id);
+                    bundle.putString("title", video.name);
                     bundle.putString("picture", video.pic);   //xuameng某些网站图片部显示
                     bundle.putString("sourceKey", video.sourceKey);
                     jumpActivity(DetailActivity.class, bundle);
