731ccb88dbe3446d4a4f6f1d93ccea340fdbde9c
xuameng <142598141+xuameng@users.noreply.github.com>
2026-06-27 11:08:47 +0800

    Update CollectActivity.java

--- app/src/main/java/com/github/tvbox/osc/ui/activity/CollectActivity.java
+++ app/src/main/java/com/github/tvbox/osc/ui/activity/CollectActivity.java
@@ -121,8 +121,8 @@ public class CollectActivity extends BaseActivity {
                         bundle.putString("id", vodInfo.vodId);
                         bundle.putString("sourceKey", vodInfo.sourceKey);
                         if (ApiConfig.get().getSource(vodInfo.sourceKey) != null) {
-                            bundle.putString("title", vodInfo.name);
-                            bundle.putString("picture", vodInfo.pic);
+                            bundle.putString("title", vodInfo.name);  //xuameng 转送名称
+                            bundle.putString("picture", vodInfo.pic);  //xuameng 转送图片
                             jumpActivity(DetailActivity.class, bundle);
                         }else {
                             bundle.putString("title", vodInfo.name);
