a3d53a5ce8bbd51f745ef000ecc67e807452aa49
xuameng <142598141+xuameng@users.noreply.github.com>
2026-06-27 14:20:25 +0800

    Update SourceViewModel.java

--- app/src/main/java/com/github/tvbox/osc/viewmodel/SourceViewModel.java
+++ app/src/main/java/com/github/tvbox/osc/viewmodel/SourceViewModel.java
@@ -5,6 +5,7 @@ import android.text.TextUtils;
 import android.util.Base64;
 import android.os.Handler;
 import android.os.Looper;
+import android.util.Log;
 
 import androidx.annotation.NonNull;
 import androidx.lifecycle.MutableLiveData;
