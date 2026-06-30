be754053e60d6bea3d71e760f9b8884688ba7c38
xuameng <142598141+xuameng@users.noreply.github.com>
2026-06-30 10:48:55 +0800

    Update ApiConfig.java

--- app/src/main/java/com/github/tvbox/osc/api/ApiConfig.java
+++ app/src/main/java/com/github/tvbox/osc/api/ApiConfig.java
@@ -865,7 +865,7 @@ public class ApiConfig {
                             ArrayList<LiveSettingItem> liveSettingItemList = new ArrayList<>();
                             for (int i = 0; i < livesGroups.size(); i++) {
                                 JsonObject jsonObject = livesGroups.get(i).getAsJsonObject();
-                                String name = jsonObject.has("name") ? jsonObject.get("name").getAsString() : "聚汇直播" + (i + 1);
+                                String name = jsonObject.has("name") ? jsonObject.get("name").getAsString() : "聚汇直播";
                                 if (name == null || name.isEmpty()) {
                                     name = "聚汇直播";
                                 }
@@ -889,7 +889,7 @@ public class ApiConfig {
                             ArrayList<LiveSettingItem> liveSettingItemList = new ArrayList<>();
                             for (int i = 0; i < livesGroups.size(); i++) {
                                 JsonObject jsonObject = livesGroups.get(i).getAsJsonObject();
-                                String name = jsonObject.has("name") ? jsonObject.get("name").getAsString() : "聚汇直播" + (i + 1);
+                                String name = jsonObject.has("name") ? jsonObject.get("name").getAsString() : "聚汇直播";
                                 if (name == null || name.isEmpty()) {
                                     name = "聚汇直播";
                                 }
@@ -1023,7 +1023,7 @@ public class ApiConfig {
 
     private void loadDefaultConfig() {
         String defaultIJKADS="{\"ijk\":[{\"options\":[{\"name\":\"opensles\",\"category\":4,\"value\":\"0\"},{\"name\":\"framedrop\",\"category\":4,\"value\":\"1\"},{\"name\":\"soundtouch\",\"category\":4,\"value\":\"1\"},{\"name\":\"start-on-prepared\",\"category\":4,\"value\":\"1\"},{\"name\":\"http-detect-rangeupport\",\"category\":1,\"value\":\"0\"},{\"name\":\"fflags\",\"category\":1,\"value\":\"fastseek\"},{\"name\":\"skip_loop_filter\",\"category\":2,\"value\":\"48\"},{\"name\":\"reconnect\",\"category\":4,\"value\":\"1\"},{\"name\":\"enable-accurate-seek\",\"category\":4,\"value\":\"0\"},{\"name\":\"mediacodec\",\"category\":4,\"value\":\"0\"},{\"name\":\"mediacodec-all-videos\",\"category\":4,\"value\":\"0\"},{\"name\":\"mediacodec-auto-rotate\",\"category\":4,\"value\":\"0\"},{\"name\":\"mediacodec-handle-resolution-change\",\"category\":4,\"value\":\"0\"},{\"name\":\"mediacodec-hevc\",\"category\":4,\"value\":\"0\"},{\"name\":\"max-buffer-size\",\"category\":4,\"value\":\"15728640\"}],\"group\":\"软解码\"},{\"options\":[{\"name\":\"opensles\",\"category\":4,\"value\":\"0\"},{\"name\":\"framedrop\",\"category\":4,\"value\":\"1\"},{\"name\":\"soundtouch\",\"category\":4,\"value\":\"1\"},{\"name\":\"start-on-prepared\",\"category\":4,\"value\":\"1\"},{\"name\":\"http-detect-rangeupport\",\"category\":1,\"value\":\"0\"},{\"name\":\"fflags\",\"category\":1,\"value\":\"fastseek\"},{\"name\":\"skip_loop_filter\",\"category\":2,\"value\":\"48\"},{\"name\":\"reconnect\",\"category\":4,\"value\":\"1\"},{\"name\":\"enable-accurate-seek\",\"category\":4,\"value\":\"0\"},{\"name\":\"mediacodec\",\"category\":4,\"value\":\"1\"},{\"name\":\"mediacodec-all-videos\",\"category\":4,\"value\":\"1\"},{\"name\":\"mediacodec-auto-rotate\",\"category\":4,\"value\":\"1\"},{\"name\":\"mediacodec-handle-resolution-change\",\"category\":4,\"value\":\"1\"},{\"name\":\"mediacodec-hevc\",\"category\":4,\"value\":\"1\"},{\"name\":\"max-buffer-size\",\"category\":4,\"value\":\"15728640\"}],\"group\":\"硬解码\"}],\"ads\":[\"mimg.0c1q0l.cn\",\"www.googletagmanager.com\",\"www.google-analytics.com\",\"mc.usihnbcq.cn\",\"mg.g1mm3d.cn\",\"mscs.svaeuzh.cn\",\"cnzz.hhttm.top\",\"tp.vinuxhome.com\",\"cnzz.mmstat.com\",\"www.baihuillq.com\",\"s23.cnzz.com\",\"z3.cnzz.com\",\"c.cnzz.com\",\"stj.v1vo.top\",\"z12.cnzz.com\",\"img.mosflower.cn\",\"tips.gamevvip.com\",\"ehwe.yhdtns.com\",\"xdn.cqqc3.com\",\"www.jixunkyy.cn\",\"sp.chemacid.cn\",\"hm.baidu.com\",\"s9.cnzz.com\",\"z6.cnzz.com\",\"um.cavuc.com\",\"mav.mavuz.com\",\"wofwk.aoidf3.com\",\"z5.cnzz.com\",\"xc.hubeijieshikj.cn\",\"tj.tianwenhu.com\",\"xg.gars57.cn\",\"k.jinxiuzhilv.com\",\"cdn.bootcss.com\",\"ppl.xunzhuo123.com\",\"xomk.jiangjunmh.top\",\"img.xunzhuo123.com\",\"z1.cnzz.com\",\"s13.cnzz.com\",\"xg.huataisangao.cn\",\"z7.cnzz.com\",\"xg.huataisangao.cn\",\"z2.cnzz.com\",\"s96.cnzz.com\",\"q11.cnzz.com\",\"thy.dacedsfa.cn\",\"xg.whsbpw.cn\",\"s19.cnzz.com\",\"z8.cnzz.com\",\"s4.cnzz.com\",\"f5w.as12df.top\",\"ae01.alicdn.com\",\"www.92424.cn\",\"k.wudejia.com\",\"vivovip.mmszxc.top\",\"qiu.xixiqiu.com\",\"cdnjs.hnfenxun.com\",\"cms.qdwght.com\"]}";
-        JsonObject defaultJson = gson.fromJson(defaultIJKADS, JsonObject.class);
+        JsonObject defaultJson=gson.fromJson(defaultIJKADS, JsonObject.class);
         // 广告
         if (AdBlocker.isEmpty()) {
             // 默认广告
@@ -1169,7 +1169,7 @@ public class ApiConfig {
                         ArrayList<LiveSettingItem> liveSettingItemList = new ArrayList<>();
                         for (int i = 0; i < livesGroups.size(); i++) {
                             JsonObject jsonObject = livesGroups.get(i).getAsJsonObject();
-                            String name = jsonObject.has("name") ? jsonObject.get("name").getAsString() : "聚汇直播" + (i + 1);
+                            String name = jsonObject.has("name") ? jsonObject.get("name").getAsString() : "聚汇直播";
                             if (name == null || name.isEmpty()) {
                                 name = "聚汇直播";
                             }
@@ -1193,7 +1193,7 @@ public class ApiConfig {
                         ArrayList<LiveSettingItem> liveSettingItemList = new ArrayList<>();
                         for (int i = 0; i < livesGroups.size(); i++) {
                             JsonObject jsonObject = livesGroups.get(i).getAsJsonObject();
-                            String name = jsonObject.has("name") ? jsonObject.get("name").getAsString() : "聚汇直播" + (i + 1);
+                            String name = jsonObject.has("name") ? jsonObject.get("name").getAsString() : "聚汇直播";
                             if (name == null || name.isEmpty()) {
                                 name = "聚汇直播";
                             }
