2d1aad551483ceb4a41c7c96a577c7b3a31bdda2
xuameng <142598141+xuameng@users.noreply.github.com>
2026-07-07 17:13:54 +0800

    Update VodController.java

--- app/src/main/java/com/github/tvbox/osc/player/controller/VodController.java
+++ app/src/main/java/com/github/tvbox/osc/player/controller/VodController.java
@@ -1812,7 +1812,6 @@ public class VodController extends BaseController {
                 initLandscapePortraitBtnInfo();
                 listener.hideTipXu(); //xuameng 只要播放就隐藏错误信息
                 startProgress();
-                EventBus.getDefault().post(new RefreshEvent(RefreshEvent.TYPE_SET_DANMU_SETTINGS, true));           
                 break;
             case VideoView.STATE_PAUSED:
                 isVideoPlay = false;
@@ -1840,7 +1839,7 @@ public class VodController extends BaseController {
                 String width = Integer.toString(mControlWrapper.getVideoSize()[0]);
                 String height = Integer.toString(mControlWrapper.getVideoSize()[1]);
                 mVideoSize.setText("[ " + width + " X " + height + " ]");
-                initialVisualizer();    
+                initialVisualizer();
                 break;
             case VideoView.STATE_BUFFERED:
                 mPlayLoadNetSpeed.setVisibility(View.GONE);
