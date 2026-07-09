428ba114db184cac690a9e29ea05d903af7c6774
xuameng <142598141+xuameng@users.noreply.github.com>
2026-07-09 17:17:50 +0800

    Update GridFragment.java

--- app/src/main/java/com/github/tvbox/osc/ui/fragment/GridFragment.java
+++ app/src/main/java/com/github/tvbox/osc/ui/fragment/GridFragment.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.animation.BounceInterpolator;
+import android.view.MotionEvent;
+import android.view.ViewConfiguration;
 
 import androidx.core.content.ContextCompat;
 import androidx.lifecycle.Observer;
@@ -73,6 +75,12 @@ public class GridFragment extends BaseLazyFragment {
     private View focusedView = null;
     private boolean isRequesting = false;
     private boolean hasActionItems = false;
+    private float pullRefreshStartX;
+    private float pullRefreshStartY;
+    private boolean pullRefreshStartAtTop = false;
+    private boolean pullRefreshReady = false;
+    private int pullRefreshThreshold = 0;
+    private View loadSirView = null;
 
     private static class GridInfo{
         public String sortID="";
@@ -332,6 +340,83 @@ public class GridFragment extends BaseLazyFragment {
         });
         gridAdapter.setLoadMoreView(new LoadMoreView());
         setLoadSir2(mGridView);
+        initPullRefresh();
+    }
+
+    private void initPullRefresh() {
+        pullRefreshThreshold = ViewConfiguration.get(mContext).getScaledTouchSlop() * 6;
+        mGridView.addOnItemTouchListener(new RecyclerView.OnItemTouchListener() {
+            @Override
+            public boolean onInterceptTouchEvent(@NonNull RecyclerView rv, @NonNull MotionEvent event) {
+                return handlePullRefreshTouch(rv, event);
+            }
+
+            @Override
+            public void onTouchEvent(@NonNull RecyclerView rv, @NonNull MotionEvent event) {
+            }
+
+            @Override
+            public void onRequestDisallowInterceptTouchEvent(boolean disallowIntercept) {
+            }
+        });
+        loadSirView = (View) mGridView.getParent();
+        if (loadSirView != null) {
+            loadSirView.setOnTouchListener(new View.OnTouchListener() {
+                @Override
+                public boolean onTouch(View v, MotionEvent event) {
+                    return handlePullRefreshTouch(v, event);
+                }
+            });
+        }
+    }
+
+    private void bindPullRefreshTouch(View view) {
+        if (view == null || view == mGridView) return;
+        view.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                return handlePullRefreshTouch(v, event);
+            }
+        });
+        if (view instanceof ViewGroup) {
+            ViewGroup group = (ViewGroup) view;
+            for (int i = 0; i < group.getChildCount(); i++) {
+                bindPullRefreshTouch(group.getChildAt(i));
+            }
+        }
+    }
+
+    private boolean handlePullRefreshTouch(View view, MotionEvent event) {
+        switch (event.getActionMasked()) {
+            case MotionEvent.ACTION_DOWN:
+                pullRefreshStartX = event.getX();
+                pullRefreshStartY = event.getY();
+                pullRefreshStartAtTop = !view.canScrollVertically(-1);
+                pullRefreshReady = false;
+                break;
+            case MotionEvent.ACTION_MOVE:
+                float diffX = Math.abs(event.getX() - pullRefreshStartX);
+                float diffY = event.getY() - pullRefreshStartY;
+                pullRefreshReady = pullRefreshStartAtTop && diffY > pullRefreshThreshold && diffY > diffX;
+                break;
+            case MotionEvent.ACTION_UP:
+                if (pullRefreshReady) {
+                    pullRefreshReady = false;
+                    forceRefresh();
+                    return true;
+                }
+                break;
+            case MotionEvent.ACTION_CANCEL:
+                pullRefreshReady = false;
+                break;
+        }
+        return false;
+    }
+
+    @Override
+    protected void showEmpty() {
+        super.showEmpty();
+        bindPullRefreshTouch(loadSirView);
     }
 
     private void initViewModel() {
