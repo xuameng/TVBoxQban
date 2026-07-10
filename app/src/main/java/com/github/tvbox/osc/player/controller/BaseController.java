340652ea696a5882f926ed0d75a339fce8adb7d6
xuameng <142598141+xuameng@users.noreply.github.com>
2026-07-10 12:37:49 +0800

    Update BaseController.java

--- app/src/main/java/com/github/tvbox/osc/player/controller/BaseController.java
+++ app/src/main/java/com/github/tvbox/osc/player/controller/BaseController.java
@@ -45,7 +45,9 @@ public abstract class BaseController extends BaseVideoController implements Gest
     private boolean mCanSlide;
     private int mCurPlayState;
 
-    private boolean playspeed = false;  //xuameng 判断快进是否显示
+    public void setPlayspeed(boolean playspeed) {  //xuameng 判断快进是否显示
+        this.playspeed = playspeed;
+    }
 
     protected Handler mHandler;
 
@@ -334,7 +336,7 @@ public abstract class BaseController extends BaseVideoController implements Gest
     }
 
     protected void slideToChangePosition(float deltaX) {
-		if (playspeed){  //xuameng 判断快进显示就返回
+		if (setPlayspeed){  //xuameng 判断快进显示就返回
             return;
         }
         deltaX = -deltaX;
