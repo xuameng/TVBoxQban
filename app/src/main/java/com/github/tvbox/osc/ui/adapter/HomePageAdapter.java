2693d2193f8dc110619363e9a16c1af9fdf7ba6d
xuameng <142598141+xuameng@users.noreply.github.com>
2026-07-10 14:35:27 +0800

    Update HomePageAdapter.java

--- app/src/main/java/com/github/tvbox/osc/ui/adapter/HomePageAdapter.java
+++ app/src/main/java/com/github/tvbox/osc/ui/adapter/HomePageAdapter.java
@@ -5,6 +5,7 @@ import android.view.ViewGroup;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.fragment.app.FragmentTransaction;
 
 import com.github.tvbox.osc.base.BaseLazyFragment;
 
