4c058d883028f62dab0058087a25fd242943e915
xuameng <142598141+xuameng@users.noreply.github.com>
2026-07-26 12:43:34 +0800

    Update ApiConfig.java

--- app/src/main/java/com/github/tvbox/osc/api/ApiConfig.java
+++ app/src/main/java/com/github/tvbox/osc/api/ApiConfig.java
@@ -331,7 +331,7 @@ public class ApiConfig {
     public boolean shouldReloadLiveConfig() {
         String liveApiUrl = Hawk.get(HawkConfig.LIVE_API_URL, "");
         if (liveApiUrl.isEmpty()) liveApiUrl = loadedLiveConfigUrl;
-        return liveChannelGroupList == null || liveChannelGroupList.isEmpty() || !liveApiUrl.equals(loadedLiveConfigUrl);
+        return !liveApiUrl.equals(loadedLiveConfigUrl);
     }
 
     private static final int LOAD_JAR_MAX_RETRY = 1;
