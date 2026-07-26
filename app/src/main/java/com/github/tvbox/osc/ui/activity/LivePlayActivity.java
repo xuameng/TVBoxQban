469955ff25b50cecc11b70fa8f73f32297f89c66
xuameng <142598141+xuameng@users.noreply.github.com>
2026-07-26 13:18:07 +0800

    Update LivePlayActivity.java

--- app/src/main/java/com/github/tvbox/osc/ui/activity/LivePlayActivity.java
+++ app/src/main/java/com/github/tvbox/osc/ui/activity/LivePlayActivity.java
@@ -3162,7 +3162,7 @@ public class LivePlayActivity extends BaseActivity {
         if (loadingLiveConfigOnEnter) return;
         loadingLiveConfigOnEnter = true;
         showLoading();
-        ApiConfig.get().loadLiveConfig(false, new ApiConfig.LoadConfigCallback() {
+        ApiConfig.get().loadLiveConfig(true, new ApiConfig.LoadConfigCallback() {
             @Override
             public void success() {
                 mHandler.post(new Runnable() {
