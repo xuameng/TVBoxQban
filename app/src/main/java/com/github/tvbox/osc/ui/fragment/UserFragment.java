f80f9392ba5b67f2c71a02238e26d6e0db4300f2
xuameng <142598141+xuameng@users.noreply.github.com>
2026-08-04 14:13:33 +0800

    Update UserFragment.java

--- app/src/main/java/com/github/tvbox/osc/ui/fragment/UserFragment.java
+++ app/src/main/java/com/github/tvbox/osc/ui/fragment/UserFragment.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.animation.BounceInterpolator;
+import android.view.MotionEvent;
+import android.view.ViewConfiguration;
 import android.widget.LinearLayout;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
