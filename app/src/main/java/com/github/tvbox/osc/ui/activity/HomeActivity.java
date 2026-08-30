9474e25b1690c4166d490c45cdc4c77e9fd4890f
xuameng <142598141+xuameng@users.noreply.github.com>
2026-08-30 13:52:51 +0800

    Update HomeActivity.java

--- app/src/main/java/com/github/tvbox/osc/ui/activity/HomeActivity.java
+++ app/src/main/java/com/github/tvbox/osc/ui/activity/HomeActivity.java
@@ -880,12 +880,13 @@ public class HomeActivity extends BaseActivity {
 
     @Override
     protected void onDestroy() {
-        super.onDestroy();
         dismissHomeDialogs();
         mHandler.removeCallbacksAndMessages(null);
+        super.onDestroy();
         EventBus.getDefault().unregister(this);
-        AppManager.getInstance().appExit(0);
-        ControlManager.get().stopServer();
+        if (isFinishing()) {
+            ControlManager.get().stopServer();
+        }
     }
 
     private SelectDialog<SourceBean> mSiteSwitchDialog;
