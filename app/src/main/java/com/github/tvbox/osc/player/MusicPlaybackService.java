e7d802c12ce67ca811aa53624571f0c85bc5f730
xuameng <142598141+xuameng@users.noreply.github.com>
2026-09-02 15:26:21 +0800

    Update MusicPlaybackService.java

--- app/src/main/java/com/github/tvbox/osc/player/MusicPlaybackService.java
+++ app/src/main/java/com/github/tvbox/osc/player/MusicPlaybackService.java
@@ -135,7 +135,7 @@ public class MusicPlaybackService extends Service {
                         pauseForSwitch();
                         fragment.playPrevious();
                     } else { 
-                        App.showToastShort(getApplicationContext(), "已经是第一集了！");
+                        App.showToastShort(MusicPlaybackService.this, "已经是第一集了！");
                     }
                 }
             }
@@ -148,7 +148,7 @@ public class MusicPlaybackService extends Service {
                         pauseForSwitch();
                         fragment.playNext(false);
                     } else { 
-                        App.showToastShort(getApplicationContext(), "已经是最后一集了！");
+                        App.showToastShort(MusicPlaybackService.this, "已经是最后一集了！");
                     }
                 }
             }
@@ -206,7 +206,7 @@ public class MusicPlaybackService extends Service {
                     pauseForSwitch();
                     fragment.playPrevious();
                 } else { 
-                    App.showToastShort(getApplicationContext(), "已经是第一集了！");
+                    App.showToastShort(MusicPlaybackService.this, "已经是第一集了！");
                 }
             }
             return;
@@ -218,7 +218,7 @@ public class MusicPlaybackService extends Service {
                     pauseForSwitch();
                     fragment.playNext(false);
                 } else { 
-                    App.showToastShort(getApplicationContext(), "已经是最后一集了！");
+                    App.showToastShort(MusicPlaybackService.this, "已经是最后一集了！");
                 }
             }
             return;
