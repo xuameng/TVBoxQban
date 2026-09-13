5a78606e554162e848d1bcf090a0ea2db56fb600
xuameng <142598141+xuameng@users.noreply.github.com>
2026-09-13 13:20:16 +0800

    Update SearchDanmuAdapter.java

--- app/src/main/java/com/github/tvbox/osc/ui/adapter/SearchDanmuAdapter.java
+++ app/src/main/java/com/github/tvbox/osc/ui/adapter/SearchDanmuAdapter.java
@@ -1,7 +1,5 @@
 package com.github.tvbox.osc.ui.adapter;
 
-import android.view.KeyEvent;
-
 import com.chad.library.adapter.base.BaseQuickAdapter;
 import com.chad.library.adapter.base.BaseViewHolder;
 import com.github.tvbox.osc.R;
@@ -18,17 +16,5 @@ public class SearchDanmuAdapter extends BaseQuickAdapter<DanmuSearchResult, Base
     @Override
     protected void convert(BaseViewHolder helper, DanmuSearchResult item) {
         helper.setText(R.id.danmuName, item.getName());
-        helper.addOnClickListener(R.id.danmuItem);
-        helper.getView(R.id.danmuItem).setOnKeyListener((view, keyCode, event) -> {
-            if (event.getAction() == KeyEvent.ACTION_UP
-                    && (keyCode == KeyEvent.KEYCODE_ENTER
-                    || keyCode == KeyEvent.KEYCODE_DPAD_CENTER
-                    || keyCode == KeyEvent.KEYCODE_NUMPAD_ENTER
-                    || keyCode == KeyEvent.KEYCODE_BUTTON_A)) {
-                view.performClick();
-                return true;
-            }
-            return false;
-        });
     }
 }
