641abc84ad6eb317368193ec8a60cb43630b83a0
xuameng <142598141+xuameng@users.noreply.github.com>
2026-09-13 13:16:33 +0800

    Update DanmuLoadController.java

--- app/src/main/java/com/github/tvbox/osc/player/danmu/DanmuLoadController.java
+++ app/src/main/java/com/github/tvbox/osc/player/danmu/DanmuLoadController.java
@@ -80,6 +80,7 @@ public class DanmuLoadController {
         danmakuContext.setDanmakuStyle(IDisplayer.DANMAKU_STYLE_STROKEN, 3)
                 .setDanmakuMargin(8);
         if (reload && !TextUtils.isEmpty(danmuText) && DanmuHelper.isOpen()) {
+            temporarilyClosed = false;  //xuameng 必须加 重置 临时弹幕状态判断 
             prepare(danmuText);
         }
     }
