fae569ec11cd7b02cfe6c31e4c3608124f4e9f77
xuameng <142598141+xuameng@users.noreply.github.com>
2026-09-15 18:26:46 +0800

    Update VodRecordPath.java

--- app/src/main/java/com/github/tvbox/osc/cache/VodRecordPath.java
+++ app/src/main/java/com/github/tvbox/osc/cache/VodRecordPath.java
@@ -2,6 +2,12 @@ package com.github.tvbox.osc.cache;
 
 import androidx.room.ColumnInfo;
 
+/**
+ * @author xuameng
+ * @since 2026/9/15
+ * 历史列表专用：只查轻量字段，dataJson以文件形式存储 解决大列表数据库崩溃
+ */
+
 public class VodRecordPath {
     @ColumnInfo(name = "id")
     public int id;
