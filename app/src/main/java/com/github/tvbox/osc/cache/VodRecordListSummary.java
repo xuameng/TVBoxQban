cf10e16adfc5dbf72c9f0d1ef9d5a9755de992bc
xuameng <142598141+xuameng@users.noreply.github.com>
2026-09-15 17:22:04 +0800

    Add files via upload

---
+++ app/src/main/java/com/github/tvbox/osc/cache/VodRecordListSummary.java
@@ -0,0 +1,13 @@
+package com.github.tvbox.osc.cache;
+
+import androidx.room.ColumnInfo;
+
+public class VodRecordListSummary {
+    public int id;
+    public String vodId;
+    public long updateTime;
+    public String sourceKey;
+    public String vodName;
+    public String vodPic;
+    public String playNote;
+}
