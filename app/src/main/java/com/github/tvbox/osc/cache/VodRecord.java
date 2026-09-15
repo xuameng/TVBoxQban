31704271b9292b058f601f63acced075068b591a
xuameng <142598141+xuameng@users.noreply.github.com>
2026-09-15 18:32:11 +0800

    Update VodRecord.java

--- app/src/main/java/com/github/tvbox/osc/cache/VodRecord.java
+++ app/src/main/java/com/github/tvbox/osc/cache/VodRecord.java
@@ -9,7 +9,7 @@ import java.io.Serializable;
 /**
  * @author xuameng
  * @since 2026/9/15
- * 历史列表专用：只查轻量字段，不碰 dataJson
+ * 历史列表专用：只查轻量字段，dataJson以文件形式存储 解决大列表数据库崩溃
  */
 
 @Entity(tableName = "vodRecord")
