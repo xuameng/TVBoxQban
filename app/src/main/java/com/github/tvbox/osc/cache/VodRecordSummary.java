45eb374793cd7c2b8183d9136834ce95e775dcb2
xuameng <142598141+xuameng@users.noreply.github.com>
2026-09-15 15:29:30 +0800

    Update VodRecordSummary.java

--- app/src/main/java/com/github/tvbox/osc/cache/VodRecordSummary.java
+++ app/src/main/java/com/github/tvbox/osc/cache/VodRecordSummary.java
@@ -2,28 +2,17 @@ package com.github.tvbox.osc.cache;
 
 import androidx.room.ColumnInfo;
 
-/**
+/* @author xuameng
+ * @since 2026/9/15
  * 历史列表专用轻量 POJO，不映射到表
  */
+
 public class VodRecordSummary {
-    @ColumnInfo(name = "id")
     public int id;
-
-    @ColumnInfo(name = "vodId")
     public String vodId;
-
-    @ColumnInfo(name = "updateTime")
     public long updateTime;
-
-    @ColumnInfo(name = "sourceKey")
     public String sourceKey;
-
-    @ColumnInfo(name = "vodName")
     public String vodName;
-
-    @ColumnInfo(name = "vodPic")
     public String vodPic;
-
-    @ColumnInfo(name = "playNote")
     public String playNote;
 }
