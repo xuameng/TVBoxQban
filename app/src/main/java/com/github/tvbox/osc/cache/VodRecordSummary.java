9d63eb13378cba1ec62548e586c4649261a6913d
xuameng <142598141+xuameng@users.noreply.github.com>
2026-09-15 18:27:32 +0800

    Update VodRecordSummary.java

--- app/src/main/java/com/github/tvbox/osc/cache/VodRecordSummary.java
+++ app/src/main/java/com/github/tvbox/osc/cache/VodRecordSummary.java
@@ -2,9 +2,10 @@ package com.github.tvbox.osc.cache;
 
 import androidx.room.ColumnInfo;
 
-/* @author xuameng
+/**
+ * @author xuameng
  * @since 2026/9/15
- * 历史列表专用轻量 POJO，不映射到表
+ * 历史列表专用：只查轻量字段，dataJson以文件形式存储 解决大列表数据库崩溃
  */
 
 public class VodRecordSummary {
