94df2dc91ff36b29ab4cc2b671e3317e202684ff
xuameng <142598141+xuameng@users.noreply.github.com>
2026-09-16 14:12:46 +0800

    Update SearchActivity.java

--- app/src/main/java/com/github/tvbox/osc/ui/activity/SearchActivity.java
+++ app/src/main/java/com/github/tvbox/osc/ui/activity/SearchActivity.java
@@ -386,6 +386,7 @@ public class SearchActivity extends BaseActivity {
         tvSearchCheckboxBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+        initCheckedSourcesForSearch(); 
                 List<SourceBean> searchAbleSource = ApiConfig.get().getSearchSourceBeanList();
                 if (mSearchCheckboxDialog == null) {
                     mSearchCheckboxDialog = new SearchCheckboxDialog(SearchActivity.this, searchAbleSource, mCheckSources);
@@ -393,6 +394,8 @@ public class SearchActivity extends BaseActivity {
                     if(searchAbleSource.size()!=mSearchCheckboxDialog.mSourceList.size()){
                         mSearchCheckboxDialog.setMSourceList(searchAbleSource);
                     }
+
+            mSearchCheckboxDialog.refresh(mCheckSources, searchAbleSource);
                 }
                 mSearchCheckboxDialog.setOnDismissListener(new DialogInterface.OnDismissListener() {
                     @Override
@@ -634,7 +637,7 @@ public class SearchActivity extends BaseActivity {
     }
 
     private void search(String title) {
-    initCheckedSourcesForSearch(); 
+    initCheckedSourcesForSearch();
         if (TextUtils.isEmpty(title)){
             App.showToastShort(SearchActivity.this, "输入内容不能为空！");
             return;
