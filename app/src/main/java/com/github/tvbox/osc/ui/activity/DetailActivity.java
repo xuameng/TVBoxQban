475e76b99c730e78cc87fd38efadcd8e87e6e263
xuameng <142598141+xuameng@users.noreply.github.com>
2026-09-16 11:26:18 +0800

    Update DetailActivity.java

--- app/src/main/java/com/github/tvbox/osc/ui/activity/DetailActivity.java
+++ app/src/main/java/com/github/tvbox/osc/ui/activity/DetailActivity.java
@@ -1689,9 +1689,7 @@ public class DetailActivity extends BaseActivity {
         mGridView.setVisibility(fullWindows ? View.GONE : View.VISIBLE);
         mGridViewFlag.setVisibility(fullWindows ? View.GONE : View.VISIBLE);
         mSeriesGroupView.setVisibility(fullWindows ? View.GONE : View.VISIBLE);
-if (!fullWindows && mGridView != null && mGridView.getVisibility() == View.VISIBLE){
-	mGridView.requestFocus();
-}
+
         //全屏下禁用详情页几个按键的焦点 防止上键跑过来
         tvPlay.setFocusable(!fullWindows);
         tvSort.setFocusable(!fullWindows);
