77d50582c2775e42726c6fac2e89c77ddc0535f0
xuameng <142598141+xuameng@users.noreply.github.com>
2026-09-17 13:28:03 +0800

    Update DetailActivity.java

--- app/src/main/java/com/github/tvbox/osc/ui/activity/DetailActivity.java
+++ app/src/main/java/com/github/tvbox/osc/ui/activity/DetailActivity.java
@@ -957,6 +957,7 @@ public class DetailActivity extends BaseActivity {
     }
 
     private void initViewModel() {
+        disabledFocusable();
         sourceViewModel = new ViewModelProvider(this).get(SourceViewModel.class);
         sourceViewModel.detailResult.observe(this, new Observer<AbsXml>() {
             @Override
@@ -1101,6 +1102,7 @@ public class DetailActivity extends BaseActivity {
                             }
                         });
                         safeSelectMGridView(vodInfo.playIndex);
+                        enableFocusable();
 
                         tvPlay.setNextFocusUpId(R.id.mGridView);   //xuameng上面焦点是选剧集
                         tvQuickSearch.setNextFocusUpId(R.id.mGridView); 
@@ -1130,6 +1132,7 @@ public class DetailActivity extends BaseActivity {
                         if (fullWindows) {
                             exitFullPreview();
                         }
+                        enableFocusable();
                         mGridViewFlag.setVisibility(View.GONE);
                         mGridView.setVisibility(View.GONE);
                         mSeriesGroupView.setVisibility(View.GONE);
@@ -1668,6 +1671,7 @@ public class DetailActivity extends BaseActivity {
     void enterFullPreview() {
         setFullPreview(true);
         playFragment.isFullPreview(true);   //xuameng 非小窗口模式返回后播放BUG
+        disabledFocusable();
     }
 
     void exitFullPreview() {
@@ -1690,14 +1694,6 @@ public class DetailActivity extends BaseActivity {
         mGridViewFlag.setVisibility(fullWindows ? View.GONE : View.VISIBLE);
         mSeriesGroupView.setVisibility(fullWindows ? View.GONE : View.VISIBLE);
 
-        //全屏下禁用详情页几个按键的焦点 防止上键跑过来
-        tvPlay.setFocusable(!fullWindows);
-        tvSort.setFocusable(!fullWindows);
-        tvCollect.setFocusable(!fullWindows);
-        tvQuickSearch.setFocusable(!fullWindows);
-        tvDesc.setFocusable(!fullWindows);      //xuameng 内容简介
-        tvPush.setFocusable(!fullWindows);    //xuameng 远程推送
-        llPlayerFragmentContainerBlock.setFocusable(!fullWindows);
         if (!showPreview && !fullWindows && playFragment != null) {    //xuameng如果显示小窗口播放就释放视频，修复退出还显示暂停图标等图标的BUG
             try {
                 playFragment.pauseForHidden();
@@ -1830,6 +1826,7 @@ public class DetailActivity extends BaseActivity {
         // 4. 立即检查是否需要直接执行选择（避免滚动不触发）
         safeSelectMGridView(vodInfo.playIndex); 
         App.showToastShort(DetailActivity.this, "已滚动到当前播放剧集！");
+        enableFocusable();
     }
 
     private void switchTomGridViewFlag() {  //xuameng 自动滚动到当前播放源
@@ -1941,5 +1938,27 @@ public class DetailActivity extends BaseActivity {
             isPushUrl = false;
         }
     };
+
+    private void disabledFocusable() {
+        tvPlay.setFocusable(false);
+        tvSort.setFocusable(false);
+        tvCollect.setFocusable(false);
+        tvQuickSearch.setFocusable(false);
+        tvDesc.setFocusable(false); 
+        tvPush.setFocusable(false);
+        llPlayerFragmentContainerBlock.setFocusable(false);
+        ivThumb.setFocusable(false);
+    }
+
+    private void enableFocusable() {
+        tvPlay.setFocusable(!fullWindows);
+        tvSort.setFocusable(!fullWindows);
+        tvCollect.setFocusable(!fullWindows);
+        tvQuickSearch.setFocusable(!fullWindows);
+        tvDesc.setFocusable(!fullWindows);      //xuameng 内容简介
+        tvPush.setFocusable(!fullWindows);    //xuameng 远程推送
+        ivThumb.setFocusable(!fullWindows);    //xuameng 影片图片
+        llPlayerFragmentContainerBlock.setFocusable(!fullWindows);
+	}
 
 }
