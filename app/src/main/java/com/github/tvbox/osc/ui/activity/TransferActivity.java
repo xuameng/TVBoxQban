e0973c08d2808e37f904aa82e0a5bb73ebf2bacb
xuameng <142598141+xuameng@users.noreply.github.com>
2026-09-17 12:36:25 +0800

    Update TransferActivity.java

--- app/src/main/java/com/github/tvbox/osc/ui/activity/TransferActivity.java
+++ app/src/main/java/com/github/tvbox/osc/ui/activity/TransferActivity.java
@@ -28,6 +28,7 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.List;
+import java.util.ArrayList;
 import org.json.JSONObject;
 
 import com.hjq.permissions.OnPermissionCallback;
