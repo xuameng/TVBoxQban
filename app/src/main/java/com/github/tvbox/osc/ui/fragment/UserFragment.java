9e7f9748e5f156eec989d789e7091be898c2587d
xuameng <142598141+xuameng@users.noreply.github.com>
2026-09-23 14:01:11 +0800

    Update UserFragment.java

--- app/src/main/java/com/github/tvbox/osc/ui/fragment/UserFragment.java
+++ app/src/main/java/com/github/tvbox/osc/ui/fragment/UserFragment.java
@@ -352,7 +352,7 @@ public class UserFragment extends BaseLazyFragment implements View.OnClickListen
                 FastClickCheckUtil.check(v);
                 if (mActivity instanceof HomeActivity) {
                     ((HomeActivity) mActivity).refreshHome(false);
-                    App.showToastShort(mContext, "重载数据！");
+                    App.showToastShort(mContext, "主页刷新！");
                 }
                 return true;
             }
@@ -364,7 +364,7 @@ public class UserFragment extends BaseLazyFragment implements View.OnClickListen
                 FastClickCheckUtil.check(v);
                 if (mActivity instanceof HomeActivity) {
                     ((HomeActivity) mActivity).refreshHomeSort();
-                    App.showToastShort(mContext, "主页刷新！");
+                    App.showToastShort(mContext, "重载数据！");
                 }
                 return true;
             }
@@ -731,7 +731,7 @@ public class UserFragment extends BaseLazyFragment implements View.OnClickListen
                 if (pullRefreshReady) {
                     pullRefreshReady = false;
                     if (mActivity instanceof HomeActivity) {
-                        ((HomeActivity) mActivity).refreshHomeSort();
+                        ((HomeActivity) mActivity).refreshHome(false);
                         App.showToastShort(mContext, "主页刷新！");
                     }
                     return true;
