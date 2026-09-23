2dee191fe231c236469ab9c5754b3b9bb566a55c
xuameng <142598141+xuameng@users.noreply.github.com>
2026-09-23 18:27:59 +0800

    Update HomeHotVodAdapter.java

--- app/src/main/java/com/github/tvbox/osc/ui/adapter/HomeHotVodAdapter.java
+++ app/src/main/java/com/github/tvbox/osc/ui/adapter/HomeHotVodAdapter.java
@@ -97,7 +97,7 @@ public class HomeHotVodAdapter extends BaseQuickAdapter<Movie.Video, BaseViewHol
             newWidth = ImgUtilHot.defaultWidth;
             newHeight = ImgUtilHot.defaultHeight;
         }
-        if (style != null && !this.mShowList) {
+        if (style != null && !this.mShowList) { //xuameng判断是否显示列表
             newWidth = defaultWidth;
             float safeRatio = ImgUtilHot.normalizeRatio(style.ratio);  //xuameng normalizeRatio强行指定ratio值防止用户乱写
             newHeight = (int) (newWidth / safeRatio);
