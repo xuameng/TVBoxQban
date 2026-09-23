f5bc1085720192dbf3493535e755f8005ff36838
xuameng <142598141+xuameng@users.noreply.github.com>
2026-09-23 18:28:43 +0800

    Update ImgUtil.java

--- app/src/main/java/com/github/tvbox/osc/util/ImgUtil.java
+++ app/src/main/java/com/github/tvbox/osc/util/ImgUtil.java
@@ -101,7 +101,7 @@ public class ImgUtil {
 
     public static int getStyleDefaultWidth(Style style) {
         // 1. style 为空，回退默认
-        if (style == null && !HawkConfig.isShowList) {
+        if (style == null && !HawkConfig.isShowList) {  //xuameng判断是否显示列表
             return defaultWidth;
         }
 
